42711af7207e0f808e861db607bbcfd843ce0926
Ricardo Antunes <me@riscadoa.com>
2021-09-24 22:01:30 +0100

    Added render device and clear functions

--- include/cubos/gl/render_device.hpp
+++ include/cubos/gl/render_device.hpp
@@ -3,7 +3,17 @@
 
 namespace cubos::gl
 {
+    class RenderDevice
+    {
+    public:
+        RenderDevice() = default;
+        virtual ~RenderDevice() = default;
+        RenderDevice(const RenderDevice&) = delete;
 
+        virtual void clearColor(float r, float g, float b, float a) const = 0;
+        virtual void clearDepth(float depth) const = 0;
+        virtual void clearStencil(int stencil) const = 0;
+    };
 } // namespace cubos::gl
 
 #endif // CUBOS_GL_RENDER_DEVICE_HPP
--- src/gl/ogl_render_device.cpp
+++ src/gl/ogl_render_device.cpp
@@ -4,6 +4,30 @@
 
 #include <glad/glad.h>
 
+using namespace cubos::gl;
 
+OGLRenderDevice::OGLRenderDevice()
+{
+    if (!gladLoadGL())
+        ; // TODO: Log critical error and abort
+}
+
+void OGLRenderDevice::clearColor(float r, float g, float b, float a) const
+{
+    glClearColor(r, g, b, a);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void OGLRenderDevice::clearDepth(float depth) const
+{
+    glClearDepth(depth);
+    glClear(GL_DEPTH_BUFFER_BIT);
+}
+
+void OGLRenderDevice::clearStencil(int stencil) const
+{
+    glClearStencil(stencil);
+    glClear(GL_STENCIL_BUFFER_BIT);
+}
 
 #endif // WITH_OPENGL
