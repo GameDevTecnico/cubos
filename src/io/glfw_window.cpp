7c1d728ac1fdc4bce34834e19ca56ca51f838a59
Ricardo Antunes <me@riscadoa.com>
2021-09-24 22:01:58 +0100

    Added glfwInit and glfwTerminate

--- src/io/glfw_window.cpp
+++ src/io/glfw_window.cpp
@@ -8,6 +8,9 @@ using namespace cubos::io;
 
 Window::Window()
 {
+    if (!glfwInit())
+        ; // TODO: Log critical error and abort
+
     // TODO: Read settings and set the correct window size, title and mode (fullscreen, ...)
     this->handle = glfwCreateWindow(800, 600, "Cubos", nullptr, nullptr);
     if (!this->handle)
@@ -16,7 +19,7 @@ Window::Window()
 
 Window::~Window()
 {
-    glfwDestroyWindow((GLFWwindow *)this->handle);
+    glfwTerminate();
 }
 
 void Window::pollEvents() const
@@ -26,19 +29,19 @@ void Window::pollEvents() const
 
 void Window::swapBuffers() const
 {
-    glfwSwapBuffers((GLFWwindow*)this->handle);
+    glfwSwapBuffers((GLFWwindow *)this->handle);
 }
 
 glm::ivec2 Window::getFramebufferSize() const
 {
     int width, height;
-    glfwGetFramebufferSize((GLFWwindow*)this->handle, &width, &height);
-    return { width, height };
+    glfwGetFramebufferSize((GLFWwindow *)this->handle, &width, &height);
+    return {width, height};
 }
 
 bool Window::shouldClose() const
 {
-    return glfwWindowShouldClose((GLFWwindow*)this->handle);
+    return glfwWindowShouldClose((GLFWwindow *)this->handle);
 }
 
 #endif // WITH_GLFW
