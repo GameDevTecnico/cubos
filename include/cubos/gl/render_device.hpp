504d496073b652cb2f8d278566511baf1936a110
Ricardo Antunes <me@riscadoa.com>
2021-09-24 20:30:39 +0100

    Prepared files for render device

---
+++ include/cubos/gl/render_device.hpp
@@ -0,0 +1,9 @@
+#ifndef CUBOS_GL_RENDER_DEVICE_HPP
+#define CUBOS_GL_RENDER_DEVICE_HPP
+
+namespace cubos::gl
+{
+
+} // namespace cubos::gl
+
+#endif // CUBOS_GL_RENDER_DEVICE_HPP
---
+++ src/gl/ogl_render_device.cpp
@@ -0,0 +1,9 @@
+#include <cubos/gl/ogl_render_device.hpp>
+
+#ifdef WITH_OPENGL
+
+#include <glad/glad.h>
+
+
+
+#endif // WITH_OPENGL
