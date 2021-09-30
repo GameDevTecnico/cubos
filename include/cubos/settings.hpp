62dd4c622dbb1fb697482b1c310395eb4e8925dc
Diogo Rato <diogo.rato.11@gmail.com>
2021-09-30 15:48:39 +0100

    Move Settings class definitions to settings.cpp

--- include/cubos/settings.hpp
+++ include/cubos/settings.hpp
@@ -31,33 +31,4 @@ namespace cubos
 
 } // namespace cubos
 
-cubos::Settings* cubos::Settings::_instance = nullptr;
-
-cubos::Settings* cubos::Settings::GetInstance()
-{
-    if (_instance == nullptr)
-    {
-        _instance = new Settings();
-    }
-    return _instance;
-}
-
-void cubos::Settings::clear()
-{
-    _values.clear();
-}
-
-void cubos::Settings::setString(const std::string& key, const std::string& value)
-{
-    _values[key] = value;
-}
-
-std::string cubos::Settings::getString(const std::string& key, const std::string& defaultValue)
-{
-    if (_values.count(key) == 0)
-        return defaultValue;
-
-    return _values[key];
-}
-
 #endif // CUBOS_SETTINGS_HPP
