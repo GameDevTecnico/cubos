13cec50a94a610b5adc9d51aec03c0f346f26d58
ratuspro <diogo.rato.11@gmail.com>
2021-10-01 23:18:59 +0100

    Add Doxygen documentation to public methods

--- include/cubos/settings.hpp
+++ include/cubos/settings.hpp
@@ -18,15 +18,36 @@ namespace cubos
     public:
         Settings(Settings& settings) = delete;
         void operator=(const Settings&) = delete;
-        
+
+        /**
+         * @brief Clears all the settings
+         *
+         * @return * void
+         */
         static void clear();
+
+        /**
+         * @brief Defines a new string setting.
+         *        If the setting already exists, overrides its value with the new value.
+         *
+         * @param key the setting's key
+         * @param value the setting's value
+         */
         static void setString(const std::string& key, const std::string& value);
+
+        /**
+         * @brief Retrieves the string settings with a specific key.
+         *        If no setting exists with such key, returns the default value.
+         *
+         * @param key the setting's key
+         * @param defaultValue the value returned when not setting with the provided key exists
+         * @return std::string the setting's value
+         */
         static std::string getString(const std::string& key, const std::string& defaultValue);
 
     private:
         std::unordered_map<std::string, std::string> _values;
         static Settings* GetInstance();
-
     };
 
 } // namespace cubos
