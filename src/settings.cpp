6a46cdc3247bdd5a84b56954c8c4962c603fb5cd
RiscadoA <me@riscadoa.com>
2021-10-11 08:20:29 +0100

    Made Settings::getInstance() return a reference

--- src/settings.cpp
+++ src/settings.cpp
@@ -2,34 +2,29 @@
 
 using namespace cubos;
 
-Settings* Settings::getInstance()
+Settings& Settings::getInstance()
 {
-    static Settings* instance = nullptr;
-
-    if (instance == nullptr)
-    {
-        instance = new Settings();
-    }
+    static Settings instance;
     return instance;
 }
 
 void Settings::clear()
 {
-    auto settings = Settings::getInstance();
-    settings->values.clear();
+    auto& settings = Settings::getInstance();
+    settings.values.clear();
 }
 
 void Settings::setString(const std::string& key, const std::string& value)
 {
-    auto settings = Settings::getInstance();
-    settings->values[key] = value;
+    auto& settings = Settings::getInstance();
+    settings.values[key] = value;
 }
 
 std::string Settings::getString(const std::string& key, const std::string& defaultValue)
 {
-    auto settings = Settings::getInstance();
-    if (settings->values.count(key) == 0)
+    auto& settings = Settings::getInstance();
+    if (settings.values.count(key) == 0)
         return defaultValue;
 
-    return settings->values[key];
+    return settings.values[key];
 }
