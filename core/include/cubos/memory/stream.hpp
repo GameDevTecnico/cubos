f830398fb484ebf07aa753fdb7eb7f2bf61fdec3
Ricardo Antunes <me@riscadoa.com>
2021-11-02 17:16:21 +0000

    Added seek, tell, eof, print & parse to Stream

--- core/include/cubos/memory/stream.hpp
+++ core/include/cubos/memory/stream.hpp
@@ -2,14 +2,28 @@
 #define CUBOS_MEMORY_STREAM_HPP
 
 #include <cstddef>
+#include <cstdint>
+#include <string>
 
 namespace cubos::memory
 {
+    /// Seek origin.
+    enum class SeekOrigin
+    {
+        Begin,
+        Current,
+        End
+    };
+
     /// Abstract class for memory streams.
     class Stream
     {
     public:
         virtual ~Stream() = default;
+
+        static Stream& stdIn;  ///< Stream wrapper for stdin.
+        static Stream& stdOut; ///< Stream wrapper for stdout.
+        static Stream& stdErr; ///< Stream wrapper for stderr.
 
         /// Reads data from the stream.
         /// @param data The buffer to read data into.
@@ -22,6 +36,130 @@ namespace cubos::memory
         /// @param size The size of the buffer.
         /// @return The number of bytes written.
         virtual size_t write(const void* data, size_t size) = 0;
+
+        /// Gets the current position in the stream.
+        /// @return The current position in the stream.
+        virtual size_t tell() const = 0;
+
+        /// Seeks to a position in the stream.
+        /// @param offset The offset to seek to.
+        /// @param origin The origin of the offset.
+        virtual void seek(size_t offset, SeekOrigin origin) = 0;
+
+        /// Checks if the stream still has content to read.
+        /// @return True if the stream has no more content to read, false otherwise.
+        virtual bool eof() const = 0;
+
+        /// Prints a 8 bit signed integer to the stream.
+        /// @param value The value to print.
+        /// @param base The base to use.
+        void print(int8_t value, size_t base = 10);
+
+        /// Prints a 16 bit signed integer to the stream.
+        /// @param value The value to print.
+        /// @param base The base to use.
+        void print(int16_t value, size_t base = 10);
+
+        /// Prints a 32 bit signed integer to the stream.
+        /// @param value The value to print.
+        /// @param base The base to use.
+        void print(int32_t value, size_t base = 10);
+
+        /// Prints a 64 bit signed integer to the stream.
+        /// @param value The value to print.
+        /// @param base The base to use.
+        void print(int64_t value, size_t base = 10);
+
+        /// Prints a 8 bit unsigned integer to the stream.
+        /// @param value The value to print.
+        /// @param base The base to use.
+        void print(uint8_t value, size_t base = 10);
+
+        /// Prints a 16 bit unsigned integer to the stream.
+        /// @param value The value to print.
+        /// @param base The base to use.
+        void print(uint16_t value, size_t base = 10);
+
+        /// Prints a 32 bit unsigned integer to the stream.
+        /// @param value The value to print.
+        /// @param base The base to use.
+        void print(uint32_t value, size_t base = 10);
+
+        /// Prints a 64 bit unsigned integer to the stream.
+        /// @param value The value to print.
+        /// @param base The base to use.
+        void print(uint64_t value, size_t base = 10);
+
+        /// Prints a float to the stream.
+        /// @param value The value to print.
+        /// @param decimalPlaces The number of decimal places to print.
+        void print(float value, size_t decimalPlaces = 6);
+
+        /// Prints a double to the stream.
+        /// @param value The value to print.
+        /// @param decimalPlaces The number of decimal places to print.
+        void print(double value, size_t decimalPlaces = 6);
+
+        /// Prints a string to the stream.
+        /// @param str The value to print.
+        void print(const char* str);
+
+        /// Prints a string to the stream.
+        /// @param str The string to print.
+        /// @param size The size of the string.
+        void print(const char* str, size_t size);
+
+        /// Prints a string to the stream.
+        /// @param str The value to print.
+        void print(const std::string& str);
+
+        /// Parses a 8 bit signed integer from the stream.
+        /// @param value Parsed value.
+        /// @param base The base to use.
+        void parse(int8_t& value, size_t base = 10);
+
+        /// Parses a 16 bit signed integer from the stream.
+        /// @param value Parsed value.
+        /// @param base The base to use.
+        void parse(int16_t& value, size_t base = 10);
+
+        /// Parses a 32 bit signed integer from the stream.
+        /// @param value Parsed value.
+        /// @param base The base to use.
+        void parse(int32_t& value, size_t base = 10);
+
+        /// Parses a 64 bit signed integer from the stream.
+        /// @param value Parsed value.
+        /// @param base The base to use.
+        void parse(int64_t& value, size_t base = 10);
+
+        /// Parses a 8 bit unsigned integer from the stream.
+        /// @param value Parsed value.
+        /// @param base The base to use.
+        void parse(uint8_t& value, size_t base = 10);
+
+        /// Parses a 16 bit unsigned integer from the stream.
+        /// @param value Parsed value.
+        /// @param base The base to use.
+        void parse(uint16_t& value, size_t base = 10);
+
+        /// Parses a 32 bit unsigned integer from the stream.
+        /// @param value Parsed value.
+        /// @param base The base to use.
+        void parse(uint32_t& value, size_t base = 10);
+
+        /// Parses a 64 bit unsigned integer from the stream.
+        /// @param value Parsed value.
+        /// @param base The base to use.
+        void parse(uint64_t& value, size_t base = 10);
+
+        /// Parses a float from the stream.
+        /// @param value Parsed value.
+        void parse(float& value);
+
+        /// Parses a double from the stream.
+        /// @param value Parsed value.
+        void parse(double& value);
     };
 } // namespace cubos::memory
 
