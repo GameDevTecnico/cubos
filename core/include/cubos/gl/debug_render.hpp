d1e443e896459dda393d2291c3b088db2dbd9264
joaopat98 <joaopat98@gmail.com>
2021-11-06 00:47:57 +0000

    Fix comment format

--- core/include/cubos/gl/debug_render.hpp
+++ core/include/cubos/gl/debug_render.hpp
@@ -17,57 +17,42 @@ namespace cubos
         /// Initialize the debug rendering system.
         static void init(gl::RenderDevice& renderDevice);
 
-        /**
-         * @brief
-         * Draw a filled cube that will stay visible for a specified amount of time.
-         * @param position Position of the cube's center in world space.
-         * @param size Size of the cube in the x, y and z dimensions in world space.
-         * @param time How long should the cube be visible for?
-         * @param rotation Rotation of the cube in world space.
-         * @param color Color of the drawn cube.
-         */
+        /// Draw a filled cube that will stay visible for a specified amount of time.
+        /// @param position Position of the cube's center in world space.
+        /// @param size Size of the cube in the x, y and z dimensions in world space.
+        /// @param time How long should the cube be visible for?
+        /// @param rotation Rotation of the cube in world space.
+        /// @param color Color of the drawn cube.
         static void drawCube(glm::vec3 position, glm::vec3 size, float time, glm::quat rotation = glm::quat(),
                              glm::vec3 color = glm::vec3(1));
 
-        /**
-         * @brief
-         * Draw a wireframe cube that will stay visible for a specified amount of time.
-         * @param position Position of the cube's center in world space.
-         * @param size Size of the cube in the x, y and z dimensions in world space.
-         * @param time How long should the cube be visible for?
-         * @param rotation Rotation of the cube in world space.
-         * @param color Color of the drawn cube.
-         */
+        /// Draw a wireframe cube that will stay visible for a specified amount of time.
+        /// @param position Position of the cube's center in world space.
+        /// @param size Size of the cube in the x, y and z dimensions in world space.
+        /// @param time How long should the cube be visible for?
+        /// @param rotation Rotation of the cube in world space.
+        /// @param color Color of the drawn cube.
         static void drawWireCube(glm::vec3 position, glm::vec3 size, float time, glm::quat rotation = glm::quat(),
                                  glm::vec3 color = glm::vec3(1));
 
-        /**
-         * @brief
-         * Draw a filled sphere that will stay visible for a specified amount of time.
-         * @param center Position of the sphere's center in world space.
-         * @param radius Radius of the sphere.
-         * @param time How long should the sphere be visible?
-         * @param color Color of the drawn sphere.
-         */
+        /// Draw a filled sphere that will stay visible for a specified amount of time.
+        /// @param center Position of the sphere's center in world space.
+        /// @param radius Radius of the sphere.
+        /// @param time How long should the sphere be visible?
+        /// @param color Color of the drawn sphere.
         static void drawSphere(glm::vec3 center, float radius, float time, glm::vec3 color = glm::vec3(1));
 
-        /**
-         * @brief
-         * Draw a wireframe sphere that will stay visible for a specified amount of time.
-         * @param center Position of the sphere's center in world space.
-         * @param radius Radius of the sphere.
-         * @param time How long should the sphere be visible?
-         * @param color Color of the drawn sphere.
-         */
+        /// Draw a wireframe sphere that will stay visible for a specified amount of time.
+        /// @param center Position of the sphere's center in world space.
+        /// @param radius Radius of the sphere.
+        /// @param time How long should the sphere be visible?
+        /// @param color Color of the drawn sphere.
         static void drawWireSphere(glm::vec3 center, float radius, float time, glm::vec3 color = glm::vec3(1));
 
-        /**
-         * @brief
-         * Render all objects in the debug renderer's buffer and update said buffer, removing all objects that have
-         * exhausted their requested time.
-         * @param vp View-Projection matrix that the objects should be drawn with.
-         * @param deltaT Time that has passed since the last call to flush.
-         */
+        /// Render all objects in the debug renderer's buffer and update said buffer, removing all objects that have
+        /// exhausted their requested time.
+        /// @param vp View-Projection matrix that the objects should be drawn with.
+        /// @param deltaT Time that has passed since the last call to flush.
         static void flush(glm::mat4 vp, double deltaT);
 
     private:
