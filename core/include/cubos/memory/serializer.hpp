0ec8e779f2506bd960b7cc27b216dd6588c81aef
Ricardo Antunes <me@riscadoa.com>
2021-11-07 11:45:32 +0000

    Added generic serialization context support

--- core/include/cubos/memory/serializer.hpp
+++ core/include/cubos/memory/serializer.hpp
@@ -2,7 +2,6 @@
 #define CUBOS_MEMORY_SERIALIZER_HPP
 
 #include <cubos/memory/stream.hpp>
-#include <cubos/memory/serialization_map.hpp>
 
 #include <string>
 #include <vector>
@@ -27,12 +26,11 @@ namespace cubos::memory
         ->std::same_as<void>;
     };
 
-    /// Concept for serializable objects which require a serialization map.
-    template <typename T, typename R, typename I>
-    concept MapSerializable = requires(T obj, Serializer& s, SerializationMap<R, I>& map)
+    /// Concept for serializable objects which require a context to be serialized.
+    template <typename T, typename TCtx> concept ContextSerializable = requires(T obj, Serializer& s, TCtx ctx)
     {
         {
-            obj.serialize(s, map)
+            obj.serialize(s, ctx)
         }
         ->std::same_as<void>;
     };
@@ -48,173 +46,175 @@ namespace cubos::memory
         /// Serializes a signed 8 bit integer.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(int8_t value, const char* name = nullptr) = 0;
+        virtual void write(int8_t value, const char* name) = 0;
 
         /// Serializes an signed 16 bit integer.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(int16_t value, const char* name = nullptr) = 0;
+        virtual void write(int16_t value, const char* name) = 0;
 
         /// Serializes an signed 32 bit integer.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(int32_t value, const char* name = nullptr) = 0;
+        virtual void write(int32_t value, const char* name) = 0;
 
         /// Serializes an signed 64 bit integer.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(int64_t value, const char* name = nullptr) = 0;
+        virtual void write(int64_t value, const char* name) = 0;
 
         /// Serializes an unsigned 8 bit integer.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(uint8_t value, const char* name = nullptr) = 0;
+        virtual void write(uint8_t value, const char* name) = 0;
 
         /// Serializes an unsigned 16 bit integer.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(uint16_t value, const char* name = nullptr) = 0;
+        virtual void write(uint16_t value, const char* name) = 0;
 
         /// Serializes an unsigned 32 bit integer.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(uint32_t value, const char* name = nullptr) = 0;
+        virtual void write(uint32_t value, const char* name) = 0;
 
         /// Serializes an unsigned 64 bit integer.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(uint64_t value, const char* name = nullptr) = 0;
+        virtual void write(uint64_t value, const char* name) = 0;
 
         /// Serializes a float.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(float value, const char* name = nullptr) = 0;
+        virtual void write(float value, const char* name) = 0;
 
         /// Serializes a double.
         /// @param value The value to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(double value, const char* name = nullptr) = 0;
+        virtual void write(double value, const char* name) = 0;
+
+        /// Serializes a boolean.
+        /// @param value The value to serialize.
+        /// @param name The name of the value (optional).
+        virtual void write(bool value, const char* name) = 0;
 
         /// Serializes a string.
         /// @param str The string to serialize.
         /// @param name The name of the value (optional).
-        virtual void write(const char* str, const char* name = nullptr) = 0;
+        virtual void write(const char* str, const char* name) = 0;
+
+        /// Serializes a string.
+        /// @param str The string to serialize.
+        /// @param name The name of the value (optional).
+        void write(const std::string& str, const char* name);
 
         /// Serializes a 2D vector.
         /// @tparam T The type of the vector.
         /// @param vec The vector to serialize.
         /// @param name The name of the value (optional).
-        template <typename T> void write(const glm::tvec2<T>& vec, const char* name = nullptr);
+        template <typename T> void write(const glm::tvec2<T>& vec, const char* name);
 
         /// Serializes a 3D vector.
         /// @tparam T The type of the vector.
         /// @param vec The vector to serialize.
         /// @param name The name of the value (optional).
-        template <typename T> void write(const glm::tvec3<T>& vec, const char* name = nullptr);
+        template <typename T> void write(const glm::tvec3<T>& vec, const char* name);
 
         /// Serializes a 4D vector.
         /// @tparam T The type of the vector.
         /// @param vec The vector to serialize.
         /// @param name The name of the value (optional).
-        template <typename T> void write(const glm::tvec4<T>& vec, const char* name = nullptr);
+        template <typename T> void write(const glm::tvec4<T>& vec, const char* name);
 
         /// Serializes a quaternion.
         /// @tparam T The type of the quaternion.
         /// @param quat The quaternion to serialize.
         /// @param name The name of the value (optional).
-        template <typename T> void write(const glm::tquat<T>& quat, const char* name = nullptr);
+        template <typename T> void write(const glm::tquat<T>& quat, const char* name);
 
         /// Serializes a object.
         /// @tparam T The type of the object.
         /// @param obj The object to serialize.
         /// @param name The name of the object (optional).
-        template <typename T> requires TriviallySerializable<T> void write(const T& obj, const char* name = nullptr);
+        template <typename T> requires TriviallySerializable<T> void write(const T& obj, const char* name);
 
         /// Serializes a object.
         /// @tparam T The type of the object.
-        /// @tparam R The reference type.
-        /// @tparam I The serialized identifier type.
+        /// @tparam TCtx The type of the context.
         /// @param obj The object to serialize.
-        /// @param map The serialization map.
+        /// @param ctx The context required to serialize.
         /// @param name The name of the object (optional).
-        template <typename T, typename R, typename I>
-        requires TriviallySerializable<T> void write(const T& obj, SerializationMap<R, I>& map,
-                                                     const char* name = nullptr);
+        template <typename T, typename TCtx>
+        requires TriviallySerializable<T> void write(const T& obj, TCtx ctx, const char* name);
 
-        /// Serializes a object which requires a serialization map to be serialized.
+        /// Serializes a object which requires context to be serialized.
         /// @tparam T The type of the object.
-        /// @tparam R The reference type.
-        /// @tparam I The serialized identifier type.
+        /// @tparam TCtx The type of the context.
         /// @param obj The object to serialize.
-        /// @param map The serialization map.
+        /// @param ctx The context required to serialize.
         /// @param name The name of the object (optional).
-        template <typename T, typename R, typename I>
-        requires MapSerializable<T, R, I> void write(const T& obj, SerializationMap<R, I>& map,
-                                                     const char* name = nullptr);
+        template <typename T, typename TCtx>
+        requires ContextSerializable<T, TCtx> void write(const T& obj, TCtx ctx, const char* name);
 
-        /// Serializes a object which requires a serialization map to be serialized.
+        /// Serializes a object.
         /// @tparam T The type of the object.
-        /// @tparam R The reference type.
-        /// @tparam I The serialized identifier type.
+        /// @tparam TCtx The type of the context.
         /// @param obj The object to serialize.
-        /// @param map The serialization map.
+        /// @param ctx The context required to serialize.
         /// @param name The name of the object (optional).
-        template <typename T, typename R, typename I>
-        void write(const T& obj, SerializationMap<R, I>& map, const char* name = nullptr);
+        template <typename T, typename TCtx> void write(const T& obj, TCtx ctx, const char* name);
 
         /// Serializes a vector.
         /// @tparam T The type of the vector.
         /// @param vec The vector to serialize.
         /// @param name The name of the vector (optional).
-        template <typename T> void write(const std::vector<T>& vec, const char* name = nullptr);
+        template <typename T> void write(const std::vector<T>& vec, const char* name);
 
         /// Serializes an array.
         /// @tparam T The type of the array.
         /// @param ptr The array to serialize.
         /// @param length The length of the array.
         /// @param name The name of the array (optional).
-        template <typename T> void write(const T* ptr, size_t length, const char* name = nullptr);
+        template <typename T> void write(const T* ptr, size_t length, const char* name);
 
         /// Serializes a unordered map.
         /// @tparam K The key type.
         /// @tparam V The value type.
         /// @param dic The map to serialize.
         /// @param name The name of the map (optional).
-        template <typename K, typename V> void write(const std::unordered_map<K, V>& dic, const char* name = nullptr);
+        template <typename K, typename V> void write(const std::unordered_map<K, V>& dic, const char* name);
 
         /// Serializes a vector.
         /// @tparam T The type of the vector.
-        /// @tparam R The reference type.
-        /// @tparam I The serialized identifier type.
+        /// @tparam TCtx The type of the context.
         /// @param vec The vector to serialize.
-        /// @param map The serialization map.
+        /// @param ctx The context required to serialize.
         /// @param name The name of the vector (optional).
-        template <typename T, typename R, typename I>
-        void write(const std::vector<T>& vec, SerializationMap<R, I>& map, const char* name = nullptr);
+        template <typename T, typename TCtx> void write(const std::vector<T>& vec, TCtx ctx, const char* name);
 
         /// Serializes an array.
         /// @tparam T The type of the array.
-        /// @tparam R The reference type.
-        /// @tparam I The serialized identifier type.
+        /// @tparam TCtx The type of the context.
         /// @param ptr The array to serialize.
         /// @param length The length of the array.
-        /// @param map The serialization map.
+        /// @param ctx The context required to serialize.
         /// @param name The name of the array (optional).
-        template <typename T, typename R, typename I>
-        void write(const T* ptr, size_t length, SerializationMap<R, I>& map, const char* name = nullptr);
+        template <typename T, typename TCtx> void write(const T* ptr, size_t length, TCtx ctx, const char* name);
 
         /// Serializes a unordered map.
         /// @tparam K The key type.
         /// @tparam V The value type.
+        /// @tparam TCtx The type of the context.
         /// @param dic The map to serialize.
+        /// @param ctx The context required to serialize.
         /// @param name The name of the map (optional).
-        template <typename K, typename V, typename R, typename I>
-        void write(const std::unordered_map<K, V>& dic, SerializationMap<R, I>& map, const char* name = nullptr);
+        template <typename K, typename V, typename TCtx>
+        void write(const std::unordered_map<K, V>& dic, TCtx ctx, const char* name);
 
     protected:
         /// Indicates that a object is currently being serialized.
         /// @param name The name of the object (optional).
-        virtual void beginObject(const char* name = nullptr) = 0;
+        virtual void beginObject(const char* name) = 0;
 
         /// Indicates that a object is no longer being serialized.
         virtual void endObject() = 0;
@@ -222,7 +222,7 @@ namespace cubos::memory
         /// Indicates that a array is currently being serialized.
         /// @param length The length of the array.
         /// @param name The name of the array (optional).
-        virtual void beginArray(size_t length, const char* name = nullptr) = 0;
+        virtual void beginArray(size_t length, const char* name) = 0;
 
         /// Indicates that a array is no longer being serialized.
         virtual void endArray() = 0;
@@ -230,7 +230,7 @@ namespace cubos::memory
         /// Indicates that a dictionary is currently being serialized.
         /// @param length The length of the dictionary.
         /// @param name The name of the dictionary (optional).
-        virtual void beginDictionary(size_t length, const char* name = nullptr) = 0;
+        virtual void beginDictionary(size_t length, const char* name) = 0;
 
         /// Indicates that a dictionary is no longer being serialized.
         virtual void endDictionary() = 0;
@@ -288,23 +288,21 @@ namespace cubos::memory
         this->endObject();
     }
 
-    template <typename T, typename R, typename I>
-    requires TriviallySerializable<T> void Serializer::write(const T& obj, SerializationMap<R, I>&, const char* name)
+    template <typename T, typename TCtx>
+    requires TriviallySerializable<T> void Serializer::write(const T& obj, TCtx ctx, const char* name)
     {
         this->write(obj, name);
     }
 
-    template <typename T, typename R, typename I>
-    requires MapSerializable<T, R, I> void Serializer::write(const T& obj, SerializationMap<R, I>& map,
-                                                             const char* name)
+    template <typename T, typename TCtx>
+    requires ContextSerializable<T, TCtx> void Serializer::write(const T& obj, TCtx ctx, const char* name)
     {
         this->beginObject(name);
-        obj.serialize(*this, map);
+        obj.serialize(*this, ctx);
         this->endObject();
     }
 
-    template <typename T, typename R, typename I>
-    void Serializer::write(const T& obj, SerializationMap<R, I>&, const char* name)
+    template <typename T, typename TCtx> void Serializer::write(const T& obj, TCtx, const char* name)
     {
         this->write(obj, name);
     }
@@ -330,38 +328,36 @@ namespace cubos::memory
         this->beginDictionary(dic.size(), name);
         for (const auto& pair : dic)
         {
-            this->write(pair.first);
-            this->write(pair.second);
+            this->write(pair.first, nullptr);
+            this->write(pair.second, nullptr);
         }
         this->endDictionary();
     }
 
-    template <typename T, typename R, typename I>
-    void Serializer::write(const std::vector<T>& vec, SerializationMap<R, I>& map, const char* name)
+    template <typename T, typename TCtx> void Serializer::write(const std::vector<T>& vec, TCtx ctx, const char* name)
     {
         this->beginArray(vec.size(), name);
         for (const auto& obj : vec)
-            this->write(obj, map);
+            this->write(obj, ctx, nullptr);
         this->endArray();
     }
 
-    template <typename T, typename R, typename I>
-    void Serializer::write(const T* ptr, size_t length, SerializationMap<R, I>& map, const char* name)
+    template <typename T, typename TCtx> void Serializer::write(const T* ptr, size_t length, TCtx ctx, const char* name)
     {
         this->beginArray(length, name);
         for (size_t i = 0; i < length; ++i)
-            this->write(ptr[i], map);
+            this->write(ptr[i], ctx, nullptr);
         this->endArray();
     }
 
-    template <typename K, typename V, typename R, typename I>
-    void Serializer::write(const std::unordered_map<K, V>& dic, SerializationMap<R, I>& map, const char* name)
+    template <typename K, typename V, typename TCtx>
+    void Serializer::write(const std::unordered_map<K, V>& dic, TCtx ctx, const char* name)
     {
         this->beginDictionary(dic.size(), name);
         for (const auto& pair : dic)
         {
-            this->write(pair.first, map);
-            this->write(pair.second, map);
+            this->write(pair.first, ctx, nullptr);
+            this->write(pair.second, ctx, nullptr);
         }
         this->endDictionary();
     }
--- core/src/cubos/memory/serializer.cpp
+++ core/src/cubos/memory/serializer.cpp
@@ -5,3 +5,8 @@ using namespace cubos::memory;
 Serializer::Serializer(Stream& stream) : stream(stream)
 {
 }
+
+void Serializer::write(const std::string& str, const char* name)
+{
+    this->write(str.c_str(), name);
+}
