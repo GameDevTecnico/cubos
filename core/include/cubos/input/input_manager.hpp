ba4aafe33c491920043e878a26acd21afe68f04a
João Patrício <joaopat98@gmail.com>
2022-01-12 20:13:48 +0000

    Move method callbacks to Event class

--- core/include/cubos/input/input_manager.hpp
+++ core/include/cubos/input/input_manager.hpp
@@ -77,26 +77,6 @@ namespace cubos::input
         static std::map<std::string, std::shared_ptr<InputAction>> actions;
 
         static std::map<cubos::io::Key, std::vector<std::function<void(void)>>> keyDownCallbacks;
-
-        template <typename... TArgs> class Callback
-        {
-        public:
-            virtual void call(TArgs...) = 0;
-        };
-        template <class TObj, typename... TArgs> class ObjectCallback : public Callback<TArgs...>
-        {
-        public:
-            ObjectCallback(TObj* obj, void (TObj::*callback)(TArgs...)) : obj(obj), callback(callback)
-            {
-            }
-            TObj* obj;
-            void (TObj::*callback)(TArgs...);
-            virtual void call(TArgs... args) override
-            {
-                (obj->*callback)(args...);
-            }
-        };
-        static std::map<cubos::io::Key, std::vector<Callback<>*>> keyDownObjectCallbacks;
         static std::map<cubos::io::Key, std::vector<std::function<void(void)>>> keyUpCallbacks;
         static std::map<cubos::io::MouseButton, std::vector<std::function<void(void)>>> mouseButtonDownCallbacks;
         static std::map<cubos::io::MouseButton, std::vector<std::function<void(void)>>> mouseButtonUpCallbacks;
@@ -109,40 +89,9 @@ namespace cubos::input
         static void registerKeyDownCallback(std::function<void(void)> callback, cubos::io::Key key);
         template <class T> static void registerKeyDownCallback(T* obj, void (T::*callback)(), cubos::io::Key key)
         {
-            auto cb = new ObjectCallback<T>(obj, callback);
-            if (InputManager::keyDownObjectCallbacks.contains(key))
-            {
-                InputManager::keyDownObjectCallbacks[key].push_back(cb);
-            }
-            else
-            {
-                std::vector<Callback<>*> v = std::vector<Callback<>*>();
-                v.push_back(cb);
-                InputManager::keyDownObjectCallbacks.insert({key, v});
-            }
         }
-        template <class T> static void removeKeyDownCallback(T* obj, void (T::*callback)(), cubos::io::Key key)
+        template <class T> static void unregisterKeyDownCallback(T* obj, void (T::*callback)(), cubos::io::Key key)
         {
-            if (InputManager::keyDownObjectCallbacks.contains(key))
-            {
-                auto it = keyDownObjectCallbacks[key].begin();
-                for (; it < keyDownObjectCallbacks[key].end(); it++)
-                {
-                    Callback<>& ref = **it;
-                    if (typeid(ref) == typeid(ObjectCallback<T>))
-                    {
-                        auto cb = static_cast<ObjectCallback<T>*>(*it);
-                        if (cb->obj == obj && cb->callback == callback)
-                            break;
-                    }
-                }
-                if (it != keyDownObjectCallbacks[key].end())
-                {
-                    auto cb = *it;
-                    keyDownObjectCallbacks[key].erase(it);
-                    delete cb;
-                }
-            }
         }
         static void registerMouseButtonDownCallback(std::function<void(void)> callback, cubos::io::MouseButton);
 
