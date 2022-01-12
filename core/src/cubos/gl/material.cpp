999d8c9152534f4e2d7a51b940ae62eeef779ce3
Ricardo Antunes <me@riscadoa.com>
2022-01-12 14:40:21 +0000

    Added Material struct

---
+++ core/src/cubos/gl/material.cpp
@@ -0,0 +1,15 @@
+#include <cubos/gl/material.hpp>
+
+using namespace cubos::gl;
+
+const Material Material::Empty = {{0, 0, 0, 0}};
+
+void Material::serialize(memory::Serializer& serializer) const
+{
+    serializer.write(color, "color");
+}
+
+void Material::deserialize(memory::Deserializer& deserializer)
+{
+    deserializer.read(color);
+}
