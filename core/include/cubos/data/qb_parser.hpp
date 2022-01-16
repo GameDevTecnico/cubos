8fa29cc73e88a859b2f11f3be037a37b6bed539e
Ricardo Antunes <me@riscadoa.com>
2022-01-16 16:38:59 +0000

    Implements parseQB()

--- core/include/cubos/data/qb_parser.hpp
+++ core/include/cubos/data/qb_parser.hpp
@@ -5,14 +5,25 @@
 #include <cubos/gl/grid.hpp>
 #include <cubos/gl/palette.hpp>
 
+#include <glm/glm.hpp>
+
+#include <vector>
+
 namespace cubos::data
 {
-    /// Parses a Qubicle file (.qb), outputting a gl::Grid and a gl::Palette.
-    /// @param grid The grid to output.
-    /// @param palette The palette to output.
+    /// Represents the data read from a matrix in a QB file.
+    struct QBMatrix
+    {
+        gl::Grid grid;       ///< The grid of the matrix.
+        gl::Palette palette; ///< The palette of the matrix.
+        glm::ivec3 position; ///< The position of the matrix.
+    };
+
+    /// Parses a Qubicle file (.qb), pushing every matrix found in the file to the passed vector.
+    /// @param matrices The matrices to output.
     /// @param stream The stream to read from.
     /// @return True if the file was parsed successfully, otherwise false.
-    bool parseQB(gl::Grid& grid, gl::Palette& palette, memory::Stream& stream);
+    bool parseQB(std::vector<QBMatrix>& matrices, memory::Stream& stream);
 } // namespace cubos::data
 
 #endif // CUBOS_DATA_QB_PARSER_HPP
