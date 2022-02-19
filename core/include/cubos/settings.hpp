c6815c42909d6f42af89122768771e6da21c27ed
ratuspro <diogo.rato.11@gmail.com>
2022-02-19 18:35:12 +0100

    Add integer and double getters and setters to Settings
    Add respective tests

--- core/include/cubos/settings.hpp
+++ core/include/cubos/settings.hpp
@@ -26,7 +26,33 @@ namespace cubos
         /// @param key The setting's key.
         /// @param defaultValue The value returned when not setting with the provided key exists.
         /// @return The setting's value.
-        std::string getString(const std::string& key, const std::string& defaultValue);
+        std::string getString(const std::string& key, const std::string& defaultValue) const;
+
+        /// Defines a new integer setting.
+        /// If the setting already exists, overrides its value with the new value.
+        /// @param key The setting's key.
+        /// @param value The setting's value.
+        void setInteger(const std::string& key, const int value);
+
+        /// Retrieves the integer settings with a specific key.
+        /// If no setting exists with such key, returns the default value.
+        /// @param key The setting's key.
+        /// @param defaultValue The value returned when not setting with the provided key exists.
+        /// @return The setting's value.
+        int getInteger(const std::string& key, const int defaultValue) const;
+
+        /// Defines a new double setting.
+        /// If the setting already exists, overrides its value with the new value.
+        /// @param key The setting's key.
+        /// @param value The setting's value.
+        void setDouble(const std::string& key, const double value);
+
+        /// Retrieves the double settings with a specific key.
+        /// If no setting exists with such key, returns the default value.
+        /// @param key The setting's key.
+        /// @param defaultValue The value returned when not setting with the provided key exists.
+        /// @return The setting's value.
+        double getDouble(const std::string& key, const double defaultValue) const;
 
         /// Merges the settings from settingsToMerge.
         /// If a setting from settingsToMerge already exists, overrides its value. @see setString.
--- core/src/cubos/settings.cpp
+++ core/src/cubos/settings.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <cubos/settings.hpp>
 
 using namespace cubos;
@@ -14,12 +15,38 @@ void Settings::setString(const std::string& key, const std::string& value)
     this->values[key] = value;
 }
 
-std::string Settings::getString(const std::string& key, const std::string& defaultValue)
+std::string Settings::getString(const std::string& key, const std::string& defaultValue) const
 {
     if (this->values.count(key) == 0)
         return defaultValue;
 
-    return this->values[key];
+    return this->values.at(key);
+}
+
+void Settings::setInteger(const std::string& key, const int value)
+{
+    this->values[key] = std::to_string(value);
+}
+
+int Settings::getInteger(const std::string& key, const int defaultValue) const
+{
+    if (this->values.count(key) == 0)
+        return defaultValue;
+
+    return std::stoi(this->values.at(key));
+}
+
+void Settings::setDouble(const std::string& key, const double value)
+{
+    this->values[key] = std::to_string(value);
+}
+
+double Settings::getDouble(const std::string& key, const double defaultValue) const
+{
+    if (this->values.count(key) == 0)
+        return defaultValue;
+
+    return std::stod(this->values.at(key));
 }
 
 void Settings::merge(const Settings& settingsToMerge)
