2d5addd6391197aa812c43b3f61a69c79a863683
RiscadoA <RiscadoA@users.noreply.github.com>
2022-04-09 23:04:42 +0000

    deploy: 1c34686d71a734ce2769e7eca98e0065cf7c6511

--- navtreeindex2.js
+++ navtreeindex2.js
@@ -1,8 +1,5 @@
 var NAVTREEINDEX2 =
 {
-"files.html":[2,0],
-"functions.html":[1,3,0],
-"functions.html":[1,3,0,0],
 "functions_b.html":[1,3,0,1],
 "functions_c.html":[1,3,0,2],
 "functions_d.html":[1,3,0,3],
