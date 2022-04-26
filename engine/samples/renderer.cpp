b79c1b81293f32528aac42eec22c665c8c30839e
Ricardo Antunes <me@riscadoa.com>
2022-04-26 15:31:15 +0100

    Fix renderer samples

--- engine/samples/renderer.cpp
+++ engine/samples/renderer.cpp
@@ -19,107 +19,111 @@ int main(void)
     auto window = io::Window::create();
     auto& renderDevice = window->getRenderDevice();
 
-    using namespace cubos::core::gl;
-    using namespace cubos::engine::gl;
-
-    auto renderer = cubos::engine::gl::deferred::Renderer(*window);
-
-    Palette palette1(std::vector<Material>{
-        {{1, 0, 0, 1}},
-        {{0, 1, 0, 1}},
-        {{0, 0, 1, 1}},
-        {{1, 1, 0, 1}},
-        {{0, 1, 1, 1}},
-        {{1, 0, 1, 1}},
-    });
-    Palette palette2(std::vector<Material>{
-        {{0, 1, 1, 1}},
-        {{1, 0, 1, 1}},
-        {{1, 1, 0, 1}},
-        {{0, 0, 1, 1}},
-        {{1, 0, 0, 1}},
-        {{0, 1, 0, 1}},
-    });
-
-    auto palette1ID = renderer.registerPalette(palette1);
-    auto palette2ID = renderer.registerPalette(palette2);
-
-    Grid cube;
-
-    cube.set({0,0,0}, 1);
-
-    Renderer::ModelID id = renderer.registerModel(cube);
-
-    pps::CopyPass pass = pps::CopyPass(*window);
-    renderer.addPostProcessingPass(pass);
-
-    glm::vec2 windowSize = window->getFramebufferSize();
-    CameraData mainCamera = {// glm::mat4 viewMatrix;
-                             glm::lookAt(glm::vec3{7, 7, 7}, glm::vec3{0, 0, 0}, glm::vec3{0, 1, 0}),
-                             // glm::mat4 perspectiveMatrix;
-                             glm::perspective(glm::radians(20.0f), windowSize.x / windowSize.y, 0.1f, 1000.f),
-                             // gl::Framebuffer target;
-                             0};
-    float t = 0;
-    int s = 0;
-
-    while (!window->shouldClose())
     {
-        float currentT = window->getTime();
-        float deltaT = 0;
-        if (t != 0)
-        {
-            deltaT = currentT - t;
-            int newS = std::round(t);
-            if (newS != s)
-                logDebug("FPS: {}", std::round(1 / deltaT));
-            s = newS;
-        }
-        t = currentT;
-        renderDevice.setFramebuffer(0);
-        renderDevice.clearColor(0.0, 0.0, 0.0, 0.0f);
+        using namespace cubos::core::gl;
+        using namespace cubos::engine::gl;
 
-        for (int x = -1; x < 1; x++)
+        auto renderer = deferred::Renderer(*window);
+
+        Palette palette1(std::vector<Material>{
+            {{1, 0, 0, 1}},
+            {{0, 1, 0, 1}},
+            {{0, 0, 1, 1}},
+            {{1, 1, 0, 1}},
+            {{0, 1, 1, 1}},
+            {{1, 0, 1, 1}},
+        });
+        Palette palette2(std::vector<Material>{
+            {{0, 1, 1, 1}},
+            {{1, 0, 1, 1}},
+            {{1, 1, 0, 1}},
+            {{0, 0, 1, 1}},
+            {{1, 0, 0, 1}},
+            {{0, 1, 0, 1}},
+        });
+
+        auto palette1ID = renderer.registerPalette(palette1);
+        auto palette2ID = renderer.registerPalette(palette2);
+
+        Grid cube;
+
+        cube.set({0, 0, 0}, 1);
+
+        Renderer::ModelID id = renderer.registerModel(cube);
+
+        pps::CopyPass pass = pps::CopyPass(*window);
+        renderer.addPostProcessingPass(pass);
+
+        glm::vec2 windowSize = window->getFramebufferSize();
+        CameraData mainCamera = {// glm::mat4 viewMatrix;
+                                 glm::lookAt(glm::vec3{7, 7, 7}, glm::vec3{0, 0, 0}, glm::vec3{0, 1, 0}),
+                                 // glm::mat4 perspectiveMatrix;
+                                 glm::perspective(glm::radians(20.0f), windowSize.x / windowSize.y, 0.1f, 1000.f),
+                                 // gl::Framebuffer target;
+                                 0};
+        float t = 0;
+        int s = 0;
+
+        while (!window->shouldClose())
         {
-            for (int y = -1; y < 1; y++)
+            float currentT = window->getTime();
+            float deltaT = 0;
+            if (t != 0)
             {
-                for (int z = -1; z < 1; z++)
-                {
-                    auto axis = glm::vec3(x, y, z) * 2.0f + glm::vec3(1);
+                deltaT = currentT - t;
+                int newS = std::round(t);
+                if (newS != s)
+                    logDebug("FPS: {}", std::round(1 / deltaT));
+                s = newS;
+            }
+            t = currentT;
+            renderDevice.setFramebuffer(0);
+            renderDevice.clearColor(0.0, 0.0, 0.0, 0.0f);
 
-                    auto modelMat = glm::translate(glm::mat4(1.0f), glm::vec3(x, y, z)) *
-                                    glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, 0.5f)) *
-                                    glm::rotate(glm::mat4(1.0f), glm::radians(t), axis) *
-                                    glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, -0.5f, -0.5f));
-                    renderer.drawModel(id, modelMat);
+            for (int x = -1; x < 1; x++)
+            {
+                for (int y = -1; y < 1; y++)
+                {
+                    for (int z = -1; z < 1; z++)
+                    {
+                        auto axis = glm::vec3(x, y, z) * 2.0f + glm::vec3(1);
+
+                        auto modelMat = glm::translate(glm::mat4(1.0f), glm::vec3(x, y, z)) *
+                                        glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, 0.5f)) *
+                                        glm::rotate(glm::mat4(1.0f), glm::radians(t), axis) *
+                                        glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, -0.5f, -0.5f));
+                        renderer.drawModel(id, modelMat);
+                    }
                 }
             }
+
+            glm::quat spotLightRotation =
+                glm::quat(glm::vec3(0, t, 0)) * glm::quat(glm::vec3(glm::radians(45.0f), 0, 0));
+            glm::quat directionalLightRotation =
+                glm::quat(glm::vec3(0, 90, 0)) * glm::quat(glm::vec3(glm::radians(45.0f), 0, 0));
+            glm::quat pointLightRotation =
+                glm::quat(glm::vec3(0, -t, 0)) * glm::quat(glm::vec3(glm::radians(45.0f), 0, 0));
+
+            renderer.drawLight(SpotLight(spotLightRotation * glm::vec3(0, 0, -5), spotLightRotation, glm::vec3(1), 1,
+                                         100, glm::radians(10.0f), glm::radians(9.0f)));
+            renderer.drawLight(DirectionalLight(directionalLightRotation, glm::vec3(1), 0.5f));
+            renderer.drawLight(PointLight(pointLightRotation * glm::vec3(0, 0, -2), glm::vec3(1), 1, 10));
+
+            if (sin(t * 4) > 0)
+            {
+                renderer.setPalette(palette1ID);
+            }
+            else
+            {
+                renderer.setPalette(palette2ID);
+            }
+
+            renderer.render(mainCamera);
+            renderer.flush();
+
+            window->swapBuffers();
+            window->pollEvents();
         }
-
-        glm::quat spotLightRotation = glm::quat(glm::vec3(0, t, 0)) * glm::quat(glm::vec3(glm::radians(45.0f), 0, 0));
-        glm::quat directionalLightRotation =
-            glm::quat(glm::vec3(0, 90, 0)) * glm::quat(glm::vec3(glm::radians(45.0f), 0, 0));
-        glm::quat pointLightRotation = glm::quat(glm::vec3(0, -t, 0)) * glm::quat(glm::vec3(glm::radians(45.0f), 0, 0));
-
-        renderer.drawLight(SpotLight(spotLightRotation * glm::vec3(0, 0, -5), spotLightRotation, glm::vec3(1), 1, 100,
-                                     glm::radians(10.0f), glm::radians(9.0f)));
-        renderer.drawLight(DirectionalLight(directionalLightRotation, glm::vec3(1), 0.5f));
-        renderer.drawLight(PointLight(pointLightRotation * glm::vec3(0, 0, -2), glm::vec3(1), 1, 10));
-
-        if (sin(t * 4) > 0)
-        {
-            renderer.setPalette(palette1ID);
-        }
-        else
-        {
-            renderer.setPalette(palette2ID);
-        }
-
-        renderer.render(mainCamera);
-        renderer.flush();
-
-        window->swapBuffers();
-        window->pollEvents();
     }
 
     delete window;
