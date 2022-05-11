5b5a1ae15f34c8afb1b20055d5c95473e87d4dcd
Ricardo Antunes <me@riscadoa.com>
2022-05-11 17:17:24 +0100

    Add Grid::convert for converting between palettes

--- core/src/cubos/core/gl/grid.cpp
+++ core/src/cubos/core/gl/grid.cpp
@@ -1,5 +1,8 @@
 #include <cubos/core/gl/grid.hpp>
+#include <cubos/core/gl/palette.hpp>
 #include <cubos/core/log.hpp>
+
+#include <unordered_map>
 
 using namespace cubos::core::gl;
 
@@ -86,6 +89,35 @@ void Grid::set(const glm::ivec3& position, uint16_t mat)
     assert(position.x >= 0 && position.x < this->size.x && position.y >= 0 && position.y < this->size.y &&
            position.z >= 0 && position.z < this->size.z);
     this->indices[position.x + position.y * size.x + position.z * size.x * size.y] = mat;
+}
+
+bool Grid::convert(const Palette& src, const Palette& dst, float min_similarity)
+{
+    // Find the mappings for every material in the source palette.
+    std::unordered_map<uint16_t, uint16_t> mappings;
+    for (uint16_t i = 0; i < src.getSize(); ++i)
+    {
+        uint16_t j = dst.find(src.get(i));
+        if (src.get(i).similarity(dst.get(j)) >= min_similarity)
+        {
+            mappings[i] = j;
+        }
+    }
+
+    // Check if the mappings are complete for every material being used in the grid.
+    for (uint16_t i = 0; i < this->size.x * this->size.y * this->size.z; ++i)
+    {
+        if (mappings.find(this->indices[i]) == mappings.end())
+            return false;
+    }
+
+    // Apply the mappings.
+    for (uint16_t i = 0; i < this->size.x * this->size.y * this->size.z; ++i)
+    {
+        this->indices[i] = mappings[this->indices[i]];
+    }
+
+    return true;
 }
 
 void Grid::serialize(memory::Serializer& serializer) const
