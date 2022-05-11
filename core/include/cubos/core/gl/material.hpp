691970c04a84eca875e8234a4bc8cf3fcddd7840
Ricardo Antunes <me@riscadoa.com>
2022-05-11 17:12:45 +0100

    Add `similarity` to Material for comparisons

--- core/include/cubos/core/gl/material.hpp
+++ core/include/cubos/core/gl/material.hpp
@@ -15,6 +15,13 @@ namespace cubos::core::gl
 
         glm::vec4 color; ///< The color of the material.
 
+        /// Compares this material with another and returns a number which indicates how similar they are to one
+        /// another. The number is in the range [0, 1], where 0 means they are completely different and 1 means they
+        /// are exactly the same.
+        /// @param other The other material to compare with.
+        /// @return The similarity between the two materials.
+        float similarity(const Material& other) const;
+
         /// Serializes the material.
         /// @param serializer The serializer to use.
         void serialize(memory::Serializer& serializer) const;
