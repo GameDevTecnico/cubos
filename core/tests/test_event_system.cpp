9621cdd14de620b9d9efa46b8352b2b6d2ed3d93
awyxx <robyawyx@gmail.com>
2022-12-13 20:57:49 +0000

    fixed sample and test case

--- core/tests/test_event_system.cpp
+++ core/tests/test_event_system.cpp
@@ -11,7 +11,7 @@ TEST(Cubos_Core_Event_System, Event_System_Simple_Test)
 {
     auto pipe = EventPipe<int>();
     auto writer = EventWriter<int>(pipe);
-    auto reader = EventReader<int, void>(pipe);
+    auto reader = EventReader<int, 0>(pipe);
 
     std::size_t size = 0;
     for (auto x : reader)
@@ -35,4 +35,34 @@ TEST(Cubos_Core_Event_System, Event_System_Simple_Test)
         size++;
     }
     EXPECT_EQ(size, 0);
+}
+
+TEST(Cubos_Core_Event_System, Event_System_Masking_Test)
+{
+    struct MyEvent
+    {
+        enum Mask
+        {
+            KEY_EVENT = 1,
+            MOUSE_EVENT = 2,
+            WHEEL_EVENT = 3,
+            ALL = 4,
+        };
+
+        int data; // random data member
+    };
+
+    auto pipe = EventPipe<MyEvent>();
+    auto writer = EventWriter<MyEvent>(pipe);
+
+    writer.push(MyEvent{.data = 4}, MyEvent::Mask::KEY_EVENT);
+    writer.push(MyEvent{.data = 1}, MyEvent::Mask::WHEEL_EVENT);
+    writer.push(MyEvent{.data = 2}, MyEvent::Mask::MOUSE_EVENT);
+    writer.push(MyEvent{.data = 6}, MyEvent::Mask::MOUSE_EVENT);
+
+    for (const auto& ev : EventReader<MyEvent, MyEvent::Mask::KEY_EVENT>(pipe))
+    {
+        EXPECT_EQ(ev.data, 4);
+        // should run only once
+    }
 }
