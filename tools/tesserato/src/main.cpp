ed7c72044bca42b4ea131a28ff268b604a858df0
awyxx <robyawyx@gmail.com>
2022-12-14 22:49:56 +0000

    added simple base editor code

--- tools/tesserato/src/main.cpp
+++ tools/tesserato/src/main.cpp
@@ -1,7 +1,40 @@
-#include <stdio.h>
+#include <cubos/engine/cubos.hpp>
 
-int main()
+#include <cubos/engine/plugins/window.hpp>
+#include <cubos/engine/plugins/env_settings.hpp>
+
+#include <cubos/core/ui/imgui.hpp>
+#include <cubos/core/ui/serialization.hpp>
+#include <imgui.h>
+
+static void setup(const cubos::core::io::Window& window, ShouldQuit& quit)
 {
-    printf("hello from tesserato editor!");
-    return 0;
+    cubos::core::ui::initialize(window);
+}
+
+static void showUserInterface(const cubos::core::io::Window& window)
+{
+    auto& renderDevice = window->getRenderDevice();
+
+    cubos::core::ui::beginFrame();
+
+    ImGui::ShowDemoWindow();
+
+    auto sz = window->getFramebufferSize();
+    renderDevice.setRasterState(nullptr);
+    renderDevice.setBlendState(nullptr);
+    renderDevice.setDepthStencilState(nullptr);
+    renderDevice.setViewport(0, 0, sz.x, sz.y);
+    renderDevice.clearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    cubos::core::ui::endFrame();
+}
+
+int main(int argc, char** argv)
+{
+    Cubos(argc, argv)
+        .addPlugin(cubos::engine::plugins::windowPlugin)
+        .addStartupSystem(setup, "Setup")
+        .addSystem(showUserInterface, "UI")
+        .run();
 }
