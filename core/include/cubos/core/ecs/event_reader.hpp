c25e2be08551a9a02ae55dad6413ad20496cb937
Ricardo Antunes <me@riscadoa.com>
2023-01-06 12:11:01 +0000

    Random fixes

--- core/include/cubos/core/ecs/event_reader.hpp
+++ core/include/cubos/core/ecs/event_reader.hpp
@@ -17,22 +17,22 @@ namespace cubos::core::ecs
 
         /// Returns a reference to current event, and advances.
         /// It returns nullopt if there are no more events to read!
-        std::optional<std::reference_wrapper<T>> read();
+        std::optional<std::reference_wrapper<const T>> read();
 
         /// EventReader custom iterator.
         class Iterator
         {
         public:
-            Iterator(EventReader<T, M>& reader, std::optional<std::reference_wrapper<T>> ev, bool end);
+            Iterator(EventReader<T, M>& reader, std::optional<std::reference_wrapper<const T>> ev, bool end);
 
-            T& operator*();
+            const T& operator*();
             Iterator& operator++();
             bool operator==(const Iterator& other);
             bool operator!=(const Iterator& other);
 
         private:
             EventReader<T, M>& reader;
-            std::optional<std::reference_wrapper<T>> currentEvent;
+            std::optional<std::reference_wrapper<const T>> currentEvent;
             bool end;
         };
 
@@ -56,16 +56,17 @@ namespace cubos::core::ecs
         static_assert(M != 0, "Invalid mask.");
     }
 
-    template <typename T, unsigned int M> std::optional<std::reference_wrapper<T>> EventReader<T, M>::read()
+    template <typename T, unsigned int M> std::optional<std::reference_wrapper<const T>> EventReader<T, M>::read()
     {
         while (this->index < this->pipe.size())
         {
             std::pair<const T&, unsigned int> p = pipe.get(this->index++);
-            auto event = p.first;
+            auto& event = p.first;
             auto mask = p.second;
             if (matchesMask(mask))
             {
-                return std::optional<std::reference_wrapper<T>>(std::reference_wrapper<T>(event));
+                // return std::optional<std::reference_wrapper<const T>>(std::reference_wrapper<const T>(event));
+                return std::make_optional(std::reference_wrapper<const T>(event));
             }
         }
 
@@ -77,13 +78,13 @@ namespace cubos::core::ecs
         return M == ~0 ? true : eventMask & M;
     }
 
-    template <typename T, unsigned int M> EventReader<T, M>::Iterator EventReader<T, M>::begin()
+    template <typename T, unsigned int M> typename EventReader<T, M>::Iterator EventReader<T, M>::begin()
     {
         // return a new begin iterator only if we did not read all events yet(?)
         return (this->index >= this->pipe.size()) ? this->end() : Iterator(*this, this->read(), false);
     }
 
-    template <typename T, unsigned int M> EventReader<T, M>::Iterator EventReader<T, M>::end()
+    template <typename T, unsigned int M> typename EventReader<T, M>::Iterator EventReader<T, M>::end()
     {
         return Iterator(*this, std::nullopt, true);
     }
@@ -91,17 +92,19 @@ namespace cubos::core::ecs
     // EventReader::Iterator implementation.
 
     template <typename T, unsigned int M>
-    EventReader<T, M>::Iterator::Iterator(EventReader<T, M>& r, std::optional<std::reference_wrapper<T>> ev, bool e)
+    EventReader<T, M>::Iterator::Iterator(EventReader<T, M>& r, std::optional<std::reference_wrapper<const T>> ev,
+                                          bool e)
         : reader(r), currentEvent(ev), end(e)
     {
     }
 
-    template <typename T, unsigned int M> T& EventReader<T, M>::Iterator::operator*()
+    template <typename T, unsigned int M> const T& EventReader<T, M>::Iterator::operator*()
     {
         return currentEvent->get();
     }
 
-    template <typename T, unsigned int M> EventReader<T, M>::Iterator& EventReader<T, M>::Iterator::operator++()
+    template <typename T, unsigned int M>
+    typename EventReader<T, M>::Iterator& EventReader<T, M>::Iterator::operator++()
     {
         currentEvent = reader.read();
         if (currentEvent == std::nullopt)
