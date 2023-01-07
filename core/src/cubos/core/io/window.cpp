23549bf8aa700b8b17f18638070761eb13f67f29
Ricardo Antunes <me@riscadoa.com>
2023-01-07 19:09:14 +0000

    Replace window event callbacks with polling

--- core/src/cubos/core/io/window.cpp
+++ core/src/cubos/core/io/window.cpp
@@ -7,3 +7,29 @@ Window cubos::core::io::openWindow(const std::string& title, const glm::ivec2& s
 {
     return std::make_shared<GLFWWindow>(title, size);
 }
+
+BaseWindow::BaseWindow()
+{
+    this->polled = false;
+}
+
+std::optional<WindowEvent> BaseWindow::pollEvent()
+{
+    // Only poll events after pollEvent() has returned std::nullopt (or if it has never been called).
+    if (!this->polled)
+    {
+        this->pollEvents();
+        this->polled = true;
+    }
+
+    // No more events, we're done for now, but we need to poll again next time.
+    if (this->events.empty())
+    {
+        this->polled = false;
+        return std::nullopt;
+    }
+
+    WindowEvent event = std::move(this->events.front());
+    this->events.pop_front();
+    return event;
+}
