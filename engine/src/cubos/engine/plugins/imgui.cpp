6c637d14eff03aa94fbc50f2f3145a125819fce9
RiscadoA <me@riscadoa.com>
2023-01-22 10:48:19 +0000

    Fix ImGui samples (#304)
    * Send events in the window plugin
    
    * Receive events in the ImGui plugin

--- engine/src/cubos/engine/plugins/imgui.cpp
+++ engine/src/cubos/engine/plugins/imgui.cpp
@@ -2,19 +2,28 @@
 #include <cubos/engine/plugins/window.hpp>
 #include <cubos/core/ui/imgui.hpp>
 
-static void initializeImGui(const cubos::core::io::Window& window)
+using namespace cubos::core;
+
+static void initializeImGui(const io::Window& window)
 {
-    cubos::core::ui::initialize(window);
+    ui::initialize(window);
 }
 
-static void beginImGuiFrame()
+static void beginImGuiFrame(ecs::EventReader<io::WindowEvent> events)
 {
-    cubos::core::ui::beginFrame();
+    // Pass window events to ImGui.
+    // TODO: handleEvent returns a bool indicating if the event was handled or not.
+    //       This will be used to stop propagating the event to other systems when
+    //       the mouse is over an ImGui window.
+    //       Not sure how we will propagate that information to other systems yet.
+    for (auto event : events)
+        ui::handleEvent(event);
+    ui::beginFrame();
 }
 
 static void endImGuiFrame()
 {
-    cubos::core::ui::endFrame();
+    ui::endFrame();
 }
 
 void cubos::engine::plugins::imguiPlugin(Cubos& cubos)
