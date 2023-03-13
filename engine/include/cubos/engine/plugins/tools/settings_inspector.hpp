f475a942256b1882d3131f3adcfcc2f7a72f69bb
Ricardo Antunes <me@riscadoa.com>
2023-03-13 14:39:23 +0000

    docs(engine): update settingsInspector plugin docs to reflect the tags being used

--- engine/include/cubos/engine/plugins/tools/settings_inspector.hpp
+++ engine/include/cubos/engine/plugins/tools/settings_inspector.hpp
@@ -7,7 +7,13 @@ using namespace cubos::engine;
 
 namespace cubos::engine::plugins::tools
 {
-    /// Plugin that allows inspecting the current settings
+    /// Plugin that allows inspecting the current settings through a ImGui window.
+    ///
+    /// @details This plugin adds one system, which adds a ImGui window with the current settings.
+    ///
+    /// Dependencies:
+    /// - `imguiPlugin`
+    //
     /// @param cubos CUBOS. main class
     void settingsInspectorPlugin(Cubos& cubos);
 }; // namespace cubos::engine::plugins::tools
