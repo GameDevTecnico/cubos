dbae878b75afad01de434b3f07fa8ade5705ccd9
Ricardo Antunes <me@riscadoa.com>
2023-03-13 14:38:33 +0000

    docs(engine): update window plugin docs to reflect the tags being used

--- engine/include/cubos/engine/plugins/window.hpp
+++ engine/include/cubos/engine/plugins/window.hpp
@@ -9,15 +9,29 @@ using namespace cubos::engine;
 
 namespace cubos::engine::plugins
 {
-    /// Plugin to create and handle the lifecycle of a window. This is required
-    /// for showing any visual output to the user.
+    /// Plugin to create and handle the lifecycle of a window. This is required for showing any
+    /// visual output to the user.
     ///
-    /// Startup Stages:
-    /// - openWindow: Opens the window.
+    /// @details This plugin adds three systems: one to open the window, one to poll the window
+    /// for events, and one to swap the window's back buffers. Polled events are written to the
+    /// event pipe as `core::io::WindowEvent`s.
     ///
-    /// Stages:
-    /// - poll: Polls the window for events and handles quit requests.
-    /// - swapBuffers: Swaps the window's back buffers.
+    /// Sets `ShouldQuit` to `true` if the window is closed. The window is created with the
+    /// settings `window.width`, `window.height` and `window.title`.
+    ///
+    /// Events:
+    /// - `core::io::WindowEvent`: events polled from the window.
+    ///
+    /// Resources:
+    /// - `core::io::Window`: the window's handle.
+    ///
+    /// Startup tags:
+    /// - `cubos.window.init`: initializes and opens the window, runs after `cubos.settings`.
+    ///
+    /// Tags:
+    /// - `cubos.window.poll`: polls the window for events.
+    /// - `cubos.window.render`: swaps the window's back buffers.
+    ///
     /// @param cubos CUBOS. main class
     void windowPlugin(Cubos& cubos);
 }; // namespace cubos::engine::plugins
