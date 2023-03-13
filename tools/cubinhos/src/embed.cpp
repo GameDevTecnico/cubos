2bc305fd660a280f13de13ccac0b1fba46d2ff91
Ricardo Antunes <me@riscadoa.com>
2023-03-13 20:29:24 +0000

    fix(cubinhos): add missing casts and use correct types

--- tools/cubinhos/src/embed.cpp
+++ tools/cubinhos/src/embed.cpp
@@ -223,7 +223,7 @@ static bool embedFileData(State& state, size_t id)
         {
             char buffer[1024];
             file.read(buffer, sizeof(buffer));
-            for (size_t i = 0; i < file.gcount(); ++i)
+            for (std::streamsize i = 0; i < file.gcount(); ++i)
                 state.out << "0x" << std::hex << static_cast<uint32_t>(buffer[i]) << ", ";
         }
 
