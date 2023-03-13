70a47ce3ec009c3202d99cf2b1f950dd098cfa5c
Ricardo Antunes <me@riscadoa.com>
2023-03-13 14:38:54 +0000

    docs(engine): update transform plugin docs to reflect the tags being used

--- engine/include/cubos/engine/plugins/transform.hpp
+++ engine/include/cubos/engine/plugins/transform.hpp
@@ -3,14 +3,25 @@
 
 #include <cubos/engine/cubos.hpp>
 
-using namespace cubos::engine;
-
 namespace cubos::engine::plugins
 {
-    /// Plugin to create and use transforms.
+    /// Plugin which adds components to represent the transform of an entity.
     ///
-    /// Stages:
-    /// - Transform: Sets the glm matrix for the transforms.
+    /// @details This plugin adds one system, which updates the `LocalToWorld` component of all
+    /// entities with a `LocalTransform` component, taking information from the `Position`,
+    /// `Rotation` and `Scale` components. These three components are optional, and any subset of
+    /// them can be used.
+    ///
+    /// Components:
+    /// - `LocalToWorld`: holds the local to world transform matrix.
+    /// - `Position`: holds the position of the entity.
+    /// - `Rotation`: holds the rotation of the entity.
+    /// - `Scale`: holds the scale of the entity.
+    ///
+    /// Tags:
+    /// - `cubos.transform.update`: updates the `LocalToWorld` components with the information from
+    ///   the `Position`, `Rotation` and `Scale` components.
+    ///
     /// @param cubos CUBOS. main class
     void transformPlugin(Cubos& cubos);
 }; // namespace cubos::engine::plugins
