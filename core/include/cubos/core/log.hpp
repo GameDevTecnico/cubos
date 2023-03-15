d5e7ed15482dc85b1943228a258cc1f5460a5831
Ricardo Antunes <me@riscadoa.com>
2023-03-15 19:02:25 +0000

    refactor(core): disallow logging of serializable types
    The new method of making a type serializable breaks the old code
    Removed it since maybe it bests to serialize those types explicitly anyway

--- core/include/cubos/core/log.hpp
+++ core/include/cubos/core/log.hpp
@@ -1,9 +1,6 @@
 #ifndef CUBOS_CORE_LOG_HPP
 #define CUBOS_CORE_LOG_HPP
 
-#include <cubos/core/memory/buffer_stream.hpp>
-#include <cubos/core/data/debug_serializer.hpp>
-
 #ifndef CUBOS_LOG_LEVEL
 #ifndef NDEBUG
 #define CUBOS_LOG_LEVEL 0
@@ -42,48 +39,5 @@ namespace cubos::core
     /// Initializes the logger, must be called before any logging is done.
     void initializeLogger();
 } // namespace cubos::core
-
-// Implement custom formatting for all trivially serializable types.
-
-/// @cond
-template <typename T>
-requires(cubos::core::data::TriviallySerializable<T> && !std::is_pointer_v<T> && !std::is_array_v<T> &&
-         !std::integral<T> && !std::floating_point<T> && !std::is_same_v<T, std::string>) struct fmt::formatter<T>
-    : formatter<string_view>
-{
-    bool pretty = false; ///< Whether to pretty print the data.
-    bool types = false;  ///< Whether to print the type name.
-
-    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin())
-    {
-        auto it = ctx.begin(), end = ctx.end();
-        while (it != end)
-        {
-            if (*it == 'p')
-                this->pretty = true;
-            else if (*it == 't')
-                this->types = true;
-            else if (*it != '}')
-                throw format_error("invalid format");
-            else
-                break;
-            ++it;
-        }
-        return it;
-    }
-
-    template <typename FormatContext>
-    auto format(const T& val, FormatContext& ctx) -> decltype(ctx.out())
-    {
-        auto stream = cubos::core::memory::BufferStream(32);
-        cubos::core::data::DebugSerializer serializer(stream, this->pretty, this->types);
-        serializer.write(val, nullptr);
-        stream.put('\0');
-        // Skip the '?: ' prefix.
-        auto result = std::string(static_cast<const char*>(stream.getBuffer()) + 3);
-        return formatter<string_view>::format(string_view(result), ctx);
-    }
-};
-/// @endcond
 
 #endif // CUBOS_CORE_LOG_HPP
