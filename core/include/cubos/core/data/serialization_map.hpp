3e4190ba8ec6016e53d6914925b82ca285c11a00
Ricardo Antunes <me@riscadoa.com>
2023-03-15 19:54:54 +0000

    refactor(core): refactor SerializationMap

--- core/include/cubos/core/data/serialization_map.hpp
+++ core/include/cubos/core/data/serialization_map.hpp
@@ -3,6 +3,7 @@
 
 #include <unordered_map>
 #include <functional>
+#include <cassert>
 
 namespace cubos::core::data
 {
@@ -13,46 +14,112 @@ namespace cubos::core::data
     class SerializationMap final
     {
     public:
-        SerializationMap();
         ~SerializationMap() = default;
         SerializationMap(SerializationMap&&) = default;
+
+        inline SerializationMap() : usingFunctions(false)
+        {
+        }
 
         /// Makes the serialization map use functions instead of keeping a map.
         /// @param serialize Function used to serialize references.
         /// @param deserialize Function used to deserialize references.
-        SerializationMap(std::function<bool(const R&, I&)> serialize, std::function<bool(R&, const I&)> deserialize);
+        inline SerializationMap(std::function<bool(const R&, I&)> serialize,
+                                std::function<bool(R&, const I&)> deserialize)
+            : usingFunctions(true), serialize(serialize), deserialize(deserialize)
+        {
+        }
 
         /// Adds a reference <-> serialized identifier pair.
         /// @param reference Reference to add.
         /// @param id Serialized identifier to add.
-        void add(const R& reference, const I& id);
+        inline void add(const R& reference, const I& id)
+        {
+            assert(!this->usingFunctions);
+            this->refToId.insert({reference, id});
+            this->idToRef.insert({id, reference});
+        }
 
         /// Checks if a reference is in the map.
         /// @param reference Reference to check.
         /// @return True if the reference is in the map, false otherwise.
-        bool hasRef(const R& reference) const;
+        inline bool hasRef(const R& reference) const
+        {
+            if (this->usingFunctions)
+            {
+                I i;
+                return this->serialize(reference, i);
+            }
+            else
+            {
+                return this->refToId.find(reference) != this->refToId.end();
+            }
+        }
 
         /// Checks if a serialized identifier is in the map.
         /// @param id Serialized identifier to check.
         /// @return True if the serialized identifier is in the map, false otherwise.
-        bool hasId(const I& id) const;
+        inline bool hasId(const I& id) const
+        {
+            if (this->usingFunctions)
+            {
+                R r;
+                return this->deserialize(r, id);
+            }
+            else
+            {
+                return this->idToRef.find(id) != this->idToRef.end();
+            }
+        }
 
         /// Gets the reference of a serialized identifier.
         /// @param id Serialized identifier to get the reference of.
         /// @returns Reference of the serialized identifier.
-        R getRef(const I& id) const;
+        inline R getRef(const I& id) const
+        {
+            if (this->usingFunctions)
+            {
+                R reference;
+                assert(this->deserialize(reference, id));
+                return reference;
+            }
+            else
+            {
+                return this->idToRef.at(id);
+            }
+        }
 
         /// Gets the serialized identifier of a reference.
         /// @param reference Reference to get the serialized identifier of.
         /// @returns Serialized identifier of the reference.
-        I getId(const R& reference) const;
+        inline I getId(const R& reference) const
+        {
+            if (this->usingFunctions)
+            {
+                I id;
+                assert(this->serialize(reference, id));
+                return id;
+            }
+            else
+            {
+                return this->refToId.at(reference);
+            }
+        }
 
         /// Clears the map.
-        void clear();
+        inline void clear()
+        {
+            this->usingFunctions = false;
+            this->refToId.clear();
+            this->idToRef.clear();
+        }
 
         /// Gets the number of mapped references.
         /// @returns Number of mapped references.
-        size_t size() const;
+        inline size_t size() const
+        {
+            return this->refToId.size();
+        }
 
     private:
         SerializationMap(const SerializationMap&) = delete;
@@ -63,100 +130,6 @@ namespace cubos::core::data
         std::unordered_map<R, I> refToId;              ///< Map of references to serialized IDs.
         std::unordered_map<I, R> idToRef;              ///< Map of serialized IDs to references.
     };
-
-    // Implementation
-
-    template <typename R, typename I>
-    SerializationMap<R, I>::SerializationMap() : usingFunctions(false)
-    {
-    }
-
-    template <typename R, typename I>
-    SerializationMap<R, I>::SerializationMap(std::function<bool(const R&, I&)> serialize,
-                                             std::function<bool(R&, const I&)> deserialize)
-        : usingFunctions(true), serialize(serialize), deserialize(deserialize)
-    {
-    }
-
-    template <typename R, typename I>
-    void SerializationMap<R, I>::add(const R& reference, const I& id)
-    {
-        assert(!this->usingFunctions);
-        this->refToId.insert({reference, id});
-        this->idToRef.insert({id, reference});
-    }
-
-    template <typename R, typename I>
-    bool SerializationMap<R, I>::hasRef(const R& reference) const
-    {
-        if (this->usingFunctions)
-        {
-            I i;
-            return this->serialize(reference, i);
-        }
-        else
-        {
-            return this->refToId.find(reference) != this->refToId.end();
-        }
-    }
-
-    template <typename R, typename I>
-    bool SerializationMap<R, I>::hasId(const I& id) const
-    {
-        if (this->usingFunctions)
-        {
-            R r;
-            return this->deserialize(r, id);
-        }
-        else
-        {
-            return this->idToRef.find(id) != this->idToRef.end();
-        }
-    }
-
-    template <typename R, typename I>
-    R SerializationMap<R, I>::getRef(const I& id) const
-    {
-        if (this->usingFunctions)
-        {
-            R reference;
-            assert(this->deserialize(reference, id));
-            return reference;
-        }
-        else
-        {
-            return this->idToRef.at(id);
-        }
-    }
-
-    template <typename R, typename I>
-    I SerializationMap<R, I>::getId(const R& reference) const
-    {
-        if (this->usingFunctions)
-        {
-            I id;
-            assert(this->serialize(reference, id));
-            return id;
-        }
-        else
-        {
-            return this->refToId.at(reference);
-        }
-    }
-
-    template <typename R, typename I>
-    void SerializationMap<R, I>::clear()
-    {
-        this->usingFunctions = false;
-        this->refToId.clear();
-        this->idToRef.clear();
-    }
-
-    template <typename R, typename I>
-    size_t SerializationMap<R, I>::size() const
-    {
-        return this->refToId.size();
-    }
 } // namespace cubos::core::data
 
 #endif // CUBOS_CORE_DATA_SERIALIZATION_MAP_HPP
