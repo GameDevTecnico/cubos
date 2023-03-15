808cea7cfd4e7b0a3a46d5bfa9c3ef2ed0274774
Ricardo Antunes <me@riscadoa.com>
2023-03-15 19:02:25 +0000

    refactor(core): disallow logging of serializable types
    The new method of making a type serializable breaks the old code
    Removed it since maybe it bests to serialize those types explicitly anyway

--- core/samples/logging.cpp
+++ core/samples/logging.cpp
@@ -15,10 +15,5 @@ int main(void)
     CUBOS_ERROR("Error message");
     CUBOS_CRITICAL("Critical message");
 
-    CUBOS_INFO("Trivially serializable type: {}", glm::vec3(0.0f, 1.0f, 2.0f));
-    CUBOS_INFO("Again, but with type information: {:t}", glm::vec3(0.0f, 1.0f, 2.0f));
-    CUBOS_INFO("Since unordered maps are serializable, we can do this: {}",
-               std::unordered_map<int, const char*>{{1, "one"}, {2, "two"}});
-
     return 0;
 }
