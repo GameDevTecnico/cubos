bdaf43a55bb5f0db5b5ad459a767aa05bd0b5b32
Ricardo Antunes <me@riscadoa.com>
2023-03-16 17:02:08 +0000

    refactor(core): adapt component Storage to new serialization features

--- core/include/cubos/core/ecs/storage.hpp
+++ core/include/cubos/core/ecs/storage.hpp
@@ -1,7 +1,6 @@
 #ifndef CUBOS_CORE_ECS_STORAGE_HPP
 #define CUBOS_CORE_ECS_STORAGE_HPP
 
-#include <cubos/core/data/handle.hpp>
 #include <cubos/core/data/package.hpp>
 #include <cubos/core/data/serialization_map.hpp>
 
@@ -26,19 +25,16 @@ namespace cubos::core::ecs
 
         /// @brief Packages a value. If the value doesn't exist, undefined behavior will occur.
         /// @param index The index of the value to package.
-        /// @param map Serialization map needed for serialization.
+        /// @param context Context used for serialization.
         /// @returns The packaged value.
-        virtual data::Package pack(uint32_t index, const data::SerializationMap<Entity, std::string>& map,
-                                   data::Handle::SerContext handleCtx) const = 0;
+        virtual data::Package pack(uint32_t index, data::Context& context) const = 0;
 
         /// @brief Unpackages a value.
         /// @param index The index of the value to unpackage.
         /// @param package The package to unpackage.
-        /// @param map Serialization map needed for deserialization.
+        /// @param context Context used for deserialization.
         /// @returns True if the value was unpackaged successfully, false otherwise.
-        virtual bool unpack(uint32_t index, const data::Package& package,
-                            const data::SerializationMap<Entity, std::string>& map,
-                            data::Handle::DesContext handleCtx) = 0;
+        virtual bool unpack(uint32_t index, const data::Package& package, data::Context& context) = 0;
 
         /// @brief Gets the type the components being stored here.
         virtual std::type_index type() const = 0;
@@ -68,42 +64,28 @@ namespace cubos::core::ecs
 
         // Implementation.
 
-        virtual data::Package pack(uint32_t index, const data::SerializationMap<Entity, std::string>& map,
-                                   data::Handle::SerContext handleCtx) const override;
-        virtual bool unpack(uint32_t index, const data::Package& package,
-                            const data::SerializationMap<Entity, std::string>& map,
-                            data::Handle::DesContext handleCtx) override;
-        virtual std::type_index type() const override;
-    };
-
-    // Implementation.
-
-    template <typename T>
-    data::Package Storage<T>::pack(uint32_t index, const data::SerializationMap<Entity, std::string>& map,
-                                   data::Handle::SerContext handleCtx) const
-    {
-        return data::Package::from(*this->get(index), std::forward_as_tuple(map, handleCtx));
-    }
-
-    template <typename T>
-    bool Storage<T>::unpack(uint32_t index, const data::Package& package,
-                            const data::SerializationMap<Entity, std::string>& map, data::Handle::DesContext handleCtx)
-    {
-        T value;
-        if (package.into(value, std::forward_as_tuple(map, handleCtx)))
+        inline virtual data::Package pack(uint32_t index, data::Context& context) const override
         {
-            this->insert(index, value);
-            return true;
+            return data::Package::from(*this->get(index), &context);
         }
 
-        return false;
-    }
+        inline virtual bool unpack(uint32_t index, const data::Package& package, data::Context& context) override
+        {
+            T value;
+            if (package.into(value, &context))
+            {
+                this->insert(index, value);
+                return true;
+            }
 
-    template <typename T>
-    std::type_index Storage<T>::type() const
-    {
-        return std::type_index(typeid(T));
-    }
+            return false;
+        }
+
+        inline virtual std::type_index type() const override
+        {
+            return std::type_index(typeid(T));
+        }
+    };
 } // namespace cubos::core::ecs
 
 #endif // ECS_STORAGE_HPP
