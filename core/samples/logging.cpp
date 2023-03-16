acefe1a2ad9fb671a6289fb658800526f90a287c
Ricardo Antunes <me@riscadoa.com>
2023-03-16 09:25:04 +0000

    feat(core): add Debug for logging serializable types

--- core/samples/logging.cpp
+++ core/samples/logging.cpp
@@ -2,7 +2,10 @@
 #include <cubos/core/memory/buffer_stream.hpp>
 #include <cubos/core/data/debug_serializer.hpp>
 
+#include <glm/glm.hpp>
+
 using namespace cubos::core;
+using data::Debug;
 
 int main(void)
 {
@@ -14,6 +17,11 @@ int main(void)
     CUBOS_WARN("Warning message");
     CUBOS_ERROR("Error message");
     CUBOS_CRITICAL("Critical message");
+
+    CUBOS_INFO("Serializable type: {}", Debug(glm::vec3(0.0f, 1.0f, 2.0f)));
+    CUBOS_INFO("Again, but with type information: {:t}", Debug(glm::vec3(0.0f, 1.0f, 2.0f)));
+    CUBOS_INFO("Since unordered maps are serializable, we can do this: {}",
+               Debug((std::unordered_map<int, const char*>{{1, "one"}, {2, "two"}})));
 
     return 0;
 }
