03f511c63df951626514b8e8b6ed4cdd85a6a074
Ricardo Antunes <me@riscadoa.com>
2023-03-17 23:35:37 +0000

    refactor(core): adapt ui/ecs.hpp to new serialization interface

--- core/src/cubos/core/ui/ecs.cpp
+++ core/src/cubos/core/ui/ecs.cpp
@@ -3,28 +3,27 @@
 
 #include <imgui.h>
 
-void cubos::core::ui::showWorld(const ecs::World& world, data::Handle::SerContext handleCtx)
+void cubos::core::ui::showWorld(const ecs::World& world, data::Context& context)
 {
     ImGui::BeginTable("showWorld", 2, ImGuiTableFlags_BordersOuter | ImGuiTableFlags_Resizable);
 
     for (auto entity : world)
     {
-        auto pkg = world.pack(entity, handleCtx);
+        auto pkg = world.pack(entity, context);
         ui::showPackage(pkg, std::to_string(entity.index));
     }
     ImGui::EndTable();
 }
 
-void cubos::core::ui::editWorld(ecs::World& world, data::Handle::SerContext handleSerCtx,
-                                data::Handle::DesContext handleDesCtx)
+void cubos::core::ui::editWorld(ecs::World& world, data::Context& serContext, data::Context& desContext)
 {
     ImGui::BeginTable("editWorld", 3, ImGuiTableFlags_BordersOuter | ImGuiTableFlags_Resizable);
     for (auto entity : world)
     {
-        auto pkg = world.pack(entity, handleSerCtx);
+        auto pkg = world.pack(entity, serContext);
         if (ui::editPackage(pkg, std::to_string(entity.index)))
         {
-            world.unpack(entity, pkg, handleDesCtx);
+            world.unpack(entity, pkg, desContext);
         }
     }
     ImGui::EndTable();
