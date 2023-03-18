f82afd6c77de250a3d3c60108c9bf423b6f4e9dd
Diogo Mendonça <diogo.cunhamen@gmail.com>
2023-03-18 21:05:57 +0000

    Fixed nitpicks

--- engine/include/cubos/engine/plugins/tools/entity_selector.hpp
+++ engine/include/cubos/engine/plugins/tools/entity_selector.hpp
@@ -2,16 +2,17 @@
 #define CUBOS_ENGINE_PLUGINS_ENTITY_SELECTOR_HPP
 
 #include <cubos/engine/cubos.hpp>
-#include <cubos/core/ecs/entity_manager.hpp>
 
-using namespace cubos::engine;
+#include <cubos/core/ecs/entity_manager.hpp>
 
 namespace cubos::engine::plugins::tools
 {
+    /// Resource used to identify the currently selected entity. Used by multiple editor plugins.
     struct EntitySelector
     {
         cubos::core::ecs::Entity selection;
     };
+
     /// Plugin that allows having a singular entity be selected for inspection
     /// @param cubos CUBOS. main class
     void entitySelectorPlugin(Cubos& cubos);
