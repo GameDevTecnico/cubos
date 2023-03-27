e3e5eca5cb6bfb0c534e32157d0bf2c2d0067ef8
Ricardo Antunes <me@riscadoa.com>
2023-03-27 14:26:01 +0100

    feat(engine): register bridge in scenePlugin

--- engine/src/cubos/engine/scene/plugin.cpp
+++ engine/src/cubos/engine/scene/plugin.cpp
@@ -1,10 +1,19 @@
 #include <cubos/engine/scene/plugin.hpp>
 
 #include <cubos/engine/assets/plugin.hpp>
+#include <cubos/engine/scene/bridge.hpp>
 
 using namespace cubos::engine;
+
+static void bridge(Assets& assets)
+{
+    // Add the bridge to load .scn files.
+    assets.registerBridge(".scn", std::make_unique<SceneBridge>());
+}
 
 void cubos::engine::scenePlugin(Cubos& cubos)
 {
     cubos.addPlugin(assetsPlugin);
+
+    cubos.startupSystem(bridge).tagged("cubos.assets.bridge");
 }
