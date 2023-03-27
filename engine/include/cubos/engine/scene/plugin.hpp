664adbed4e8da6232fb13305f64b98c8584ffbc7
Ricardo Antunes <me@riscadoa.com>
2023-03-27 14:26:01 +0100

    feat(engine): register bridge in scenePlugin

--- engine/include/cubos/engine/scene/plugin.hpp
+++ engine/include/cubos/engine/scene/plugin.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <cubos/engine/cubos.hpp>
+#include <cubos/engine/scene/scene.hpp>
 
 namespace cubos::engine
 {
     /// Plugin which adds scenes to CUBOS. Scenes are the asset equivalent to ECS blueprints.
     ///
-    /// @details Registers the `SceneBridge` asset bridge with the `.scn` extension.
+    /// @details Registers the `SceneBridge` asset bridge with the `.scn` extension. Loads assets
+    /// of the `Scene` type.
     ///
     /// @param cubos CUBOS. main class.
     void scenePlugin(Cubos& cubos);
