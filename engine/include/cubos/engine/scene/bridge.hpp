55034ca421bf49ef672a13a468e1f4ecc7a4970d
Ricardo Antunes <me@riscadoa.com>
2023-03-27 14:25:36 +0100

    feat(engine): add SceneBridge

---
+++ engine/include/cubos/engine/scene/bridge.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cubos/engine/assets/bridge.hpp>
+
+namespace cubos::engine
+{
+    /// A bridge to load and save `Scene` assets.
+    ///
+    /// @details Scenes are stored as JSON files with two top-level objects:
+    /// - "imports" - a dictionary of names to asset IDs of other scenes to import.
+    /// - "entities" - a dictionary of names to serialized entities.
+    ///
+    /// Entities imported from sub-scenes can be overriden by defining them in the
+    /// current scene. For example:
+    ///
+    ///     {
+    ///         "imports": {
+    ///             "foo": "6f42ae5a-59d1-5df3-8720-83b8df6dd536"
+    ///         },
+    ///         "entities": {
+    ///             "baz": {
+    ///                 "cubos/position": {
+    ///                     "x": 1,
+    ///                     "y": 2,
+    ///                     "z": 3
+    ///                 }
+    ///             },
+    ///             "foo.bar": {
+    ///                 "cubos/parent": "baz",
+    ///             }
+    ///         }
+    ///     }
+    ///
+    /// This scene will import the scene with ID "6f42ae5a-59d1-5df3-8720-83b8df6dd536", and prefix
+    /// all of its entities with "foo.". The entity "foo.bar" will override the entity "bar" from
+    /// the imported scene, while the entity "baz" will be added to the scene.
+    class SceneBridge : public AssetBridge
+    {
+    public:
+        virtual bool load(Assets& assets, AnyAsset handle) override;
+        virtual bool save(const Assets& assets, AnyAsset handle) override;
+    };
+} // namespace cubos::engine
