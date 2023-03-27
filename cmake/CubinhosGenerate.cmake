3ca3add0b036ba41f681102644b438ad87250ea3
Ricardo Antunes <me@riscadoa.com>
2023-03-27 20:04:12 +0100

    feat(cubinhos): make generate create a source file instead of include files

--- cmake/CubinhosGenerate.cmake
+++ cmake/CubinhosGenerate.cmake
@@ -3,14 +3,14 @@ function(cubinhos_generate target source_dir)
     get_target_property(binary_dir ${target} BINARY_DIR)
 
     add_custom_command(
-        OUTPUT ${binary_dir}/${target}-include
-        COMMAND $<TARGET_FILE:cubinhos> generate ${source_dir} ${binary_dir}/${target}-include/components
+        OUTPUT ${binary_dir}/${target}-components.cpp
+        COMMAND $<TARGET_FILE:cubinhos> generate ${source_dir} ${binary_dir}/${target}-components.cpp
         DEPENDS $<TARGET_FILE:cubinhos> ${source_dir}
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
 
-    add_custom_target(${target}-generate DEPENDS ${binary_dir}/${target}-include)
+    add_custom_target(${target}-generate DEPENDS ${binary_dir}/${target}-components.cpp)
     add_dependencies(${target} ${target}-generate)
 
-    target_include_directories(${target} PUBLIC ${binary_dir}/${target}-include)
+    target_sources(${target} PRIVATE ${binary_dir}/${target}-components.cpp)
 endfunction()    
