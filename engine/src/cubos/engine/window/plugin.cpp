a1bae2fe31d1f158125bba37ffc676bd8e10c64d
Luís Fonseca <luis@lhf.pt>
2023-05-06 18:09:12 +0100

    refactor(engine): remove using namespace cubos::core from window plugin

--- engine/src/cubos/engine/window/plugin.cpp
+++ engine/src/cubos/engine/window/plugin.cpp
@@ -2,17 +2,21 @@
 
 #include <cubos/engine/window/plugin.hpp>
 
-using namespace cubos::core;
+using cubos::core::Settings;
+using cubos::core::ecs::EventWriter;
+using cubos::core::io::openWindow;
+using cubos::core::io::Window;
+using cubos::core::io::WindowEvent;
 using namespace cubos::engine;
 
-static void init(io::Window& window, ShouldQuit& quit, const Settings& settings)
+static void init(Window& window, ShouldQuit& quit, const Settings& settings)
 {
     quit.value = false;
-    window = io::openWindow(settings.getString("window.title", "Cubos"),
-                            {settings.getInteger("window.width", 800), settings.getInteger("window.height", 600)});
+    window = openWindow(settings.getString("window.title", "Cubos"),
+                        {settings.getInteger("window.width", 800), settings.getInteger("window.height", 600)});
 }
 
-static void poll(const io::Window& window, ShouldQuit& quit, ecs::EventWriter<io::WindowEvent> events)
+static void poll(const Window& window, ShouldQuit& quit, EventWriter<WindowEvent> events)
 {
     // Send window events to other systems.
     while (auto event = window->pollEvent())
@@ -26,15 +30,15 @@ static void poll(const io::Window& window, ShouldQuit& quit, ecs::EventWriter<io
     }
 }
 
-static void render(const io::Window& window)
+static void render(const Window& window)
 {
     window->swapBuffers();
 }
 
 void cubos::engine::windowPlugin(Cubos& cubos)
 {
-    cubos.addResource<io::Window>();
-    cubos.addEvent<io::WindowEvent>();
+    cubos.addResource<Window>();
+    cubos.addEvent<WindowEvent>();
 
     cubos.startupTag("cubos.window.init").afterTag("cubos.settings");
     cubos.tag("cubos.window.poll").beforeTag("cubos.window.render");
