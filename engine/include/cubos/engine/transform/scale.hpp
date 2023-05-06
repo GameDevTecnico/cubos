38db1a77043d5965765530f3d9b3da79dece41f1
Luís Fonseca <luis@lhf.pt>
2023-05-06 15:35:53 +0100

    refactor(engine): move transform related code to transform plugin folder

--- engine/include/cubos/engine/transform/scale.hpp
+++ engine/include/cubos/engine/transform/scale.hpp
@@ -2,11 +2,11 @@
 
 #include <glm/glm.hpp>
 
-namespace cubos::engine::ecs
+namespace cubos::engine
 {
     /// Assigns a uniform scale to an entity.
     struct [[cubos::component("cubos/scale", VecStorage)]] Scale
     {
         float factor; ///< The scale factor of the entity.
     };
-} // namespace cubos::engine::ecs
+} // namespace cubos::engine
