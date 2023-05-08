f473dc0aaba3459be20d68dd7279ca73771b15e9
Ricardo Antunes <me@riscadoa.com>
2023-05-08 10:36:49 +0100

    feat(engine): add PaletteBridge

--- engine/src/cubos/engine/voxels/plugin.cpp
+++ engine/src/cubos/engine/voxels/plugin.cpp
@@ -1,5 +1,6 @@
 #include <cubos/engine/assets/plugin.hpp>
 #include <cubos/engine/voxels/grid_bridge.hpp>
+#include <cubos/engine/voxels/palette_bridge.hpp>
 #include <cubos/engine/voxels/plugin.hpp>
 
 using cubos::core::ecs::Write;
@@ -9,6 +10,7 @@ static void bridges(Write<Assets> assets)
 {
     // Add the bridges to load .grd and .pal files.
     assets->registerBridge(".grd", std::make_unique<GridBridge>());
+    assets->registerBridge(".pal", std::make_unique<PaletteBridge>());
 }
 
 void cubos::engine::voxelsPlugin(Cubos& cubos)
