d44369fae2676ac5c975abbc67301dc1cb7bba99
Ricardo Antunes <me@riscadoa.com>
2023-05-22 16:53:12 +0100

    refactor(engine): update entity and world inspectors

--- engine/src/cubos/engine/tools/entity_inspector/plugin.cpp
+++ engine/src/cubos/engine/tools/entity_inspector/plugin.cpp
@@ -1,29 +1,41 @@
-#include <cubos/engine/tools/entity_inspector/plugin.hpp>
 #include <imgui.h>
 
 #include <cubos/core/ui/serialization.hpp>
 
-#include <cubos/engine/plugins/imgui.hpp>
+#include <cubos/engine/imgui/plugin.hpp>
+#include <cubos/engine/tools/entity_inspector/plugin.hpp>
+#include <cubos/engine/tools/entity_selector/plugin.hpp>
 
-#include <cubos/engine/plugins/tools/entity_selector.hpp>
-
+using cubos::core::data::Context;
+using cubos::core::data::SerializationMap;
+using cubos::core::ecs::Entity;
 using cubos::core::ecs::World;
-using cubos::engine::plugins::tools::EntitySelector;
+using cubos::core::ecs::Write;
+using cubos::core::ui::showPackage;
+using namespace cubos::engine;
 
-static void inspectEntity(EntitySelector& selector, World& world)
+static void inspectEntity(Write<World> world)
 {
+    auto selection = world->read<tools::EntitySelector>().get().selection;
+
     ImGui::Begin("Entity Inspector");
     if (!ImGui::IsWindowCollapsed())
     {
         ImGui::BeginTable("showEntity", 2, ImGuiTableFlags_BordersOuter | ImGuiTableFlags_Resizable);
 
-        if (!selector.selection.isNull())
+        if (!selection.isNull())
         {
-            std::vector<std::string> entityNamesToRemove;
-            auto pkg = world.pack(selector.selection,
-                                  [](cubos::core::data::Serializer& ser, const cubos::core::data::Handle& handle,
-                                     const char* name) { ser.write(handle.getId(), name); });
-            cubos::core::ui::showPackage(pkg, std::to_string(selector.selection.index));
+            // Setup a context with a serialization map which simply converts the entity indices to a string.
+            // TODO: maybe there should be a global serialization map for this, since its such a common behaviour?
+            Context context{};
+            context.push(SerializationMap<Entity, std::string>{[](const Entity& entity, std::string& string) {
+                                                                   string = std::to_string(entity.index);
+                                                                   return true;
+                                                               },
+                                                               [](Entity&, const std::string&) { return false; }});
+
+            auto pkg = world->pack(selection, context);
+            showPackage(pkg, std::to_string(selection.index));
         }
         ImGui::EndTable();
     }
@@ -32,8 +44,8 @@ static void inspectEntity(EntitySelector& selector, World& world)
 
 void cubos::engine::tools::entityInspectorPlugin(Cubos& cubos)
 {
-    cubos.addPlugin(cubos::engine::plugins::imguiPlugin);
-    cubos.addPlugin(cubos::engine::plugins::tools::entitySelectorPlugin);
+    cubos.addPlugin(imguiPlugin);
+    cubos.addPlugin(tools::entitySelectorPlugin);
 
     cubos.system(inspectEntity).tagged("cubos.imgui");
 }
