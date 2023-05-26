ec81f55a1428ab5f370f1084eb0c69a5309cc941
Ricardo Antunes <me@riscadoa.com>
2023-05-26 10:59:17 +0100

    refactor(engine): change BinaryBridge to use FileBridge

--- engine/include/cubos/engine/assets/bridges/binary.hpp
+++ engine/include/cubos/engine/assets/bridges/binary.hpp
@@ -1,37 +1,42 @@
 #pragma once
 
-#include <cubos/core/data/json_deserializer.hpp>
-#include <cubos/core/data/json_serializer.hpp>
+#include <cubos/core/data/binary_deserializer.hpp>
+#include <cubos/core/data/binary_serializer.hpp>
 #include <cubos/core/log.hpp>
 
 #include <cubos/engine/assets/bridges/file.hpp>
 
 namespace cubos::engine
 {
-    /// A bridge for loading and saving assets which are serialized to and from a JSON file.
+    /// A bridge for loading and saving assets which are serialized to and from a binary file.
     ///
     /// @details This bridge automatically serializes and deserializes assets of type `T` to and
-    /// from a JSON file. Thus, `T` must be serializable and deserializable. No additional context
-    /// is given to the serializer or deserializer.
+    /// from a binary file. Thus, `T` must be serializable and deserializable. No additional
+    /// context is given to the serializer or deserializer.
     ///
     /// @tparam T The type of asset to load and save. Must be default constructible.
     template <typename T>
-    class JSONBridge : public FileBridge
+    class BinaryBridge : public FileBridge
     {
+    public:
+        /// @param littleEndian Whether to use little endian byte order.
+        BinaryBridge(bool littleEndian = true)
+            : mLittleEndian{littleEndian}
+        {
+        }
+
     protected:
         bool loadFromFile(Assets& assets, const AnyAsset& handle, core::memory::Stream& stream) override
         {
-            // Dump the file stream into a string and initialize a JSON deserializer with it.
-            std::string json{};
-            stream.readUntil(json, nullptr);
-            core::data::JSONDeserializer deserializer{json};
+            // Initialize a binary deserializer with the file stream.
+            core::data::BinaryDeserializer deserializer{stream, mLittleEndian};
 
             // Deserialize the asset and store it in the asset manager.
             T data{};
             deserializer.read(data);
             if (deserializer.failed())
             {
-                CUBOS_ERROR("Could not deserialize JSON file '{}'", path);
+                CUBOS_ERROR("Could not deserialize binary file '{}'", path);
                 return false;
             }
 
@@ -41,19 +46,22 @@ namespace cubos::engine
 
         bool saveToFile(const Assets& assets, const AnyAsset& handle, core::memory::Stream& stream) override
         {
-            // Initialize a JSON serializer with the file stream.
-            core::data::JSONSerializer serializer{*stream};
+            // Initialize a binary serializer with the file stream.
+            core::data::BinarySerializer serializer{*stream, mLittleEndian};
 
             // Read the asset from the asset manager and serialize it to the file stream.
             auto data = assets.read<T>(handle);
-            serializer.write(*data, nullptr); // JSON serializer does not use names.
+            serializer.write(*data, nullptr); // Binary serializer does not use names.
             if (serializer.failed())
             {
-                CUBOS_ERROR("Could not serialize asset to JSON file '{}'", path);
+                CUBOS_ERROR("Could not serialize asset to binary file '{}'", path);
                 return false;
             }
 
             return true;
         }
+
+    private:
+        bool mLittleEndian;
     };
 } // namespace cubos::engine
--- engine/src/cubos/engine/voxels/plugin.cpp
+++ engine/src/cubos/engine/voxels/plugin.cpp
@@ -1,16 +1,20 @@
+#include <cubos/core/gl/grid.hpp>
+#include <cubos/core/gl/palette.hpp>
+
+#include <cubos/engine/assets/bridges/binary.hpp>
 #include <cubos/engine/assets/plugin.hpp>
-#include <cubos/engine/voxels/grid_bridge.hpp>
-#include <cubos/engine/voxels/palette_bridge.hpp>
 #include <cubos/engine/voxels/plugin.hpp>
 
 using cubos::core::ecs::Write;
+using cubos::core::gl::Grid;
+using cubos::core::gl::Palette;
 using namespace cubos::engine;
 
 static void bridges(Write<Assets> assets)
 {
     // Add the bridges to load .grd and .pal files.
-    assets->registerBridge(".grd", std::make_unique<GridBridge>());
-    assets->registerBridge(".pal", std::make_unique<PaletteBridge>());
+    assets->registerBridge(".grd", std::make_unique<BinaryBridge<Grid>>());
+    assets->registerBridge(".pal", std::make_unique<BinaryBridge<Palette>>());
 }
 
 void cubos::engine::voxelsPlugin(Cubos& cubos)
