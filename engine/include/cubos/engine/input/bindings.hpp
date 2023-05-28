87ea65b76cec3d929a7fc9408c3159da299fdd94
Luís Fonseca <luis@lhf.pt>
2023-05-28 13:31:43 +0100

    feat(engine): add Action class

--- engine/include/cubos/engine/input/bindings.hpp
+++ engine/include/cubos/engine/input/bindings.hpp
@@ -65,22 +65,42 @@ namespace cubos::engine
         std::vector<KeyWithModifier> mNegative;
     };
 
+    class Action final
+    {
+    public:
+        Action() = default;
+        Action(std::vector<KeyWithModifier> keys)
+            : mKeys(keys)
+        {
+        }
+
+        ~Action() = default;
+
+        const std::vector<KeyWithModifier>& getKeys() const;
+        std::vector<KeyWithModifier>& getKeys();
+
+        std::string toString() const;
+
+    private:
+        std::vector<KeyWithModifier> mKeys;
+    };
+
     class Bindings final
     {
     public:
         Bindings() = default;
         ~Bindings() = default;
 
-        const std::unordered_map<std::string, std::vector<KeyWithModifier>>& getActions() const;
+        const std::unordered_map<std::string, Action>& getActions() const;
         const std::unordered_map<std::string, Axis>& getAxes() const;
 
-        std::unordered_map<std::string, std::vector<KeyWithModifier>>& getActions();
+        std::unordered_map<std::string, Action>& getActions();
         std::unordered_map<std::string, Axis>& getAxes();
 
         std::string toString() const;
 
     private:
-        std::unordered_map<std::string, std::vector<KeyWithModifier>> mActions;
+        std::unordered_map<std::string, Action> mActions;
         std::unordered_map<std::string, Axis> mAxes;
     };
 } // namespace cubos::engine
