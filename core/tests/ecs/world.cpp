2be3c7fd94b71e3f7f42af47874a01efbb52177b
Ricardo Antunes <me@riscadoa.com>
2023-05-28 09:56:36 +0100

    test(core): test destructors and resources on ecs::World

--- core/tests/ecs/world.cpp
+++ core/tests/ecs/world.cpp
@@ -40,20 +40,25 @@ TEST_CASE("ecs::World")
 
     SUBCASE("add and remove components")
     {
-        // Create an entity with an integer component.
-        auto foo = world.create(IntegerComponent{0});
-        CHECK(world.has<IntegerComponent>(foo));
+        bool destroyed = false;
+
+        // Create an entity with an detect destructor component.
+        auto foo = world.create(DetectDestructorComponent{{&destroyed}});
+        CHECK(world.has<DetectDestructorComponent>(foo));
         CHECK_FALSE(world.has<ParentComponent>(foo));
+        CHECK_FALSE(destroyed);
 
         // Add a parent component.
         world.add(foo, ParentComponent{});
-        CHECK(world.has<IntegerComponent>(foo));
+        CHECK(world.has<DetectDestructorComponent>(foo));
         CHECK(world.has<ParentComponent>(foo));
+        CHECK_FALSE(destroyed);
 
-        // Remove the integer component.
-        world.remove<IntegerComponent>(foo);
-        CHECK_FALSE(world.has<IntegerComponent>(foo));
+        // Remove the detect destructor component.
+        world.remove<DetectDestructorComponent>(foo);
+        CHECK_FALSE(world.has<DetectDestructorComponent>(foo));
         CHECK(world.has<ParentComponent>(foo));
+        CHECK(destroyed);
     }
 
     SUBCASE("add and remove components through pack and unpack")
@@ -106,5 +111,67 @@ TEST_CASE("ecs::World")
         CHECK(pkg.fields().size() == 2);
         CHECK(pkg.field("integer").get<int>() == 1);
         CHECK(pkg.field("parent").get<Entity>() == bar);
+    }
+
+    SUBCASE("components are correctly destructed when their entity is destroyed")
+    {
+        bool destroyed = false;
+
+        // Create an entity and add a detect destructor component.
+        auto foo = world.create();
+        world.add(foo, DetectDestructorComponent{{&destroyed}});
+        CHECK_FALSE(destroyed);
+
+        // Destroy the entity.
+        world.destroy(foo);
+        CHECK(destroyed);
+    }
+
+    SUBCASE("components are correctly destructed when the world is destroyed")
+    {
+        bool destroyed = false;
+
+        // Create a world with an entity and a component and immediately destroy it.
+        {
+            World world{};
+            setupWorld(world);
+            world.create(DetectDestructorComponent{{&destroyed}});
+            CHECK_FALSE(destroyed);
+        }
+
+        CHECK(destroyed);
+    }
+
+    SUBCASE("read and write resources")
+    {
+        // Register some resources.
+        world.registerResource<int>(42);
+        world.registerResource<char>('x');
+
+        // Check if the read values are correct.
+        CHECK(world.read<int>().get() == 42);
+        CHECK(world.read<char>().get() == 'x');
+
+        // Write new values.
+        world.write<int>().get() = 43;
+        world.write<char>().get() = 'y';
+
+        // Check if the read values are correct.
+        CHECK(world.read<int>().get() == 43);
+        CHECK(world.read<char>().get() == 'y');
+    }
+
+    SUBCASE("resources are correctly destructed when the world is destroyed")
+    {
+        bool destroyed = false;
+
+        // Create a world with a resource and immediately destroy it.
+        {
+            World world{};
+            world.registerResource<DetectDestructor>(&destroyed);
+            CHECK_FALSE(destroyed);
+        }
+
+        CHECK(destroyed);
     }
 }
