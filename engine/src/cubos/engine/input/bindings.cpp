66978786520e39830f2ad534414d466fc27d73a7
Luís Fonseca <luis@lhf.pt>
2023-05-29 09:48:42 +0100

    refactor(engine): move classes to their files

--- engine/src/cubos/engine/input/bindings.cpp
+++ engine/src/cubos/engine/input/bindings.cpp
@@ -1,92 +1,9 @@
 #include <cubos/core/data/deserializer.hpp>
 #include <cubos/core/data/serializer.hpp>
-#include <cubos/core/log.hpp>
 
 #include <cubos/engine/input/bindings.hpp>
 
-using cubos::core::data::Deserializer;
-using cubos::core::data::Serializer;
-using cubos::core::io::Key;
-using cubos::core::io::keyToString;
-using cubos::core::io::Modifiers;
-using cubos::core::io::modifiersToString;
-using cubos::core::io::stringToKey;
-using cubos::core::io::stringToModifiers;
 using namespace cubos::engine;
-
-const Key& KeyWithModifiers::key() const
-{
-    return mKey;
-}
-
-const Modifiers& KeyWithModifiers::modifiers() const
-{
-    return mModifiers;
-}
-
-Key& KeyWithModifiers::key()
-{
-    return mKey;
-}
-
-Modifiers& KeyWithModifiers::modifiers()
-{
-    return mModifiers;
-}
-
-const std::vector<KeyWithModifiers>& InputAxis::positive() const
-{
-    return mPositive;
-}
-
-const std::vector<KeyWithModifiers>& InputAxis::negative() const
-{
-    return mNegative;
-}
-
-std::vector<KeyWithModifiers>& InputAxis::positive()
-{
-    return mPositive;
-}
-
-std::vector<KeyWithModifiers>& InputAxis::negative()
-{
-    return mNegative;
-}
-
-float InputAxis::value() const
-{
-    return mValue;
-}
-
-void InputAxis::value(float value)
-{
-    if (std::abs(value) > 1.0f)
-    {
-        CUBOS_WARN("Axis value out of range: {}", value);
-    }
-    mValue = std::clamp(value, -1.0f, 1.0f);
-}
-
-const std::vector<KeyWithModifiers>& InputAction::keys() const
-{
-    return mKeys;
-}
-
-std::vector<KeyWithModifiers>& InputAction::keys()
-{
-    return mKeys;
-}
-
-bool InputAction::pressed() const
-{
-    return mPressed;
-}
-
-void InputAction::pressed(bool pressed)
-{
-    mPressed = pressed;
-}
 
 const std::unordered_map<std::string, InputAction>& InputBindings::actions() const
 {
@@ -171,27 +88,4 @@ void cubos::core::data::deserialize<InputBindings>(Deserializer& des, InputBindi
     des.endDictionary();
 
     des.endObject();
-}
-
-template <>
-void cubos::core::data::serialize<KeyWithModifiers>(Serializer& ser, const KeyWithModifiers& obj, const char* name)
-{
-    ser.write(modifiersToString(obj.modifiers()) + keyToString(obj.key()), name);
-}
-
-template <>
-void cubos::core::data::deserialize<KeyWithModifiers>(Deserializer& des, KeyWithModifiers& obj)
-{
-    std::string str;
-    des.readString(str);
-
-    std::size_t split = str.find_last_of('-');
-    if (split == std::string::npos)
-    {
-        obj = KeyWithModifiers(stringToKey(str), Modifiers::None);
-    }
-    else
-    {
-        obj = KeyWithModifiers(stringToKey(str.substr(split + 1)), stringToModifiers(str.substr(0, split + 1)));
-    }
 }
