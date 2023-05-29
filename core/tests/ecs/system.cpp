6db312d2b7f60ee5b9bdd8fc0fe86f2ce587572e
Ricardo Antunes <me@riscadoa.com>
2023-05-29 09:25:53 +0100

    test(core): cover ecs::SystemWrapper

--- core/tests/ecs/system.cpp
+++ core/tests/ecs/system.cpp
@@ -7,8 +7,15 @@
 
 #include "utils.hpp"
 
+using cubos::core::ecs::CommandBuffer;
+using cubos::core::ecs::Commands;
+using cubos::core::ecs::Entity;
+using cubos::core::ecs::Query;
+using cubos::core::ecs::Read;
 using cubos::core::ecs::SystemInfo;
 using cubos::core::ecs::SystemWrapper;
+using cubos::core::ecs::World;
+using cubos::core::ecs::Write;
 
 TEST_CASE("ecs::SystemInfo")
 {
@@ -180,5 +187,165 @@ TEST_CASE("ecs::SystemInfo")
 
         CHECK_FALSE(infoA.compatible(infoB));
         CHECK_FALSE(infoB.compatible(infoA));
+    }
+}
+
+// Some system functions which do nothing, used to test if the wrapper extracts the correct system
+// information.
+
+static void accessNothing()
+{
+}
+
+static void accessResources(Read<int>, Write<double>, Read<float>)
+{
+}
+
+static void accessComponents(Query<Read<int>, Write<double>>, Query<Read<int>, Read<float>>)
+{
+}
+
+static void accessCommands(Commands)
+{
+}
+
+static void accessWriteWorld(Write<World>)
+{
+}
+
+static void accessReadWorld(Read<World>)
+{
+}
+
+/// Utility used to run a system function with the given world.
+template <typename F>
+static void runSystem(World& world, CommandBuffer& cmdBuf, F f)
+{
+    SystemWrapper<F> wrapper{f};
+    wrapper.prepare(world);
+    wrapper.call(world, cmdBuf);
+}
+
+TEST_CASE("ecs::SystemWrapper")
+{
+    SUBCASE("Wrapper extracts the system information correctly")
+    {
+        SUBCASE("System does not access anything")
+        {
+            auto info = SystemWrapper(accessNothing).info();
+            CHECK(info.componentsRead.empty());
+            CHECK(info.componentsWritten.empty());
+            CHECK(info.resourcesRead.empty());
+            CHECK(info.resourcesWritten.empty());
+            CHECK_FALSE(info.usesCommands);
+            CHECK_FALSE(info.usesWorld);
+        }
+
+        SUBCASE("System accesses resources")
+        {
+            auto info = SystemWrapper(accessResources).info();
+            CHECK(info.componentsRead.empty());
+            CHECK(info.componentsWritten.empty());
+            CHECK(info.resourcesRead.size() == 2);
+            CHECK(info.resourcesWritten.size() == 1);
+            CHECK(info.resourcesRead.contains(typeid(int)));
+            CHECK(info.resourcesRead.contains(typeid(float)));
+            CHECK(info.resourcesWritten.contains(typeid(double)));
+            CHECK_FALSE(info.usesCommands);
+            CHECK_FALSE(info.usesWorld);
+        }
+
+        SUBCASE("System accesses components")
+        {
+            auto info = SystemWrapper(accessComponents).info();
+            CHECK(info.componentsRead.size() == 2);
+            CHECK(info.componentsWritten.size() == 1);
+            CHECK(info.componentsRead.contains(typeid(int)));
+            CHECK(info.componentsRead.contains(typeid(float)));
+            CHECK(info.componentsWritten.contains(typeid(double)));
+            CHECK(info.resourcesRead.empty());
+            CHECK(info.resourcesWritten.empty());
+            CHECK_FALSE(info.usesCommands);
+            CHECK_FALSE(info.usesWorld);
+        }
+
+        SUBCASE("System accesses commands")
+        {
+            auto info = SystemWrapper(accessCommands).info();
+            CHECK(info.componentsRead.empty());
+            CHECK(info.componentsWritten.empty());
+            CHECK(info.resourcesRead.empty());
+            CHECK(info.resourcesWritten.empty());
+            CHECK(info.usesCommands);
+            CHECK_FALSE(info.usesWorld);
+        }
+
+        SUBCASE("System accesses the world directly")
+        {
+            SystemInfo info{};
+
+            SUBCASE("System reads the world")
+            {
+                info = SystemWrapper(accessReadWorld).info();
+            }
+
+            SUBCASE("System writes to the world")
+            {
+                info = SystemWrapper(accessWriteWorld).info();
+            }
+
+            CHECK(info.componentsRead.empty());
+            CHECK(info.componentsWritten.empty());
+            CHECK(info.resourcesRead.empty());
+            CHECK(info.resourcesWritten.empty());
+            CHECK_FALSE(info.usesCommands);
+            CHECK(info.usesWorld);
+        }
+    }
+
+    SUBCASE("Wrapper calls the system correctly")
+    {
+        World world{};
+        CommandBuffer cmdBuf{world};
+
+        SUBCASE("Systems read and write to a resource")
+        {
+            world.registerResource<int>(0);
+            runSystem(world, cmdBuf, [](Read<int> res) { CHECK(*res == 0); });
+            runSystem(world, cmdBuf, [](Write<int> res) { *res = 1; });
+            runSystem(world, cmdBuf, [](Read<int> res) { CHECK(*res == 1); });
+        }
+
+        SUBCASE("Systems spawn an entity and read/write from/to its component")
+        {
+            setupWorld(world);
+
+            // Spawn entity with no components.
+            Entity ent;
+            runSystem(world, cmdBuf, [&](Commands cmd) { ent = cmd.create().entity(); });
+            runSystem(world, cmdBuf, [&](Query<Write<IntegerComponent>> q) { CHECK_FALSE(q[ent].has_value()); });
+
+            // Add component.
+            runSystem(world, cmdBuf, [&](Commands cmd) { cmd.add(ent, IntegerComponent{0}); });
+
+            // Commands have not been committed yet, so the component should not be readable.
+            runSystem(world, cmdBuf, [&](Query<Write<IntegerComponent>> q) { CHECK_FALSE(q[ent].has_value()); });
+
+            cmdBuf.commit();
+
+            // Write to it.
+            runSystem(world, cmdBuf, [&](Query<Write<IntegerComponent>> q) {
+                REQUIRE(q[ent].has_value());
+                auto [comp] = *q[ent];
+                CHECK(comp->value == 0);
+                comp->value = 1;
+            });
+
+            // Read from it.
+            runSystem(world, cmdBuf, [&](Query<Read<IntegerComponent>> query) {
+                auto [comp] = *query[ent];
+                CHECK(comp->value == 1);
+            });
+        }
     }
 }
