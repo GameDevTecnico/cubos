466493dc6f1f06b77e025e1977db0c6d67848934
Ricardo Antunes <me@riscadoa.com>
2023-05-29 08:51:42 +0100

    fix(core): fix ecs::System::valid/compatible

--- core/src/cubos/core/ecs/system.cpp
+++ core/src/cubos/core/ecs/system.cpp
@@ -4,6 +4,12 @@ using namespace cubos::core::ecs;
 
 bool SystemInfo::valid() const
 {
+    if (this->usesWorld)
+    {
+        return !this->usesCommands && this->resourcesRead.empty() && this->resourcesWritten.empty() &&
+               this->componentsRead.empty() && this->componentsWritten.empty();
+    }
+
     for (const auto& rsc : this->resourcesRead)
     {
         if (this->resourcesWritten.contains(rsc))
@@ -46,17 +52,17 @@ bool SystemInfo::compatible(const SystemInfo& other) const
         }
     }
 
-    for (const auto& rsc : other.resourcesRead)
+    for (const auto& rsc : this->resourcesWritten)
     {
-        if (this->resourcesWritten.contains(rsc))
+        if (other.resourcesRead.contains(rsc) || other.resourcesWritten.contains(rsc))
         {
             return false;
         }
     }
 
-    for (const auto& comp : other.componentsRead)
+    for (const auto& comp : this->componentsWritten)
     {
-        if (this->componentsWritten.contains(comp))
+        if (other.componentsRead.contains(comp) || other.componentsWritten.contains(comp))
         {
             return false;
         }
