39b0e155c5202d0d68364f4aa56b7dfee10316ba
Ricardo Antunes <me@riscadoa.com>
2023-05-31 20:01:01 +0100

    test(core): cover data::FileSystem

--- core/tests/data/utils.hpp
+++ core/tests/data/utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <filesystem>
+
 #include <cubos/core/memory/stream.hpp>
 
 #include "../utils.hpp"
@@ -10,4 +12,15 @@ inline std::string dump(cubos::core::memory::Stream& stream)
     std::string contents;
     stream.readUntil(contents, nullptr);
     return contents;
+}
+
+/// Returns a path to a temporary location to use for testing.
+/// It is guaranteed that the path does not exist, but its parent directory does.
+/// Always returns the same path for the same test, but erases the path before returning it.
+inline std::filesystem::path genTempPath(std::string name = "")
+{
+    auto tmp = std::filesystem::temp_directory_path();
+    auto path = tmp / ("cubos-core-tests" + (name.empty() ? "" : "-" + name));
+    std::filesystem::remove_all(path);
+    return path;
 }
