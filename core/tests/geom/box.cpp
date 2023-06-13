37e19dbb7c37fc4a9aa8f006888aa2cec3985311
Luís Fonseca <luis@lhf.pt>
2023-06-13 18:47:59 +0100

    test(core): make box tests more readable

--- core/tests/geom/box.cpp
+++ core/tests/geom/box.cpp
@@ -8,39 +8,42 @@ using cubos::core::geom::Box;
 
 TEST_CASE("geom::Box")
 {
-    Box box{{1.0f, 2.0f, 3.0f}};
+    auto x = 1.0f;
+    auto y = 2.0f;
+    auto z = 3.0f;
+    Box box{{x, y, z}};
 
     SUBCASE("two corners")
     {
         glm::vec3 corners[2];
         box.corners2(corners);
-        CHECK_GLM_EQ(corners[0], glm::vec3{-1.0f, -2.0f, -3.0f});
-        CHECK_GLM_EQ(corners[1], glm::vec3{1.0f, 2.0f, 3.0f});
+        CHECK_GLM_EQ(corners[0], glm::vec3{-x, -y, -z});
+        CHECK_GLM_EQ(corners[1], glm::vec3{x, y, z});
     }
 
     SUBCASE("six corners")
     {
         glm::vec3 corners[6];
         box.corners6(corners);
-        CHECK_GLM_EQ(corners[0], glm::vec3{-1.0f, -2.0f, -3.0f});
-        CHECK_GLM_EQ(corners[1], glm::vec3{1.0f, 2.0f, 3.0f});
-        CHECK_GLM_EQ(corners[2], glm::vec3{-1.0f, -2.0f, 3.0f});
-        CHECK_GLM_EQ(corners[3], glm::vec3{1.0f, 2.0f, -3.0f});
-        CHECK_GLM_EQ(corners[4], glm::vec3{-1.0f, 2.0f, -3.0f});
-        CHECK_GLM_EQ(corners[5], glm::vec3{1.0f, -2.0f, 3.0f});
+        CHECK_GLM_EQ(corners[0], glm::vec3{-x, -y, -z});
+        CHECK_GLM_EQ(corners[1], glm::vec3{x, y, z});
+        CHECK_GLM_EQ(corners[2], glm::vec3{-x, -y, z});
+        CHECK_GLM_EQ(corners[3], glm::vec3{x, y, -z});
+        CHECK_GLM_EQ(corners[4], glm::vec3{-x, y, -z});
+        CHECK_GLM_EQ(corners[5], glm::vec3{x, -y, z});
     }
 
     SUBCASE("eight corners")
     {
         glm::vec3 corners[8];
         box.corners(corners);
-        CHECK_GLM_EQ(corners[0], glm::vec3{-1.0f, -2.0f, -3.0f});
-        CHECK_GLM_EQ(corners[1], glm::vec3{1.0f, 2.0f, 3.0f});
-        CHECK_GLM_EQ(corners[2], glm::vec3{-1.0f, -2.0f, 3.0f});
-        CHECK_GLM_EQ(corners[3], glm::vec3{1.0f, 2.0f, -3.0f});
-        CHECK_GLM_EQ(corners[4], glm::vec3{-1.0f, 2.0f, -3.0f});
-        CHECK_GLM_EQ(corners[5], glm::vec3{1.0f, -2.0f, 3.0f});
-        CHECK_GLM_EQ(corners[6], glm::vec3{-1.0f, 2.0f, 3.0f});
-        CHECK_GLM_EQ(corners[7], glm::vec3{1.0f, -2.0f, -3.0f});
+        CHECK_GLM_EQ(corners[0], glm::vec3{-x, -y, -z});
+        CHECK_GLM_EQ(corners[1], glm::vec3{x, y, z});
+        CHECK_GLM_EQ(corners[2], glm::vec3{-x, -y, z});
+        CHECK_GLM_EQ(corners[3], glm::vec3{x, y, -z});
+        CHECK_GLM_EQ(corners[4], glm::vec3{-x, y, -z});
+        CHECK_GLM_EQ(corners[5], glm::vec3{x, -y, z});
+        CHECK_GLM_EQ(corners[6], glm::vec3{-x, y, z});
+        CHECK_GLM_EQ(corners[7], glm::vec3{x, -y, -z});
     }
 }
