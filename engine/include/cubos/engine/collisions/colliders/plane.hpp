74a6f9bdbcffe9283ad991b0987d9e32b0dd04c5
Luís Fonseca <luis@lhf.pt>
2023-06-14 12:39:37 +0100

    docs(engine): add comments to colliders

--- engine/include/cubos/engine/collisions/colliders/plane.hpp
+++ engine/include/cubos/engine/collisions/colliders/plane.hpp
@@ -1,12 +1,16 @@
+/// @file
+/// @brief Contains the class for the Plane Collider component.
+
 #pragma once
 
 #include <cubos/core/geom/plane.hpp>
 
 namespace cubos::engine
 {
+    /// @brief Plane Collider.
     struct [[cubos::component("cubos/plane_collider", VecStorage)]] PlaneCollider
     {
-        glm::vec3 offset;
-        cubos::core::geom::Plane shape;
+        glm::vec3 offset;               ///< The offset of the collider.
+        cubos::core::geom::Plane shape; ///< The plane shape of the collider.
     };
 }
