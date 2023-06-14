87db1e501a402056fac7ab83afe50b557431d761
Luís Fonseca <luis@lhf.pt>
2023-06-14 21:46:48 +0100

    docs(core): further document the colliders

--- engine/include/cubos/engine/collisions/colliders/box.hpp
+++ engine/include/cubos/engine/collisions/colliders/box.hpp
@@ -8,10 +8,21 @@
 namespace cubos::engine
 {
     /// @brief Box Collider.
+    ///
+    /// @details
+    /// - Convex: For any 2 points inside the shape, the line segment connecting those points lies inside the shape.
+    /// - Positive margin: Requires a positive margin to round off sharp corners. Defined in physics-space units.
     struct [[cubos::component("cubos/box_collider", VecStorage)]] BoxCollider
     {
         glm::mat4 transform{1.0f};    ///< The transform of the collider.
         cubos::core::geom::Box shape; ///< The box shape of the collider.
-        float margin = 0.04f;         ///< The margin of the collider. Needed for collision stability.
+
+        /// @brief The margin of the collider. Needed for collision stability.
+        ///
+        /// @details
+        /// The collider margin avoids collision errors by rounding off sharp corners.
+        /// It is absolute so the collider's transform won't affect it.
+        /// Shouldn't be changed without good reason, it's preferable to scale down the collider to account for it.
+        float margin = 0.04f;
     };
 }
--- engine/include/cubos/engine/collisions/colliders/simplex.hpp
+++ engine/include/cubos/engine/collisions/colliders/simplex.hpp
@@ -8,10 +8,21 @@
 namespace cubos::engine
 {
     /// @brief Simplex Collider.
+    ///
+    /// @details
+    /// - Convex: For any 2 points inside the shape, the line segment connecting those points lies inside the shape.
+    /// - Positive margin: Requires a positive margin to round off sharp corners. Defined in physics-space units.
+    /// - Zero thickness (up to 3 points): Not suitable for dynamic objects, but can be used for static objects.
     struct [[cubos::component("cubos/simplex", VecStorage)]] SimplexCollider
     {
         glm::vec3 offset{0.0f};           ///< The offset of the collider.
         cubos::core::geom::Simplex shape; ///< The simplex shape of the collider.
-        float margin = 0.04f;             ///< The margin of the collider. Needed for collision stability.
+
+        /// @brief The margin of the collider. Needed for collision stability.
+        ///
+        /// @details
+        /// The collider margin avoids collision errors by rounding off sharp corners.
+        /// Shouldn't be changed without good reason, it's preferable to move the simplex corners to account for it.
+        float margin = 0.04f;
     };
 }
