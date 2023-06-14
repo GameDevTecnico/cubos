064411db68eeabd8b3e5a9ae31929e247c5553ae
Luís Fonseca <luis.h.fonseca@tecnico.ulisboa.pt>
2023-06-14 15:36:00 +0100

    fix(core): vec_to_string in tests should be inline
    Co-authored-by: Ricardo Antunes <me@riscadoa.com>

--- core/tests/geom/utils.hpp
+++ core/tests/geom/utils.hpp
@@ -4,7 +4,7 @@
 
 #define CHECK_VEC3_EQ(a, ...) CHECK_MESSAGE((a == __VA_ARGS__), vec_to_string(a), " != ", vec_to_string(__VA_ARGS__))
 
-std::string vec_to_string(const glm::vec3& v)
+inline std::string vec_to_string(const glm::vec3& v)
 {
     std::stringstream ss;
     ss << "(" << v.x << ", " << v.y << ", " << v.z << ")";
