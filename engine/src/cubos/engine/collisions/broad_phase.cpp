bc3496b67c257cf8e00e9a0d200cd0fb69bc50f3
Luís Fonseca <luis@lhf.pt>
2023-06-15 21:45:30 +0100

    feat(engine): implement system to update collider markers

--- engine/src/cubos/engine/collisions/broad_phase.cpp
+++ engine/src/cubos/engine/collisions/broad_phase.cpp
@@ -23,8 +23,20 @@ void updateSimplexAABBs(Query<Read<LocalToWorld>, Read<SimplexCollider>, Write<C
 
 void updateMarkers(Query<Read<ColliderAABB>> query, Write<BroadPhaseCollisions> collisions)
 {
-    (void)query;
-    (void)collisions;
+    // TODO: This is parallelizable.
+    for (glm::length_t axis = 0; axis < 3; axis++)
+    {
+        // TODO: Should use insert sort to leverage spatial coherence.
+        std::sort(
+            collisions->markersPerAxis[axis].begin(), collisions->markersPerAxis[axis].end(),
+            [axis, &query](const BroadPhaseCollisions::SweepMarker& a, const BroadPhaseCollisions::SweepMarker& b) {
+                auto [aAABB] = query[a.entity].value();
+                auto [bAABB] = query[b.entity].value();
+                auto aPos = a.isMin ? aAABB->min() : aAABB->max();
+                auto bPos = b.isMin ? bAABB->min() : bAABB->max();
+                return aPos[axis] < bPos[axis];
+            });
+    }
 }
 
 void sweep(Write<BroadPhaseCollisions> collisions)
