5d42f7b70af632e3de685bfc6d867c40a822d388
Luís Fonseca <luis@lhf.pt>
2023-06-15 10:33:28 +0100

    feat(engine): make BroadPhaseCollisions resource data oriented

--- engine/src/cubos/engine/collisions/broad_phase_collisions.cpp
+++ engine/src/cubos/engine/collisions/broad_phase_collisions.cpp
@@ -1,106 +1,111 @@
-#include <unordered_set>
-
 #include <cubos/engine/collisions/broad_phase_collisions.hpp>
 
 using cubos::engine::BroadPhaseCollisions;
-
-template <typename F>
-void BroadPhaseCollisions::updateMarkers(F position)
-{
-    for (auto& entity : mEntities)
-    {
-        auto [min, max] = bounds(entity.entity);
-        entity.min = min;
-        entity.max = max;
-    }
-
-    for (size_t axis = 0; axis < 3; axis++)
-    {
-        std::sort(mMarkersPerAxis[axis].begin(), mMarkersPerAxis[axis].end(),
-                  [this, axis](const SweepMarker& a, const SweepMarker& b) {
-                      auto a = mEntities[a.entityIndex];
-                      auto b = mEntities[b.entityIndex];
-                      return (a.isMin ? a.min : a.max)[axis] < (b.isMin ? b.min : b.max)[axis];
-                  });
-    }
-}
-
-const std::vector<std::unordered_map<Entity, std::vector<Entity>>>& BroadPhaseCollisions::sweep()
-{
-    mSweepOverlapMaps.clear();
-    auto active = std::unordered_set<size_t>{};
-
-    for (size_t axis = 0; axis < 3; axis++)
-    {
-        for (auto& marker : mMarkersPerAxis[axis])
-        {
-            if (marker.isMin)
-            {
-                auto entity = mEntities[marker.entityIndex].entity;
-                for (auto& otherIndex : active)
-                {
-                    auto other = mEntities[otherIndex].entity;
-                    mSweepOverlapMaps[axis][entity].push_back(other);
-                }
-
-                active.insert(marker.entityIndex);
-            }
-            else
-            {
-                active.erase(marker.entityIndex);
-            }
-        }
-    }
-
-    return mSweepOverlapMaps;
-}
+using Candidate = cubos::engine::BroadPhaseCollisions::Candidate;
+using CollisionType = cubos::engine::BroadPhaseCollisions::CollisionType;
+using SweepEntity = cubos::engine::BroadPhaseCollisions::SweepEntity;
+using SweepMarker = cubos::engine::BroadPhaseCollisions::SweepMarker;
 
 void BroadPhaseCollisions::addEntity(Entity entity)
 {
-    mEntities.push_back({entity, glm::vec3(0.0f)});
+    entities.push_back({entity, AABB{}});
 
     for (size_t axis = 0; axis < 3; axis++)
     {
-        mMarkersPerAxis[axis].push_back({mEntities.size() - 1, true});
-        mMarkersPerAxis[axis].push_back({mEntities.size() - 1, false});
+        markersPerAxis[axis].push_back({entities.size() - 1, true});
+        markersPerAxis[axis].push_back({entities.size() - 1, false});
     }
 }
 
 void BroadPhaseCollisions::removeEntity(Entity entity)
 {
-    auto it =
-        std::find_if(mEntities.begin(), mEntities.end(), [entity](const SweepEntity& e) { return e.entity == entity; });
-
-    if (it != mEntities.end())
+    size_t index;
+    for (index = 0; index < entities.size(); index++)
     {
-        auto index = std::distance(mEntities.begin(), it);
-        mEntities.erase(it);
+        if (entities[index].entity == entity)
+            break;
+    }
 
-        for (size_t axis = 0; axis < 3; axis++)
-        {
-            auto& markers = mMarkersPerAxis[axis];
-            markers.erase(std::remove_if(markers.begin(), markers.end(),
-                                         [index](const SweepMarker& m) { return m.entityIndex == index; }),
-                          markers.end());
-        }
+    if (index == entities.size())
+        return;
+
+    entities.erase(entities.begin() + index);
+
+    for (size_t axis = 0; axis < 3; axis++)
+    {
+        auto& markers = markersPerAxis[axis];
+        markers.erase(std::remove_if(markers.begin(), markers.end(),
+                                     [index](const SweepMarker& m) { return m.entityIndex == index; }),
+                      markers.end());
     }
 }
 
 void BroadPhaseCollisions::clearEntities()
 {
-    mEntities.clear();
+    entities.clear();
     for (size_t axis = 0; axis < 3; axis++)
     {
-        mMarkersPerAxis[axis].clear();
+        markersPerAxis[axis].clear();
     }
 }
 
 void BroadPhaseCollisions::addCandidate(CollisionType type, Candidate candidate)
 {
-    mCandidatesPerType[static_cast<size_t>(type)].push_back(candidate);
+    candidatesPerType[static_cast<size_t>(type)].push_back(candidate);
 }
 
-const std::vector<BroadPhaseCollisions::Candidate>& BroadPhaseCollisions::getCandidates(CollisionType type) const
+const std::vector<Candidate>& BroadPhaseCollisions::getCandidates(CollisionType type)
 {
-    return mCandidatesPerType[static_cast<size_t>(type)];
+    return candidatesPerType[static_cast<size_t>(type)];
 }
+
+// template <typename F>
+// void BroadPhaseCollisions::updateMarkers(F position)
+// {
+//     for (auto& entity : entities)
+//     {
+//         auto [min, max] = bounds(entity.entity);
+//         entity.min = min;
+//         entity.max = max;
+//     }
+
+//     for (size_t axis = 0; axis < 3; axis++)
+//     {
+//         std::sort(markersPerAxis[axis].begin(), markersPerAxis[axis].end(),
+//                   [this, axis](const SweepMarker& a, const SweepMarker& b) {
+//                       auto a = entities[a.entityIndex];
+//                       auto b = entities[b.entityIndex];
+//                       return (a.isMin ? a.min : a.max)[axis] < (b.isMin ? b.min : b.max)[axis];
+//                   });
+//     }
+// }
+
+// const std::vector<std::unordered_map<Entity, std::vector<Entity>>>& BroadPhaseCollisions::sweep()
+// {
+//     mSweepOverlapMaps.clear();
+//     auto active = std::unordered_set<size_t>{};
+
+//     for (size_t axis = 0; axis < 3; axis++)
+//     {
+//         for (auto& marker : markersPerAxis[axis])
+//         {
+//             if (marker.isMin)
+//             {
+//                 auto entity = entities[marker.entityIndex].entity;
+//                 for (auto& otherIndex : active)
+//                 {
+//                     auto other = entities[otherIndex].entity;
+//                     mSweepOverlapMaps[axis][entity].push_back(other);
+//                 }
+
+//                 active.insert(marker.entityIndex);
+//             }
+//             else
+//             {
+//                 active.erase(marker.entityIndex);
+//             }
+//         }
+//     }
+
+//     return mSweepOverlapMaps;
+// }
