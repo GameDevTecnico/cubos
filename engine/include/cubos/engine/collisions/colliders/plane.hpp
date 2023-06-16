3b2f3b8b534f0c5fc3b98f2adfef056586a7a89c
Luís Fonseca <luis@lhf.pt>
2023-06-16 16:54:09 +0100

    fix(engine): plane collider should have margin

--- engine/include/cubos/engine/collisions/colliders/plane.hpp
+++ engine/include/cubos/engine/collisions/colliders/plane.hpp
@@ -11,9 +11,17 @@ namespace cubos::engine
     ///
     /// @details
     /// - Zero thickness: Not suitable for dynamic objects, but can be used for static objects.
+    /// - Positive margin: Requires a positive margin. Defined in physics-space units.
     struct [[cubos::component("cubos/plane_collider", VecStorage)]] PlaneCollider
     {
         glm::vec3 offset{0.0f};         ///< The offset of the collider.
         cubos::core::geom::Plane shape; ///< The plane shape of the collider.
+
+        /// @brief The margin of the collider. Needed for creating the AABB.
+        ///
+        /// @details.
+        /// The collision margin is absolute so the collider's transform won't affect it.
+        /// Shouldn't be changed without good reason.
+        float margin = 0.04f;
     };
 } // namespace cubos::engine
