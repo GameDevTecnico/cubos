d1169a08c6d89b5006fd0a68d34b3346ca2d620c
auto code formatter <clang@format.com>
2023-06-18 11:43:37 +0000

    apply clang-format

--- tools/tesserato/src/main.cpp
+++ tools/tesserato/src/main.cpp
@@ -1,25 +1,24 @@
 #include <imgui.h>
 
-#include <cubos/engine/imgui/plugin.hpp>
-#include <cubos/engine/tools/asset_explorer/plugin.hpp>
-
-#include <cubos/engine/renderer/plugin.hpp>
-#include <cubos/engine/transform/plugin.hpp>
-
 #include <cubos/core/settings.hpp>
 
+#include <cubos/engine/imgui/plugin.hpp>
+#include <cubos/engine/renderer/plugin.hpp>
+#include <cubos/engine/tools/asset_explorer/plugin.hpp>
+#include <cubos/engine/transform/plugin.hpp>
+
 using namespace cubos::engine;
-using cubos::core::ecs::Write;
 using cubos::core::ecs::Commands;
+using cubos::core::ecs::Write;
 
-static void mockCamera(Write<ActiveCameras> camera, Commands cmds) {
+static void mockCamera(Write<ActiveCameras> camera, Commands cmds)
+{
     camera->entities[0] = cmds.create()
-                        .add(Camera{.fovY = 60.0F, .zNear = 0.1F, .zFar = 100.0F})
-                        .add(LocalToWorld{})
-                        .add(Position{{5.0F, 5.0F, -10.0F}})
-                        .add(Rotation{glm::quatLookAt(glm::vec3{0.0F, 0.0F, 1.0F}, glm::vec3{0.0F, 1.0F, 0.0F})})
-                        .entity();
-
+                              .add(Camera{.fovY = 60.0F, .zNear = 0.1F, .zFar = 100.0F})
+                              .add(LocalToWorld{})
+                              .add(Position{{5.0F, 5.0F, -10.0F}})
+                              .add(Rotation{glm::quatLookAt(glm::vec3{0.0F, 0.0F, 1.0F}, glm::vec3{0.0F, 1.0F, 0.0F})})
+                              .entity();
 }
 
 static void mockSettings(Write<cubos::core::Settings> settings)
