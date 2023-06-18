de1383b7b79f2a0a86eb65ba2c404ec224e5966e
Ricardo Antunes <me@riscadoa.com>
2023-06-18 16:29:21 +0100

    feat(engine): add LocalToWorld automagically

--- engine/src/cubos/engine/transform/plugin.cpp
+++ engine/src/cubos/engine/transform/plugin.cpp
@@ -2,10 +2,23 @@
 
 #include <cubos/engine/transform/plugin.hpp>
 
+using cubos::core::ecs::Commands;
 using cubos::core::ecs::OptRead;
 using cubos::core::ecs::Query;
 using cubos::core::ecs::Write;
 using namespace cubos::engine;
+
+static void autoLocalToWorld(Commands cmds,
+                             Query<OptRead<LocalToWorld>, OptRead<Position>, OptRead<Rotation>, OptRead<Scale>> query)
+{
+    for (auto [entity, localToWorld, position, rotation, scale] : query)
+    {
+        if (!localToWorld && (position || rotation || scale))
+        {
+            cmds.add(entity, LocalToWorld{});
+        }
+    }
+}
 
 static void applyTransform(Query<Write<LocalToWorld>, OptRead<Position>, OptRead<Rotation>, OptRead<Scale>> query)
 {
@@ -36,5 +49,6 @@ void cubos::engine::transformPlugin(Cubos& cubos)
     cubos.addComponent<Scale>();
     cubos.addComponent<LocalToWorld>();
 
+    cubos.system(autoLocalToWorld).tagged("cubos.transform.update");
     cubos.system(applyTransform).tagged("cubos.transform.update");
 }
