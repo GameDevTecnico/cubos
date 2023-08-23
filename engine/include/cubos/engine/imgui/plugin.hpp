6d5fcdff5cbebda7c7b60652fead2721611ec5e8
Ricardo Antunes <me@riscadoa.com>
2023-08-23 18:44:27 +0100

    docs(engine): cleanup imgui plugin docs

--- engine/include/cubos/engine/imgui/plugin.hpp
+++ engine/include/cubos/engine/imgui/plugin.hpp
@@ -1,27 +1,34 @@
+/// @dir
+/// @brief ImGui plugin directory.
+
+/// @file
+/// @brief Plugin entry point.
+
 #pragma once
 
 #include <cubos/engine/cubos.hpp>
 
 namespace cubos::engine
 {
-    /// Plugin to initialize and configure ImGui for CUBOS. Tag your ImGui systems with
-    /// `cubos.imgui`.
+    /// @defgroup imgui-plugin ImGui integration
+    /// @ingroup plugins
+    /// @brief Initializes and configures ImGui for CUBOS.
     ///
-    /// @details This plugin adds three systems: one to initialize ImGui, one to start a new
-    /// ImGui frame, and one to end and render the ImGui frame. Between the two ImGui frame
-    /// systems, you can add your own ImGui system, with the tag 'cubos.imgui'.
+    /// @note Systems using `ImGui` **must be** tagged with `cubos.imgui`.
     ///
-    /// Dependencies:
-    /// - `windowPlugin`
+    /// ## Startup tags
+    /// - `cubos.imgui.init` - `ImGui` is initialized, after `cubos.window.init`.
     ///
-    /// Startup tags:
-    /// - `cubos.imgui.init`: initializes the ImGui, after `cubos.window.init`.
+    /// ## Tags
+    /// - `cubos.imgui.begin` - the ImGui frame begins (after `cubos.window.poll`).
+    /// - `cubos.imgui.end` - the ImGui frame ends (before `cubos.window.render`).
+    /// - `cubos.imgui` - runs between the previous two tags.
     ///
-    /// Tags:
-    /// - `cubos.imgui.begin`: starts a new ImGui frame (after `cubos.window.poll`).
-    /// - `cubos.imgui.end`: ends the ImGui frame (before `cubos.window.render`).
-    /// - `cubos.imgui`: runs your own ImGui system (between the previous two tags).
-    ///
+    /// ## Dependencies
+    /// - @ref window-plugin
+
+    /// @brief Plugin entry function.
     /// @param cubos CUBOS. main class
+    /// @ingroup imgui-plugin
     void imguiPlugin(Cubos& cubos);
 } // namespace cubos::engine
