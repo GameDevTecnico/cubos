c6fd0f674a52b5c7560379dc2033e9de91fc385c
Ricardo Antunes <me@riscadoa.com>
2023-08-23 16:07:25 +0100

    docs(engine): cleanup cubos.hpp docs

--- engine/include/cubos/engine/cubos.hpp
+++ engine/include/cubos/engine/cubos.hpp
@@ -1,3 +1,9 @@
+/// @dir engine
+/// @brief @ref cubos::engine namespace.
+
+/// @file
+/// @brief @ref Class cubos::engine::Cubos.
+
 #pragma once
 
 #include <set>
@@ -10,45 +16,58 @@
 
 namespace cubos::engine
 {
-    /// Resource used for storing delta time
+    /// @brief Resource which stores the time since the last iteration of the main loop started.
+    ///
+    /// This resource is added and updated by the @ref Cubos class.
     struct DeltaTime
     {
         DeltaTime(float value);
         float value;
     };
 
-    /// Resource used as a flag to indicate whether the engine should stop running.
+    /// @brief Resource used as a flag to indicate whether the main loop should stop running.
+    ///
+    /// This resource is added by the @ref Cubos class, initially set to true.
     struct ShouldQuit
     {
         ShouldQuit(bool value);
         bool value;
     };
 
-    /// Resource used for storing program arguments.
+    /// @brief Resource used for storing program arguments.
+    ///
+    /// This resource is added by the @ref Cubos class when constructed with arguments.
     struct Arguments
     {
         Arguments(std::vector<std::string> value);
         const std::vector<std::string> value;
     };
 
-    /// Used to chain configurations related to tags
+    /// @brief Used to chain configurations related to tags.
     class TagBuilder
     {
     public:
+        /// @brief Construct.
+        /// @param dispatcher Dispatcher being configured
+        /// @param tags Vector which stores the tags for this dispatcher.
+        /// @return Reference to this object, for chaining.
         TagBuilder(core::ecs::Dispatcher& dispatcher, std::vector<std::string>& tags);
 
-        /// Sets the current tag to be executed before another tag.
-        /// @param tag The tag to be executed before.
+        /// @brief Sets the current tag to be executed before another tag.
+        /// @param tag Tag to be executed before.
+        /// @return Reference to this object, for chaining.
         TagBuilder& beforeTag(const std::string& tag);
 
-        /// Sets the current tag to be executed after another tag.
-        /// @param tag The tag to be executed after.
+        /// @brief Sets the current tag to be executed after another tag.
+        /// @param tag Tag to be executed after.
+        /// @return Reference to this object, for chaining.
         TagBuilder& afterTag(const std::string& tag);
 
-        /// Adds a condition to the current tag. If this condition returns
-        /// false, systems with this tag will not be executed. For the tagged
-        /// systems to run, all conditions must return true.
-        /// @param func The condition function.
+        /// @brief Adds a condition to the current tag. If this condition returns false, systems
+        /// with this tag will not be executed. For the tagged systems to run, all conditions must
+        /// return true.
+        /// @param func Condition function.
+        /// @return Reference to this object, for chaining.
         template <typename F>
         TagBuilder& runIf(F func);
 
@@ -57,38 +76,49 @@ namespace cubos::engine
         std::vector<std::string>& mTags;
     };
 
-    /// Used to chain configurations related to systems
+    /// @brief Used to chain configurations related to systems
     class SystemBuilder
     {
     public:
+        /// @brief Construct.
+        /// @param dispatcher Dispatcher being configured.
+        /// @param tags Vector which stores the tags for this dispatcher.
         SystemBuilder(core::ecs::Dispatcher& dispatcher, std::vector<std::string>& tags);
 
-        /// Sets the current system's tag.
-        /// @param tag The tag to be set.
+        /// @brief Sets the current system's tag.
+        /// @param tag Tag to be set.
+        /// @return Reference to this object, for chaining.
         SystemBuilder& tagged(const std::string& tag);
 
-        /// Sets the current system to be executed before another tag.
-        /// @param tag The tag to be executed before.
+        /// @brief Sets the current system to be executed before another tag.
+        /// @param tag Tag to be executed before.
+        /// @return Reference to this object, for chaining.
         SystemBuilder& beforeTag(const std::string& tag);
 
-        /// Sets the current system to be executed after another tag.
-        /// @param tag The tag to be executed after.
+        /// @brief Sets the current system to be executed after another tag.
+        /// @param tag Tag to be executed after.
+        /// @return Reference to this object, for chaining.
         SystemBuilder& afterTag(const std::string& tag);
 
-        /// Sets the current system to be executed before another system.
-        /// @param func The system to be executed before.
+        /// @brief Sets the current system to be executed before another system.
+        /// @tparam F System type.
+        /// @param func System to be executed before.
+        /// @return Reference to this object, for chaining.
         template <typename F>
         SystemBuilder& beforeSystem(F func);
 
-        /// Sets the current system to be executed after another system.
-        /// @param func The system to be executed after.
+        /// @brief Sets the current system to be executed after another system.
+        /// @param func System to be executed after.
+        /// @tparam F System type.
+        /// @return Reference to this object, for chaining.
         template <typename F>
         SystemBuilder& afterSystem(F func);
 
-        /// Adds a condition to the current system. If this condition returns
-        /// false, the system will not be executed. For a system to run, all
-        /// conditions must return true.
-        /// @param func The condition function.
+        /// @brief Adds a condition to the current system. If this condition returns false, the
+        /// system will not be executed. For a system to run, all conditions must return true.
+        /// @tparam F Condition system type.
+        /// @param func Condition system.
+        /// @return Reference to this object, for chaining.
         template <typename F>
         SystemBuilder& runIf(F func);
 
@@ -97,68 +127,78 @@ namespace cubos::engine
         std::vector<std::string>& mTags;
     };
 
-    /// Represents the engine itself, and exposes the interface with which the game developer interacts with.
-    /// Ties up all the different parts of the engine together.
+    /// @brief Represents the engine itself, and exposes the interface with which the game
+    /// developer interacts with. Ties up all the different parts of the engine together.
     class Cubos final
     {
     public:
-        Cubos();
-        Cubos(int argc, char** argv);
         ~Cubos() = default;
 
-        /// Adds a new plugin to the engine. If the plugin had already been added, nothing happens.
-        /// A plugin is just a function that operates on the Cubos object, further configuring it.
-        /// It is useful for separating the code into modules.
-        /// @param plugin The plugin to add.
+        /// @brief Constructs an empty application without arguments.
+        Cubos();
+
+        /// @brief Constructs an empty application with arguments.
+        /// @param argc Argument count.
+        /// @param argv Argument array.
+        Cubos(int argc, char** argv);
+
+        /// @brief Adds a new plugin to the engine. If the plugin had already been added, nothing
+        /// happens. A plugin is just a function that operates on the Cubos object, further
+        /// configuring it. It is useful for separating the code into modules.
+        /// @param plugin Plugin to add.
         /// @return Reference to this object, for chaining.
         Cubos& addPlugin(void (*func)(Cubos&));
 
-        /// Adds a new resource to the engine.
-        /// @tparam R The type of the resource.
-        /// @tparam TArgs The types of the arguments passed to the resource's constructor.
-        /// @param args The arguments passed to the resource's constructor.
+        /// @brief Adds a new resource to the engine.
+        /// @tparam R Type of the resource.
+        /// @tparam TArgs Types of the arguments passed to the resource's constructor.
+        /// @param args Arguments passed to the resource's constructor.
         /// @return Reference to this object, for chaining.
         template <typename R, typename... TArgs>
         Cubos& addResource(TArgs... args);
 
-        /// Adds a new component type to the engine.
-        /// @tparam C The type of the component.
+        /// @brief Adds a new component type to the engine.
+        /// @tparam C Type of the component.
         /// @return Reference to this object, for chaining.
         template <typename C>
         Cubos& addComponent();
 
-        /// Adds a new event type to the engine.
-        /// @tparam E The type of the event.
+        /// @brief Adds a new event type to the engine.
+        /// @tparam E Type of the event.
         /// @return Reference to this object, for chaining.
         template <typename E>
         Cubos& addEvent();
 
-        /// Sets the current tag for the main dispatcher. If the tag doesn't exist, it will be created.
-        /// @param tag The tag to set.
-        /// @return Tag builder used to configure the tag.
+        /// @brief Returns a @ref TagBuilder to configure the given tag.
+        /// @param tag Tag to configure.
+        /// @return @ref TagBuilder used to configure the tag.
         TagBuilder tag(const std::string& tag);
 
-        /// Sets the current tag for the startup dispatcher. If the tag doesn't exist, it will be created.
-        /// @param tag The tag to set.
-        /// @return Tag builder used to configure the tag.
+        /// @brief Returns a @ref TagBuilder to configure the given startup tag.
+        /// @param tag Tag to configure.
+        /// @return @ref TagBuilder used to configure the tag.
         TagBuilder startupTag(const std::string& tag);
 
-        /// Adds a new system to the engine, which will be executed at every iteration of the main loop.
-        /// @tparam F The type of the system function.
-        /// @param func The system function.
-        /// @return System builder used to configure the system.
+        /// @brief Adds a new system to the engine, which will be executed at every iteration of
+        /// the main loop.
+        /// @tparam F Type of the system function.
+        /// @param func System function.
+        /// @return @ref SystemBuilder used to configure the system.
         template <typename F>
         SystemBuilder system(F func);
 
-        /// Adds a new startup system to the engine.
-        /// Startup systems are executed only once, before the main loop starts.
-        /// @tparam F The type of the system function.
-        /// @param func The system function.
-        /// @return System builder used to configure the system.
+        /// @brief Adds a new startup system to the engine, which will be executed only once,
+        /// before the main loop starts.
+        /// @tparam F Type of the system function.
+        /// @param func System function.
+        /// @return @ref SystemBuilder used to configure the system.
         template <typename F>
         SystemBuilder startupSystem(F func);
 
-        /// Runs the engine.
+        /// @brief Runs the engine.
+        ///
+        /// Initially, dispatches all of the startup systems.
+        /// Then, while @ref ShouldQuit is false, dispatches all other systems.
         void run();
 
     private:
