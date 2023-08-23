97095f919942f8401068f492ea5f7edd94dadbb8
Ricardo Antunes <me@riscadoa.com>
2023-08-23 18:23:45 +0100

    docs(engine): cleanup file_settings plugin docs

--- engine/include/cubos/engine/file_settings/plugin.hpp
+++ engine/include/cubos/engine/file_settings/plugin.hpp
@@ -1,20 +1,30 @@
+/// @dir
+/// @brief File settings plugin directory.
+
+/// @file
+/// @brief Plugin entry point.
+
 #pragma once
 
 #include <cubos/engine/cubos.hpp>
 
 namespace cubos::engine
 {
-    /// Plugin to load settings, provided via a file.
-    /// Any duplicated setting will be overwritten.
+    /// @defgroup file-settings-plugin File settings
+    /// @ingroup plugins
+    /// @brief Loads settings from a file.
     ///
-    /// @details The settings file must be a JSON file, located at the path specified by the
-    /// `settings.path` setting. If the setting is not defined, its assumed to be located at
-    /// `./settings.json`. If the file does not exist, the plugin will abort.
+    /// The settings file must be a JSON file, located at the path specified by the `settings.path`
+    /// setting. If the setting is not defined, its assumed to be located at `./settings.json`. If
+    /// the file does not exist, the plugin will abort. Any previously set setting will be
+    /// overwritten if its set on the file.
     ///
-    /// Startup tags:
-    /// - `cubos.settings`: the settings are loaded with this tag.
-    /// - `cubos.settings.file`: the settings are loaded with this tag.
-    ///
+    /// ## Startup tags
+    /// - `cubos.settings`: the settings are loaded.
+    /// - `cubos.settings.file`: the settings are loaded.
+
+    /// @brief Plugin entry function.
     /// @param cubos CUBOS. main class
+    /// @ingroup file-settings-plugin
     void fileSettingsPlugin(Cubos& cubos);
 } // namespace cubos::engine
