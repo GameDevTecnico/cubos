4197d94c8de757fe36f5546dcd087a99797af8f8
Ricardo Antunes <me@riscadoa.com>
2023-08-24 16:11:02 +0100

    docs(core): cleanup data/fs docs

--- core/include/cubos/core/data/fs/archive.hpp
+++ core/include/cubos/core/data/fs/archive.hpp
@@ -1,5 +1,6 @@
 /// @file
-/// @brief Defines the Archive interface class.
+/// @brief Class @ref cubos::core::data::Archive.
+/// @ingroup core-data-fs
 
 #pragma once
 
@@ -7,77 +8,97 @@
 
 namespace cubos::core::data
 {
-    /// @brief Acts as a bridge between the virtual file system and some place where data is
-    /// actually stored, such as a file on disk or a network connection. Should never be used
-    /// directly by the user - instead, use the FileSystem and File classes.
+    /// @brief Interface for a bridge between the CUBOS. virtual file system and the real world.
     ///
-    /// @details The archive is responsible for creating and destroying files, supplying streams
+    /// @note Should never be used directly by the user - instead, use the FileSystem and File
+    /// classes.
+    ///
+    /// By real world, we mean something such as a file on the OS file system or a network
+    /// connection. The archive is responsible for creating and destroying files, supplying streams
     /// for reading and writing, and retrieving information about existing files.
     ///
     /// Each file in an archive is represented by a unique integer identifier.
     /// The identifier 0 is reserved for errors, and the identifier 1 is reserved for the root file
     /// of the archive.
     ///
-    /// The root file may be a directory or a regular file - if its a directory, when mounted, the
+    /// The root file may be a directory or a regular file. If its a directory, when mounted, the
     /// archive will take the form of a directory, with the root file being placed on the mount
     /// point. If the root file is a regular file, then the mount point will be a regular file.
     ///
-    /// This class is not meant to be thread-safe - it is the responsibility of the File class to
-    /// ensure that the same file is not accessed or modified by multiple threads at the same time.
+    /// This class is not meant to be thread-safe - it is the responsibility of the @ref File class
+    /// to ensure that the same file is not accessed or modified by multiple threads at the same
+    /// time.
     ///
     /// Implementations should crash when called with invalid arguments - since they're only called
-    /// by File, its okay to assume they're correct.
+    /// by @ref File, its okay to assume the arguments are correct (please do use asserts though).
     ///
-    /// @see File FileSystem
+    /// @see File
+    /// @see FileSystem
+    /// @ingroup core-data-fs
     class Archive
     {
     public:
         Archive() = default;
         virtual ~Archive() = default;
 
-        /// Creates a new file in the archive.
-        /// The first child of the parent will have its sibling set to the new file and then be
+        /// @brief Creates a new file in the archive.
+        ///
+        /// The first child of the @p parent will have its sibling set to the new file and then be
         /// replaced by the new file.
+        ///
         /// @param parent Parent directory of the new file.
         /// @param name Name of the new file.
+        /// @param directory Whether the new file is a directory or not.
         /// @return Identifier of the file, or 0 if the file could not be created.
         virtual std::size_t create(std::size_t parent, std::string_view name, bool directory = false) = 0;
 
-        /// Destroys a regular file or empty directory in the archive.
-        /// Will be replaced in the tree its next sibling.
+        /// @brief Destroys a regular file or empty directory in the archive.
+        ///
+        /// Will be replaced in the tree by its sibling.
+        ///
         /// @param id Identifier of the file.
         /// @return Whether the file was destroyed successfully.
         virtual bool destroy(std::size_t id) = 0;
 
+        /// @brief Gets the name of the file with the given @p id.
         /// @param id Identifier of the file.
         /// @return Name of the file.
         virtual std::string name(std::size_t id) const = 0;
 
+        /// @brief Checks whether the file with the given @p id is a directory.
         /// @param id Identifier of the file.
         /// @return Whether the file is a directory or not.
         virtual bool directory(std::size_t id) const = 0;
 
+        /// @brief Checks whether the archive is read-only.
         /// @return Whether the archive is read-only.
         virtual bool readOnly() const = 0;
 
+        /// @brief Gets the identifier of the parent file of the file with the given @p id.
         /// @param id Identifier of the file.
         /// @return Identifier of the parent file, or 0 if the file is the root file.
         virtual std::size_t parent(std::size_t id) const = 0;
 
+        /// @brief Gets the identifier of the sibling of the file with the given @p id.
         /// @param id Identifier of the file.
-        /// @return Identifier of the next sibling, or 0 if there are no more files in the parent.
+        /// @return Identifier of the sibling, or 0 if there are no more files in the parent.
         virtual std::size_t sibling(std::size_t id) const = 0;
 
+        /// @brief Gets the identifier of the first child of the file with the given @p id.
         /// @param id Identifier of the file.
         /// @return Identifier of the first child, or 0 if the file is not a directory or is empty.
         virtual std::size_t child(std::size_t id) const = 0;
 
-        /// Opens a file in the archive.
+        /// @brief Opens a file in the archive.
+        ///
+        /// Although a bit hacky, the @p handle parameter is used to keep a reference to the
+        /// respective @p File alive, preventing the file from being destroyed while the stream is
+        /// open.
+        ///
         /// @param id Identifier of the file.
-        /// @param handle Handle to increase the reference count of the file, preventing it from
-        /// being destroyed while the stream is open.
+        /// @param handle Handle to the file.
         /// @param mode Mode to open the file in.
-        /// @return A file stream, or nullptr if the file could not be opened.
+        /// @return File stream, or nullptr if the file could not be opened.
         virtual std::unique_ptr<memory::Stream> open(std::size_t id, File::Handle handle, File::OpenMode mode) = 0;
     };
 } // namespace cubos::core::data
--- core/include/cubos/core/data/fs/file_system.hpp
+++ core/include/cubos/core/data/fs/file_system.hpp
@@ -1,5 +1,6 @@
 /// @file
-/// @brief Defines the FileSystem singleton class.
+/// @brief Class @ref cubos::core::data::FileSystem.
+/// @ingroup core-data-fs
 
 #pragma once
 
@@ -7,97 +8,116 @@
 
 namespace cubos::core::data
 {
-    /// @brief Singleton class which represents the virtual file system of the engine.
-    /// All operations on files through this class are done with absolute paths, and are equivalent
-    /// to the same operations on the root file, with relative paths.
+    /// @brief Singleton which represents the virtual file system of the engine.
+    ///
+    /// All filesystem operations in the engine should be done through this class or through a @ref
+    /// File obtained here.
+    ///
+    /// @note Operations on this class expect absolute paths and are equivalent to the same
+    /// operations on the root file with relative paths.
     ///
     /// @see File
+    /// @ingroup core-data-fs
     class FileSystem final
     {
     public:
         FileSystem() = delete;
         ~FileSystem() = delete;
 
-        /// @return The root file of the virtual file system.
+        /// @brief Gets the root file of the virtual file system.
+        /// @return File handle.
         static File::Handle root();
 
-        /// @brief Mounts an archive to an absolute path. Creates any parent directories that may
-        /// be necessary, but the mount point itself must not already exist.
+        /// @brief Mounts an archive to an absolute path.
         ///
-        /// @details This method fails on the following conditions:
+        /// Creates any parent directories that may be necessary, but the mount point itself must
+        /// not already exist.
+        ///
+        /// This method fails on the following conditions:
         /// - path is relative or invalid.
         /// - a parent file in the path exists and is not a directory.
         /// - a parent directory in the path belongs to an archive.
         /// - a file already exists at the mount point.
         ///
-        /// @see File::mount
+        /// @see File::mount()
         /// @param path Absolute path to mount the archive on.
         /// @param archive Archive to mount.
         /// @return Whether the archive was successfully mounted.
         static bool mount(std::string_view path, std::unique_ptr<Archive> archive);
 
-        /// @brief Unmounts an archive from an absolute path. Removes all of the archive's files
-        /// from the virtual file system.
+        /// @brief Unmounts an archive from an absolute path.
         ///
-        /// @details This method fails on the following conditions:
+        /// Removes all of the archive's files from the virtual file system.
+        ///
+        /// This method fails on the following conditions:
         /// - path is relative or invalid.
         /// - any of the files in the path do not exist.
         /// - the target path is not the mount point of an archive.
         ///
-        /// @see File::unmount
+        /// @see File::unmount()
         /// @param path Path to unmount the archive from.
         /// @return Whether the archive was successfully unmounted.
         static bool unmount(std::string_view path);
 
-        /// @brief Gets a handle to a file. Fails if the file does not exist or the path is relative
-        /// or invalid.
-        /// @see File::find
+        /// @brief Gets a handle to a file.
+        ///
+        /// Fails if the file does not exist or the path is relative or invalid.
+        ///
+        /// @see File::find()
         /// @param path Absolute path to the file.
-        /// @return The file or nullptr if the search failed.
+        /// @return File handle or nullptr on failure.
         static File::Handle find(std::string_view path);
 
-        /// @brief Creates a new file at the specified absolute path. The destination location
-        /// must be writeable - that is, it must be under a mounted writeable archive. If a file at
-        /// the specified path already exists, it is returned instead. Any parent directories that
-        /// may be necessary are created.
+        /// @brief Creates a new file at the specified absolute path.
         ///
-        /// @details This method fails on the following conditions:
+        /// The destination location must be writeable - that is, it must be under a mounted
+        /// writeable archive.
+        ///
+        /// If a file at the specified path already exists, it is returned instead. Any parent
+        /// directories that may be necessary are created.
+        ///
+        /// This method fails on the following conditions:
         /// - path is relative or invalid.
         /// - a parent directory in the path does not exist and cannot be created.
         /// - a parent directory in the path already exists as a regular file.
         /// - a file of a different type already exists at the destination.
         ///
-        /// @see File::create
-        /// @param path The absolute path to the file.
+        /// @see File::create()
+        /// @param path Absolute path to the file.
         /// @param directory Whether the new file should be a directory.
-        /// @return The file or nullptr if the creation failed.
+        /// @return File or nullptr on failure.
         static File::Handle create(std::string_view path, bool directory = false);
 
-        /// @brief Marks a file for destruction. The file will only be deleted when no more
-        /// references to it exist. The file is immediately removed from its parent directory and
-        /// any children of the file are also marked for destruction.
+        /// @brief Marks a file for destruction.
         ///
-        /// @details This method fails on the following conditions:
+        /// Although The file will only be deleted when no more references to it exist, it is
+        /// immediately removed from the virtual file system.
+        ///
+        /// All children of the file are also marked for destruction.
+        ///
+        /// This method fails on the following conditions:
         /// - path is relative or invalid.
         /// - the file does not exist.
         /// - the file is the mount point of an archive.
         /// - the file does not belong to an archive.
         /// - the file belongs to a read-only archive.
         ///
-        /// @see File::destroy
+        /// @see File::destroy()
         /// @param path Absolute path of the file to destroy.
         /// @return Whether the file was successfully marked for destruction.
         static bool destroy(std::string_view path);
 
-        /// Opens a file in the virtual file system.
+        /// @brief Opens a file in the virtual file system.
         ///
-        /// @details This method fails on the following conditions:
-        /// - FileSystem::find(path) fails.
-        /// - file->open(mode) fails.
+        /// If the file is being written to, blocks until the other threads are done with the file.
+        ///
+        /// This method fails on the following conditions:
+        /// - `FileSystem::find(path)` fails.
+        /// - `file->open(mode)` fails.
         ///
         /// @param path Absolute path of the file.
         /// @param mode Mode to open the file in.
-        /// @return The file stream, or nullptr if an error occurred.
+        /// @return File stream, or nullptr if an error occurred.
         static std::unique_ptr<memory::Stream> open(std::string_view path, File::OpenMode mode);
     };
 } // namespace cubos::core::data
