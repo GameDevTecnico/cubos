3bfc1209fede9b79c63a87b720841eb3df4ad936
Ricardo Antunes <me@riscadoa.com>
2023-08-24 13:34:11 +0100

    docs(core): cleanup log.hpp docs

--- core/include/cubos/core/log.hpp
+++ core/include/cubos/core/log.hpp
@@ -1,6 +1,32 @@
+/// @file
+/// @brief Logging and assertion macros.
+/// @ingroup core
+
 #pragma once
 
 #include <cstdlib>
+
+/// @addtogroup core
+/// @{
+
+/// @def CUBOS_LOG_LEVEL
+/// @brief The log level to compile in.
+///
+/// This macro essentially controls the minimum log level that will be compiled into the binary.
+///
+/// @todo The log level should be modifiable at runtime.
+///
+/// Should be set to one of the following:
+/// - @ref CUBOS_LOG_LEVEL_TRACE
+/// - @ref CUBOS_LOG_LEVEL_DEBUG
+/// - @ref CUBOS_LOG_LEVEL_INFO
+/// - @ref CUBOS_LOG_LEVEL_WARN
+/// - @ref CUBOS_LOG_LEVEL_ERROR
+/// - @ref CUBOS_LOG_LEVEL_CRITICAL
+/// - @ref CUBOS_LOG_LEVEL_OFF
+///
+/// By default, on debug builds, this is set to @ref CUBOS_LOG_LEVEL_TRACE.
+/// On release builds, this is set to @ref CUBOS_LOG_LEVEL_INFO.
 
 #ifndef CUBOS_LOG_LEVEL
 #ifndef NDEBUG
@@ -14,27 +40,69 @@
 #define SPDLOG_ACTIVE_LEVEL CUBOS_LOG_LEVEL
 #include <spdlog/spdlog.h>
 
-// The possible log levels.
-
+/// @brief Trace log level, lowest log level. Very verbose.
+/// @sa CUBOS_LOG_LEVEL
 #define CUBOS_LOG_LEVEL_TRACE SPDLOG_LEVEL_TRACE
+
+/// @brief Debug log level. Contains messages useful for debugging, but which are not necessary in
+/// release builds.
+/// @sa CUBOS_LOG_LEVEL
 #define CUBOS_LOG_LEVEL_DEBUG SPDLOG_LEVEL_DEBUG
+
+/// @brief Information log level. Contains important events that are not errors.
+/// @sa CUBOS_LOG_LEVEL
 #define CUBOS_LOG_LEVEL_INFO SPDLOG_LEVEL_INFO
+
+/// @brief Warn log level. Contains events that are not errors, but which are unexpected and may be
+/// problematic.
+/// @sa CUBOS_LOG_LEVEL
 #define CUBOS_LOG_LEVEL_WARN SPDLOG_LEVEL_WARN
+
+/// @brief Error log level. Contains errors which are recoverable from.
+/// @sa CUBOS_LOG_LEVEL
 #define CUBOS_LOG_LEVEL_ERROR SPDLOG_LEVEL_ERROR
+
+/// @brief Critical log level, highest log level. Contains errors which are unrecoverable from.
+/// @sa CUBOS_LOG_LEVEL
 #define CUBOS_LOG_LEVEL_CRITICAL SPDLOG_LEVEL_CRITICAL
+
+/// @brief Off log level, disables all logging.
+/// @sa CUBOS_LOG_LEVEL
 #define CUBOS_LOG_LEVEL_OFF SPDLOG_LEVEL_OFF
 
-// Macros for logging.
-// These macros are only evaluated if CUBOS_LOG_LEVEL is set to a level that is equal or lower than the level of the
-// macro.
-
+/// @brief Used for logging very verbose information.
+/// @param ... Format string and arguments.
+/// @see CUBOS_LOG_LEVEL_TRACE
 #define CUBOS_TRACE(...) SPDLOG_TRACE(__VA_ARGS__)
+
+/// @brief Used for logging information which is useful for debugging but not necessary in release
+/// builds.
+/// @param ... Format string and arguments.
+/// @see CUBOS_LOG_LEVEL_DEBUG
 #define CUBOS_DEBUG(...) SPDLOG_DEBUG(__VA_ARGS__)
+
+/// @brief Used for logging information which is useful in release builds.
+/// @param ... Format string and arguments.
+/// @see CUBOS_LOG_LEVEL_INFO
 #define CUBOS_INFO(...) SPDLOG_INFO(__VA_ARGS__)
+
+/// @brief Used for logging unexpected events.
+/// @param ... Format string and arguments.
+/// @see CUBOS_LOG_LEVEL_WARN
 #define CUBOS_WARN(...) SPDLOG_WARN(__VA_ARGS__)
+
+/// @brief Used for logging recoverable errors.
+/// @param ... Format string and arguments.
+/// @see CUBOS_LOG_LEVEL_ERROR
 #define CUBOS_ERROR(...) SPDLOG_ERROR(__VA_ARGS__)
+
+/// @brief Used for logging unrecoverable errors.
+/// @param ... Format string and arguments.
+/// @see CUBOS_LOG_LEVEL_CRITICAL
 #define CUBOS_CRITICAL(...) SPDLOG_CRITICAL(__VA_ARGS__)
 
+/// @brief Aborts a program, optionally printing a critical error message.
+/// @param ... Optional format string and arguments.
 #define CUBOS_FAIL(...)                                                                                                \
     do                                                                                                                 \
     {                                                                                                                  \
@@ -45,6 +113,8 @@
         std::abort();                                                                                                  \
     } while (false)
 
+/// @brief Marks a code path as supposedly unreachable. Aborts the program when reached.
+/// @param ... Optional format string and arguments.
 #define CUBOS_UNREACHABLE(...)                                                                                         \
     do                                                                                                                 \
     {                                                                                                                  \
@@ -52,6 +122,9 @@
         CUBOS_FAIL(__VA_ARGS__);                                                                                       \
     } while (false)
 
+/// @brief Asserts that a condition is true, aborting the program if it is not.
+/// @param cond Condition to assert.
+/// @param ... Optional format string and arguments.
 #define CUBOS_ASSERT(cond, ...)                                                                                        \
     do                                                                                                                 \
     {                                                                                                                  \
@@ -62,14 +135,21 @@
         }                                                                                                              \
     } while (false)
 
-// FIXME: this should compiled out when some option is set to false
+/// @brief In debug builds asserts that a condition is true, aborting the program if it is not.
+/// @param cond Condition to assert.
+/// @param ... Optional format string and arguments.
+/// @todo This should be compiled out when some option is set to false.
 #define CUBOS_DEBUG_ASSERT(cond, ...) CUBOS_ASSERT(cond)
+
+/// @}
 
 namespace cubos::core
 {
-    /// Initializes the logger, must be called before any logging is done.
+    /// @brief Must be called before any logging is done.
+    /// @ingroup core
     void initializeLogger();
 
-    /// Disables all logging except for critical errors.
+    /// @brief Disables all logging except for critical errors.
+    /// @ingroup core
     void disableLogging();
 } // namespace cubos::core
