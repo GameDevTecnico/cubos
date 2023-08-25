c2b7c765e7dc4e28824d5953036e9d7a9f94a1ed
Ricardo Antunes <me@riscadoa.com>
2023-08-25 12:25:55 +0100

    docs(core): cleanup ui docs

--- core/include/cubos/core/ui/ecs.hpp
+++ core/include/cubos/core/ui/ecs.hpp
@@ -1,17 +1,25 @@
+/// @file
+/// @brief Functions @ref cubos::core::ui::showWorld and @ref cubos::core::ui::editWorld.
+/// @ingroup core-ui
+
 #pragma once
 
 #include <cubos/core/ecs/world.hpp>
 
 namespace cubos::core::ui
 {
-    /// ECS system which allows the user to inspect the entities and components in an ECS world.
+    /// @brief Adds a ImGui window which allows the user to inspect the entities and components in
+    /// a @ref ecs::World.
     /// @param world World to show.
     /// @param context Optional context for serializing the world.
+    /// @ingroup core-ui
     void showWorld(const ecs::World& world, data::Context* context = nullptr);
 
-    /// ECS system which allows the user to inspect and edit the entities and components in an ECS world.
+    /// @brief Adds a ImGui window which allows the user to edit the entities and components in a
+    /// @ref ecs::World.
     /// @param world World to edit.
     /// @param serContext Optional context for serializing the world.
     /// @param desContext Optional context for deserializing the world.
+    /// @ingroup core-ui
     void editWorld(ecs::World& world, data::Context* serContext = nullptr, data::Context* desContext = nullptr);
 } // namespace cubos::core::ui
--- core/include/cubos/core/ui/serialization.hpp
+++ core/include/cubos/core/ui/serialization.hpp
@@ -1,33 +1,46 @@
+/// @file
+/// @brief Functions for showing and editing serializable objects in the UI.
+/// @ingroup core-ui
+
 #pragma once
 
 #include <cubos/core/data/package.hpp>
 
 namespace cubos::core::ui
 {
-    /// Shows a packaged object's properties in the UI. Should be called
-    /// inside a ImGui::BeginTable(2)/ImGui::EndTable() block.
+    /// @brief Shows a packaged object's properties in the UI. Should be called inside a
+    /// `ImGui::BeginTable(2)` and `ImGui::EndTable()` block.
     ///
-    /// The first column displays the name of the package and the second column displays the value of the package
-    /// @param pkg The packaged object to show.
-    /// @param name The name of the object.
+    /// The first column displays the name of the package and the second column displays the value
+    /// of the package.
+    ///
+    /// @param pkg Packaged object to show.
+    /// @param name Name of the object.
+    /// @ingroup core-ui
     void showPackage(const data::Package& pkg, const std::string& name);
 
-    /// Shows a packaged object's properties in the UI, allowing the user to
-    /// edit the object. Should be called inside a
-    /// ImGui::BeginTable(3)/ImGui::EndTable() block.
+    /// @brief Shows a packaged object's properties in the UI, allowing the user to edit the
+    /// object. Should be called inside a `ImGui::BeginTable(3)` and `ImGui::EndTable()` block.
     ///
-    /// The first column displays the name of the package, the second column displays the value of the package, and the
-    /// third column is used for remove buttons in the case of arrays and dictionaries.
-    /// @param pkg The packaged object to edit.
-    /// @param name The name of the object.
-    /// @returns True if the object was edited, false otherwise.
+    /// The first column displays the name of the package, the second column displays the value
+    /// of the package and the third column is used for remove buttons in the case of arrays and
+    /// dictionaries.
+    ///
+    /// @param pkg Packaged object to edit.
+    /// @param name Name of the object.
+    /// @return True if the object was modified, false otherwise.
+    /// @ingroup core-ui
     bool editPackage(data::Package& pkg, const std::string& name);
 
-    /// Shows a serializable object's properties in the UI. Should be called
-    /// inside a ImGui::BeginTable(2)/ImGui::EndTable() block.
-    /// @tparam T The type of the serializable object.
-    /// @param object The object to show.
-    /// @param name The name of the object.
+    /// @brief Shows a serializable object's properties in the UI. Should be called inside a
+    /// `ImGui::BeginTable(2)` and `ImGui::EndTable()` block.
+    ///
+    /// Internally packages the object and calls @ref showPackage().
+    ///
+    /// @tparam T Type of the serializable object.
+    /// @param object Object to show.
+    /// @param name Name of the object.
+    /// @ingroup core-ui
     template <typename T>
     inline void show(const T& object, const std::string& name)
     {
@@ -35,14 +48,17 @@ namespace cubos::core::ui
         showPackage(pkg, name);
     }
 
-    /// Shows a serializable and deserializable object's properties in the UI,
-    /// allowing the user to edit the object. If any of the properties is
-    /// edited, the object's deserialize() method is called. Should be called
-    /// inside a ImGui::BeginTable(3)/ImGui::EndTable() block.
-    /// @tparam T The type of the serializable object.
-    /// @param object The object to edit.
-    /// @param name The name of the object.
-    /// @returns True if the object was edited, false otherwise.
+    /// @brief Shows a serializable object's properties in the UI. Should be called inside a
+    /// `ImGui::BeginTable(3)` and `ImGui::EndTable()` block.
+    ///
+    /// Internally packages the object, calls @ref editPackage() and then unpackages the object, if
+    /// it was modified.
+    ///
+    /// @tparam T Type of the serializable object.
+    /// @param object Object to edit.
+    /// @param name Name of the object.
+    /// @return True if the object was edited, false otherwise.
+    /// @ingroup core-ui
     template <typename T>
     inline bool edit(T& object, const std::string& name)
     {
