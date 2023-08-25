8805e122bbcb8893e29e6bb5b5798a7803ba0d9a
Ricardo Antunes <me@riscadoa.com>
2023-08-25 12:25:55 +0100

    docs(core): cleanup ui docs

--- core/include/cubos/core/ui/imgui.hpp
+++ core/include/cubos/core/ui/imgui.hpp
@@ -1,3 +1,7 @@
+/// @file
+/// @brief Functions for initializing and using ImGui.
+/// @ingroup core-ui
+
 #pragma once
 
 #include <cubos/core/gl/render_device.hpp>
@@ -5,23 +9,31 @@
 
 namespace cubos::core::ui
 {
-    /// Initializes ImGui for use with the given window.
-    /// Should only be called once and no ImGui calls should be made before this is called.
+    /// @brief Initializes ImGui for use with the given window.
+    /// @note Should only be called once and no ImGui calls should be made before this is called.
     /// @param window The window to use.
+    /// @ingroup core-ui
     void initialize(io::Window window);
 
-    /// Shuts down ImGui.
-    /// Should only be called once, after @ref initialize, and no ImGui calls should be made after this is called.
+    /// @brief Shuts down ImGui.
+    /// @note Should only be called once, after @ref initialize(), and no ImGui calls should be
+    /// made after this is called.
+    /// @ingroup core-ui
     void terminate();
 
-    /// Begins a new ImGui frame.
+    /// @brief Begins a new ImGui frame.
+    /// @ingroup core-ui
     void beginFrame();
 
-    /// Ends the current ImGui frame, and renders the ImGui draw data to the window.
+    /// @brief Ends the current ImGui frame, and renders the ImGui draw data to the @p target
+    /// framebuffer, or the default framebuffer if @p target is null.
+    /// @param target Framebuffer to render to.
+    /// @ingroup core-ui
     void endFrame(const gl::Framebuffer& target = nullptr);
 
-    /// Passes a window event to ImGui.
-    /// @param event The event to pass.
-    /// @return True if the event was consumed by ImGui, false otherwise.
+    /// @brief Passes a window event to ImGui.
+    /// @param event Event to pass.
+    /// @return True if the event was handled by ImGui, false otherwise.
+    /// @ingroup core-ui
     bool handleEvent(const io::WindowEvent& event);
 } // namespace cubos::core::ui
