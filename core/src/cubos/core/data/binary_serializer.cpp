c72c158c94f363b1339ee7219233fa33b7ef22da
Ricardo Antunes <me@riscadoa.com>
2023-09-10 13:17:00 +0100

    fix(core): BinarySerializer::write* now sets fail bit correctly

--- core/src/cubos/core/data/binary_serializer.cpp
+++ core/src/cubos/core/data/binary_serializer.cpp
@@ -24,66 +24,66 @@ BinarySerializer::BinarySerializer(memory::Stream& stream, bool writeLittleEndia
 
 void BinarySerializer::writeI8(int8_t value, const char* /*name*/)
 {
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeI16(int16_t value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeI32(int32_t value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeI64(int64_t value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeU8(uint8_t value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeU16(uint16_t value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeU32(uint32_t value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeU64(uint64_t value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeF32(float value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeF64(double value, const char* /*name*/)
 {
     value = toEndianness(value, mWriteLittleEndian);
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeBool(bool value, const char* /*name*/)
 {
-    mFailBit |= mStream.write(&value, sizeof(value)) == sizeof(value);
+    mFailBit |= mStream.write(&value, sizeof(value)) != sizeof(value);
 }
 
 void BinarySerializer::writeString(const char* value, const char* /*name*/)
