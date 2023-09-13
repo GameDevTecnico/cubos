c896cb488bbd5a9bb72b608ba1afb283c167cd58
Ricardo Antunes <me@riscadoa.com>
2023-09-13 12:40:03 +0100

    feat(core): define reflection for primitive types

---
+++ core/include/cubos/core/reflection/external/primitives.hpp
@@ -0,0 +1,22 @@
+/// @file
+/// @brief Reflection declarations for primitive types.
+/// @ingroup core-reflection
+
+#pragma once
+
+#include <cstdint>
+
+#include <cubos/core/reflection/reflect.hpp>
+
+CUBOS_REFLECT_EXTERNAL_DECL(bool);
+CUBOS_REFLECT_EXTERNAL_DECL(char);
+CUBOS_REFLECT_EXTERNAL_DECL(int8_t);
+CUBOS_REFLECT_EXTERNAL_DECL(int16_t);
+CUBOS_REFLECT_EXTERNAL_DECL(int32_t);
+CUBOS_REFLECT_EXTERNAL_DECL(int64_t);
+CUBOS_REFLECT_EXTERNAL_DECL(uint8_t);
+CUBOS_REFLECT_EXTERNAL_DECL(uint16_t);
+CUBOS_REFLECT_EXTERNAL_DECL(uint32_t);
+CUBOS_REFLECT_EXTERNAL_DECL(uint64_t);
+CUBOS_REFLECT_EXTERNAL_DECL(float);
+CUBOS_REFLECT_EXTERNAL_DECL(double);
