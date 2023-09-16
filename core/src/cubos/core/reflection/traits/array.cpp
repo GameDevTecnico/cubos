9a0525a7596d5347126194ecbe4e5e73e5c4eacb
Ricardo Antunes <me@riscadoa.com>
2023-09-16 19:24:32 +0100

    feat(core): add ArrayTrait

---
+++ core/src/cubos/core/reflection/traits/array.cpp
@@ -0,0 +1,124 @@
+#include <cubos/core/log.hpp>
+#include <cubos/core/reflection/traits/array.hpp>
+
+using cubos::core::reflection::ArrayTrait;
+using cubos::core::reflection::Type;
+
+ArrayTrait::ArrayTrait(const Type& elementType, Length length, AddressOf addressOf)
+    : mElementType(elementType)
+    , mLength(length)
+    , mAddressOf(addressOf)
+    , mInsertDefault(nullptr)
+    , mInsertCopy(nullptr)
+    , mInsertMove(nullptr)
+    , mErase(nullptr)
+{
+}
+
+void ArrayTrait::insertDefault(InsertDefault insertDefault)
+{
+    CUBOS_ASSERT(!mInsertDefault, "Insert default already set");
+    mInsertDefault = insertDefault;
+}
+
+void ArrayTrait::insertCopy(InsertCopy insertCopy)
+{
+    CUBOS_ASSERT(!mInsertCopy, "Insert copy already set");
+    mInsertCopy = insertCopy;
+}
+
+void ArrayTrait::insertMove(InsertMove insertMove)
+{
+    CUBOS_ASSERT(!mInsertMove, "Insert move already set");
+    mInsertMove = insertMove;
+}
+
+void ArrayTrait::erase(Erase erase)
+{
+    CUBOS_ASSERT(!mErase, "Erase already set");
+    mErase = erase;
+}
+
+const Type& ArrayTrait::elementType() const
+{
+    return mElementType;
+}
+
+std::size_t ArrayTrait::length(const void* instance) const
+{
+    return mLength(instance);
+}
+
+void* ArrayTrait::get(void* instance, std::size_t index) const
+{
+    return reinterpret_cast<void*>(mAddressOf(instance, index));
+}
+
+const void* ArrayTrait::get(const void* instance, std::size_t index) const
+{
+    return reinterpret_cast<const void*>(mAddressOf(instance, index));
+}
+
+bool ArrayTrait::insertDefault(void* instance, std::size_t index) const
+{
+    if (mInsertDefault)
+    {
+        mInsertDefault(instance, index);
+        return true;
+    }
+
+    return false;
+}
+
+bool ArrayTrait::insertCopy(void* instance, std::size_t index, const void* value) const
+{
+    if (mInsertCopy)
+    {
+        mInsertCopy(instance, index, value);
+        return true;
+    }
+
+    return false;
+}
+
+bool ArrayTrait::insertMove(void* instance, std::size_t index, void* value) const
+{
+    if (mInsertMove)
+    {
+        mInsertMove(instance, index, value);
+        return true;
+    }
+
+    return false;
+}
+
+bool ArrayTrait::erase(void* instance, std::size_t index) const
+{
+    if (mErase)
+    {
+        mErase(instance, index);
+        return true;
+    }
+
+    return false;
+}
+
+bool ArrayTrait::hasInsertDefault() const
+{
+    return mInsertDefault != nullptr;
+}
+
+bool ArrayTrait::hasInsertCopy() const
+{
+    return mInsertCopy != nullptr;
+}
+
+bool ArrayTrait::hasInsertMove() const
+{
+    return mInsertMove != nullptr;
+}
+
+bool ArrayTrait::hasErase() const
+{
+    return mErase != nullptr;
+}
