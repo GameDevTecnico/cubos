140f73173bcda7a12f1a63d8d391ad529f5e1acb
Ricardo Antunes <me@riscadoa.com>
2023-09-19 17:06:00 +0100

    docs(engine): add Settings plugin sample

--- docs/pages/3_examples/2_engine/main.md
+++ docs/pages/3_examples/2_engine/main.md
@@ -5,4 +5,5 @@
 The following examples have fully documented tutorials on how to use the
 multiple plugins of the engine:
 
+- @subpage examples-engine-settings - @copybrief examples-engine-settings
 - @subpage examples-engine-renderer - @copybrief examples-engine-renderer
--- engine/include/cubos/engine/settings/plugin.hpp
+++ engine/include/cubos/engine/settings/plugin.hpp
@@ -15,6 +15,8 @@ namespace cubos::engine
     /// @defgroup settings-plugin Settings
     /// @ingroup engine
     /// @brief Adds and manages settings.
+    /// @see Take a look at the @ref examples-engine-settings example for a demonstration of this
+    /// plugin.
     ///
     /// Initially, parses settings from the @ref Arguments resource. Then, the file at
     /// `settings.path` is loaded, as a JSON file. If the file does not exist, it is created. If it
