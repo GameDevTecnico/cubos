7db529fd03830a01684a127c2e3a513328f10f69
Ricardo Antunes <me@riscadoa.com>
2023-09-26 15:08:14 +0100

    test(core): cover std::unordered_map reflection

---
+++ core/tests/reflection/external/unordered_map.cpp
@@ -0,0 +1,25 @@
+#include <doctest/doctest.h>
+
+#include <cubos/core/reflection/external/primitives.hpp>
+#include <cubos/core/reflection/external/unordered_map.hpp>
+
+#include "../traits/constructible.hpp"
+#include "../traits/dictionary.hpp"
+
+template <typename K, typename V>
+static void test(const char* name, std::unordered_map<K, V> map, K insertedKey, V insertedValue)
+{
+    CHECK(reflect<std::unordered_map<K, V>>().name() == name);
+    testDictionary<std::unordered_map<K, V>, K, V>(map, map.size(), &insertedKey, &insertedValue);
+    testConstructible<std::unordered_map<K, V>>(&map);
+}
+
+TEST_CASE("reflection::reflect<std::unordered_map<K, V>>()")
+{
+    test<int32_t, int32_t>("std::unordered_map<int32_t, int32_t>", {{1, 2}, {2, 4}}, 3, 6);
+    test<char, std::unordered_map<char, bool>>("std::unordered_map<char, std::unordered_map<char, bool>>",
+                                               {
+                                                   {'H', {{'i', true}, {'o', false}}},
+                                               },
+                                               'A', {{'h', true}});
+}
