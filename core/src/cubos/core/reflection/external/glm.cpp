64e98c8e0de7c57344d05319c72d94763c0b3dc9
Luís Fonseca <luis@lhf.pt>
2023-09-28 13:56:50 +0100

    feat(core): use Fields trait instead of Array trait

--- core/src/cubos/core/reflection/external/glm.cpp
+++ core/src/cubos/core/reflection/external/glm.cpp
@@ -4,58 +4,120 @@
 
 #include <cubos/core/reflection/external/glm.hpp>
 #include <cubos/core/reflection/external/primitives.hpp>
-#include <cubos/core/reflection/traits/array.hpp>
 #include <cubos/core/reflection/traits/constructible.hpp>
+#include <cubos/core/reflection/traits/fields.hpp>
 #include <cubos/core/reflection/type.hpp>
 
-#define AUTO_IMPL(type, name, inner_type, size)                                                                        \
-    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
-    {                                                                                                                  \
-        return Type::create(name)                                                                                      \
-            .with(ConstructibleTrait::typed<type>()                                                                    \
-                      .withDefaultConstructor()                                                                        \
-                      .withCopyConstructor()                                                                           \
-                      .withMoveConstructor()                                                                           \
-                      .build())                                                                                        \
-            .with(ArrayTrait(                                                                                          \
-                reflect<inner_type>(), [](const void* /*unused*/) -> std::size_t { return size; },                     \
-                [](const void* instance, std::size_t index) -> uintptr_t {                                             \
-                    return reinterpret_cast<uintptr_t>(glm::value_ptr(*(static_cast<const type*>(instance))) + index); \
-                }));                                                                                                   \
+using cubos::core::reflection::FieldsTrait;
+
+template <typename T>
+class AddressOfImpl final : public FieldsTrait::AddressOf
+{
+public:
+    /// @brief Constructs.
+    /// @param column Column index.
+    AddressOfImpl(size_t column)
+        : mColumn(column)
+    {
     }
 
-AUTO_IMPL(glm::quat, "glm::quat", float, 4);
-AUTO_IMPL(glm::dquat, "glm::dquat", double, 4);
-AUTO_IMPL(glm::mat2, "glm::mat2", float, 2 * 2);
-AUTO_IMPL(glm::dmat2, "glm::dmat2", double, 2 * 2);
-AUTO_IMPL(glm::mat2x3, "glm::mat2x3", float, 2 * 3);
-AUTO_IMPL(glm::dmat2x3, "glm::dmat2x3", double, 2 * 3);
-AUTO_IMPL(glm::mat2x4, "glm::mat2x4", float, 2 * 4);
-AUTO_IMPL(glm::dmat2x4, "glm::dmat2x4", double, 2 * 4);
-AUTO_IMPL(glm::mat3x2, "glm::mat3x2", float, 3 * 2);
-AUTO_IMPL(glm::dmat3x2, "glm::dmat3x2", double, 3 * 2);
-AUTO_IMPL(glm::mat3, "glm::mat3", float, 3 * 3);
-AUTO_IMPL(glm::dmat3, "glm::dmat3", double, 3 * 3);
-AUTO_IMPL(glm::mat3x4, "glm::mat3x4", float, 3 * 4);
-AUTO_IMPL(glm::dmat3x4, "glm::dmat3x4", double, 3 * 4);
-AUTO_IMPL(glm::mat4x2, "glm::mat4x2", float, 4 * 2);
-AUTO_IMPL(glm::dmat4x2, "glm::dmat4x2", double, 4 * 2);
-AUTO_IMPL(glm::mat4x3, "glm::mat4x3", float, 4 * 3);
-AUTO_IMPL(glm::dmat4x3, "glm::dmat4x3", double, 4 * 3);
-AUTO_IMPL(glm::mat4, "glm::mat4", float, 4 * 4);
-AUTO_IMPL(glm::dmat4, "glm::dmat4", double, 4 * 4);
-AUTO_IMPL(glm::vec2, "glm::vec2", float, 2);
-AUTO_IMPL(glm::bvec2, "glm::bvec2", bool, 2);
-AUTO_IMPL(glm::dvec2, "glm::dvec2", double, 2);
-AUTO_IMPL(glm::ivec2, "glm::ivec2", int32_t, 2);
-AUTO_IMPL(glm::uvec2, "glm::uvec2", uint32_t, 2);
-AUTO_IMPL(glm::vec3, "glm::vec3", float, 3);
-AUTO_IMPL(glm::bvec3, "glm::bvec3", bool, 3);
-AUTO_IMPL(glm::dvec3, "glm::dvec3", double, 3);
-AUTO_IMPL(glm::ivec3, "glm::ivec3", int32_t, 3);
-AUTO_IMPL(glm::uvec3, "glm::uvec3", uint32_t, 3);
-AUTO_IMPL(glm::vec4, "glm::vec4", float, 4);
-AUTO_IMPL(glm::dvec4, "glm::dvec4", double, 4);
-AUTO_IMPL(glm::bvec4, "glm::bvec4", bool, 4);
-AUTO_IMPL(glm::ivec4, "glm::ivec4", int32_t, 4);
-AUTO_IMPL(glm::uvec4, "glm::uvec4", uint32_t, 4);
+    uintptr_t get(const void* instance) const override
+    {
+        return reinterpret_cast<uintptr_t>(&(static_cast<const T*>(instance)[mColumn]));
+    }
+
+private:
+    size_t mColumn;
+};
+
+#define AUTO_CONSTRUCTIBLE(type)                                                                                       \
+    ConstructibleTrait::typed<type>().withDefaultConstructor().withCopyConstructor().withMoveConstructor().build()
+
+#define AUTO_VEC2(type) FieldsTrait().withField("0", &type::x).withField("1", &type::y)
+
+#define AUTO_VEC3(type) AUTO_VEC2(type).withField("2", &type::z)
+
+#define AUTO_VEC4(type) AUTO_VEC3(type).withField("3", &type::w)
+
+#define AUTO_MAT2(type, column_type)                                                                                   \
+    FieldsTrait()                                                                                                      \
+        .withField(reflect<column_type>(), "0", new AddressOfImpl<type>(0))                                            \
+        .withField(reflect<column_type>(), "1", new AddressOfImpl<type>(1))
+
+#define AUTO_MAT3(type, column_type)                                                                                   \
+    AUTO_MAT2(type, column_type).withField(reflect<column_type>(), "2", new AddressOfImpl<type>(2))
+
+#define AUTO_MAT4(type, column_type)                                                                                   \
+    AUTO_MAT3(type, column_type).withField(reflect<column_type>(), "3", new AddressOfImpl<type>(3))
+
+#define AUTO_IMPL_VEC4(type, name)                                                                                     \
+    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
+    {                                                                                                                  \
+        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_VEC4(type));                                \
+    }
+
+#define AUTO_IMPL_VEC3(type, name)                                                                                     \
+    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
+    {                                                                                                                  \
+        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_VEC3(type));                                \
+    }
+
+#define AUTO_IMPL_VEC2(type, name)                                                                                     \
+    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
+    {                                                                                                                  \
+        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_VEC2(type));                                \
+    }
+
+#define AUTO_IMPL_MAT2(type, name, column_type)                                                                        \
+    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
+    {                                                                                                                  \
+        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_MAT2(type, column_type));                   \
+    }
+
+#define AUTO_IMPL_MAT3(type, name, column_type)                                                                        \
+    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
+    {                                                                                                                  \
+        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_MAT3(type, column_type));                   \
+    }
+
+#define AUTO_IMPL_MAT4(type, name, column_type)                                                                        \
+    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
+    {                                                                                                                  \
+        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_MAT4(type, column_type));                   \
+    }
+
+AUTO_IMPL_MAT2(glm::mat2, "glm::mat2", glm::vec2);
+AUTO_IMPL_MAT2(glm::dmat2, "glm::dmat2", glm::dvec2);
+AUTO_IMPL_MAT2(glm::mat2x3, "glm::mat2x3", glm::vec3);
+AUTO_IMPL_MAT2(glm::dmat2x3, "glm::dmat2x3", glm::dvec3);
+AUTO_IMPL_MAT2(glm::mat2x4, "glm::mat2x4", glm::vec4);
+AUTO_IMPL_MAT2(glm::dmat2x4, "glm::dmat2x4", glm::dvec4);
+AUTO_IMPL_MAT3(glm::mat3x2, "glm::mat3x2", glm::vec2);
+AUTO_IMPL_MAT3(glm::dmat3x2, "glm::dmat3x2", glm::dvec2);
+AUTO_IMPL_MAT3(glm::mat3, "glm::mat3", glm::vec3);
+AUTO_IMPL_MAT3(glm::dmat3, "glm::dmat3", glm::dvec3);
+AUTO_IMPL_MAT3(glm::mat3x4, "glm::mat3x4", glm::vec4);
+AUTO_IMPL_MAT3(glm::dmat3x4, "glm::dmat3x4", glm::dvec4);
+AUTO_IMPL_MAT4(glm::mat4x2, "glm::mat4x2", glm::vec2);
+AUTO_IMPL_MAT4(glm::dmat4x2, "glm::dmat4x2", glm::dvec2);
+AUTO_IMPL_MAT4(glm::mat4x3, "glm::mat4x3", glm::vec3);
+AUTO_IMPL_MAT4(glm::dmat4x3, "glm::dmat4x3", glm::dvec3);
+AUTO_IMPL_MAT4(glm::mat4, "glm::mat4", glm::vec4);
+AUTO_IMPL_MAT4(glm::dmat4, "glm::dmat4", glm::dvec4);
+AUTO_IMPL_VEC2(glm::vec2, "glm::vec2");
+AUTO_IMPL_VEC2(glm::bvec2, "glm::bvec2");
+AUTO_IMPL_VEC2(glm::dvec2, "glm::dvec2");
+AUTO_IMPL_VEC2(glm::ivec2, "glm::ivec2");
+AUTO_IMPL_VEC2(glm::uvec2, "glm::uvec2");
+AUTO_IMPL_VEC3(glm::vec3, "glm::vec3");
+AUTO_IMPL_VEC3(glm::bvec3, "glm::bvec3");
+AUTO_IMPL_VEC3(glm::dvec3, "glm::dvec3");
+AUTO_IMPL_VEC3(glm::ivec3, "glm::ivec3");
+AUTO_IMPL_VEC3(glm::uvec3, "glm::uvec3");
+AUTO_IMPL_VEC4(glm::vec4, "glm::vec4");
+AUTO_IMPL_VEC4(glm::dvec4, "glm::dvec4");
+AUTO_IMPL_VEC4(glm::bvec4, "glm::bvec4");
+AUTO_IMPL_VEC4(glm::ivec4, "glm::ivec4");
+AUTO_IMPL_VEC4(glm::uvec4, "glm::uvec4");
+AUTO_IMPL_VEC4(glm::quat, "glm::quat");
+AUTO_IMPL_VEC4(glm::dquat, "glm::dquat");
