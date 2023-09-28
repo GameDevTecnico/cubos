66f7d3e14d34b7a06715391ba1559c72ef6a49a3
Luís Fonseca <luis@lhf.pt>
2023-09-28 14:31:49 +0100

    refactor(core): reduce macro duplication

--- core/src/cubos/core/reflection/external/glm.cpp
+++ core/src/cubos/core/reflection/external/glm.cpp
@@ -50,74 +50,47 @@ private:
 #define AUTO_MAT4(type, column_type)                                                                                   \
     AUTO_MAT3(type, column_type).withField(reflect<column_type>(), "3", new AddressOfImpl<type>(3))
 
-#define AUTO_IMPL_VEC4(type, name)                                                                                     \
+#define AUTO_IMPL(type, name, auto_trait)                                                                              \
     CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
     {                                                                                                                  \
-        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_VEC4(type));                                \
+        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(auto_trait);                                     \
     }
 
-#define AUTO_IMPL_VEC3(type, name)                                                                                     \
-    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
-    {                                                                                                                  \
-        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_VEC3(type));                                \
-    }
+#define AUTO_IMPL_VEC(type, name, auto_vec) AUTO_IMPL(type, name, auto_vec(type))
+#define AUTO_IMPL_MAT(type, name, column_type, auto_mat) AUTO_IMPL(type, name, auto_mat(type, column_type))
 
-#define AUTO_IMPL_VEC2(type, name)                                                                                     \
-    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
-    {                                                                                                                  \
-        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_VEC2(type));                                \
-    }
-
-#define AUTO_IMPL_MAT2(type, name, column_type)                                                                        \
-    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
-    {                                                                                                                  \
-        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_MAT2(type, column_type));                   \
-    }
-
-#define AUTO_IMPL_MAT3(type, name, column_type)                                                                        \
-    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
-    {                                                                                                                  \
-        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_MAT3(type, column_type));                   \
-    }
-
-#define AUTO_IMPL_MAT4(type, name, column_type)                                                                        \
-    CUBOS_REFLECT_EXTERNAL_IMPL(type)                                                                                  \
-    {                                                                                                                  \
-        return Type::create(name).with(AUTO_CONSTRUCTIBLE(type)).with(AUTO_MAT4(type, column_type));                   \
-    }
-
-AUTO_IMPL_MAT2(glm::mat2, "glm::mat2", glm::vec2);
-AUTO_IMPL_MAT2(glm::dmat2, "glm::dmat2", glm::dvec2);
-AUTO_IMPL_MAT2(glm::mat2x3, "glm::mat2x3", glm::vec3);
-AUTO_IMPL_MAT2(glm::dmat2x3, "glm::dmat2x3", glm::dvec3);
-AUTO_IMPL_MAT2(glm::mat2x4, "glm::mat2x4", glm::vec4);
-AUTO_IMPL_MAT2(glm::dmat2x4, "glm::dmat2x4", glm::dvec4);
-AUTO_IMPL_MAT3(glm::mat3x2, "glm::mat3x2", glm::vec2);
-AUTO_IMPL_MAT3(glm::dmat3x2, "glm::dmat3x2", glm::dvec2);
-AUTO_IMPL_MAT3(glm::mat3, "glm::mat3", glm::vec3);
-AUTO_IMPL_MAT3(glm::dmat3, "glm::dmat3", glm::dvec3);
-AUTO_IMPL_MAT3(glm::mat3x4, "glm::mat3x4", glm::vec4);
-AUTO_IMPL_MAT3(glm::dmat3x4, "glm::dmat3x4", glm::dvec4);
-AUTO_IMPL_MAT4(glm::mat4x2, "glm::mat4x2", glm::vec2);
-AUTO_IMPL_MAT4(glm::dmat4x2, "glm::dmat4x2", glm::dvec2);
-AUTO_IMPL_MAT4(glm::mat4x3, "glm::mat4x3", glm::vec3);
-AUTO_IMPL_MAT4(glm::dmat4x3, "glm::dmat4x3", glm::dvec3);
-AUTO_IMPL_MAT4(glm::mat4, "glm::mat4", glm::vec4);
-AUTO_IMPL_MAT4(glm::dmat4, "glm::dmat4", glm::dvec4);
-AUTO_IMPL_VEC2(glm::vec2, "glm::vec2");
-AUTO_IMPL_VEC2(glm::bvec2, "glm::bvec2");
-AUTO_IMPL_VEC2(glm::dvec2, "glm::dvec2");
-AUTO_IMPL_VEC2(glm::ivec2, "glm::ivec2");
-AUTO_IMPL_VEC2(glm::uvec2, "glm::uvec2");
-AUTO_IMPL_VEC3(glm::vec3, "glm::vec3");
-AUTO_IMPL_VEC3(glm::bvec3, "glm::bvec3");
-AUTO_IMPL_VEC3(glm::dvec3, "glm::dvec3");
-AUTO_IMPL_VEC3(glm::ivec3, "glm::ivec3");
-AUTO_IMPL_VEC3(glm::uvec3, "glm::uvec3");
-AUTO_IMPL_VEC4(glm::vec4, "glm::vec4");
-AUTO_IMPL_VEC4(glm::dvec4, "glm::dvec4");
-AUTO_IMPL_VEC4(glm::bvec4, "glm::bvec4");
-AUTO_IMPL_VEC4(glm::ivec4, "glm::ivec4");
-AUTO_IMPL_VEC4(glm::uvec4, "glm::uvec4");
-AUTO_IMPL_VEC4(glm::quat, "glm::quat");
-AUTO_IMPL_VEC4(glm::dquat, "glm::dquat");
+AUTO_IMPL_MAT(glm::mat2, "glm::mat2", glm::vec2, AUTO_MAT2);
+AUTO_IMPL_MAT(glm::dmat2, "glm::dmat2", glm::dvec2, AUTO_MAT2);
+AUTO_IMPL_MAT(glm::mat2x3, "glm::mat2x3", glm::vec3, AUTO_MAT2);
+AUTO_IMPL_MAT(glm::dmat2x3, "glm::dmat2x3", glm::dvec3, AUTO_MAT2);
+AUTO_IMPL_MAT(glm::mat2x4, "glm::mat2x4", glm::vec4, AUTO_MAT2);
+AUTO_IMPL_MAT(glm::dmat2x4, "glm::dmat2x4", glm::dvec4, AUTO_MAT2);
+AUTO_IMPL_MAT(glm::mat3x2, "glm::mat3x2", glm::vec2, AUTO_MAT3);
+AUTO_IMPL_MAT(glm::dmat3x2, "glm::dmat3x2", glm::dvec2, AUTO_MAT3);
+AUTO_IMPL_MAT(glm::mat3, "glm::mat3", glm::vec3, AUTO_MAT3);
+AUTO_IMPL_MAT(glm::dmat3, "glm::dmat3", glm::dvec3, AUTO_MAT3);
+AUTO_IMPL_MAT(glm::mat3x4, "glm::mat3x4", glm::vec4, AUTO_MAT3);
+AUTO_IMPL_MAT(glm::dmat3x4, "glm::dmat3x4", glm::dvec4, AUTO_MAT3);
+AUTO_IMPL_MAT(glm::mat4x2, "glm::mat4x2", glm::vec2, AUTO_MAT4);
+AUTO_IMPL_MAT(glm::dmat4x2, "glm::dmat4x2", glm::dvec2, AUTO_MAT4);
+AUTO_IMPL_MAT(glm::mat4x3, "glm::mat4x3", glm::vec3, AUTO_MAT4);
+AUTO_IMPL_MAT(glm::dmat4x3, "glm::dmat4x3", glm::dvec3, AUTO_MAT4);
+AUTO_IMPL_MAT(glm::mat4, "glm::mat4", glm::vec4, AUTO_MAT4);
+AUTO_IMPL_MAT(glm::dmat4, "glm::dmat4", glm::dvec4, AUTO_MAT4);
+AUTO_IMPL_VEC(glm::vec2, "glm::vec2", AUTO_VEC2);
+AUTO_IMPL_VEC(glm::bvec2, "glm::bvec2", AUTO_VEC2);
+AUTO_IMPL_VEC(glm::dvec2, "glm::dvec2", AUTO_VEC2);
+AUTO_IMPL_VEC(glm::ivec2, "glm::ivec2", AUTO_VEC2);
+AUTO_IMPL_VEC(glm::uvec2, "glm::uvec2", AUTO_VEC2);
+AUTO_IMPL_VEC(glm::vec3, "glm::vec3", AUTO_VEC3);
+AUTO_IMPL_VEC(glm::bvec3, "glm::bvec3", AUTO_VEC3);
+AUTO_IMPL_VEC(glm::dvec3, "glm::dvec3", AUTO_VEC3);
+AUTO_IMPL_VEC(glm::ivec3, "glm::ivec3", AUTO_VEC3);
+AUTO_IMPL_VEC(glm::uvec3, "glm::uvec3", AUTO_VEC3);
+AUTO_IMPL_VEC(glm::vec4, "glm::vec4", AUTO_VEC4);
+AUTO_IMPL_VEC(glm::dvec4, "glm::dvec4", AUTO_VEC4);
+AUTO_IMPL_VEC(glm::bvec4, "glm::bvec4", AUTO_VEC4);
+AUTO_IMPL_VEC(glm::ivec4, "glm::ivec4", AUTO_VEC4);
+AUTO_IMPL_VEC(glm::uvec4, "glm::uvec4", AUTO_VEC4);
+AUTO_IMPL_VEC(glm::quat, "glm::quat", AUTO_VEC4);
+AUTO_IMPL_VEC(glm::dquat, "glm::dquat", AUTO_VEC4);
