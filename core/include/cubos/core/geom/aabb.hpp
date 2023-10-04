d8b5c31a3b0c3e82d5c5737c5b2f249d1877eaf3
Luís Fonseca <luis@lhf.pt>
2023-10-05 00:20:15 +0100

    refactor(geom): move aabb to core::geom

---
+++ core/include/cubos/core/geom/aabb.hpp
@@ -0,0 +1,114 @@
+/// @file
+/// @brief Component @ref cubos::core::geom::AABB.
+/// @ingroup core-geom
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include <cubos/core/geom/box.hpp>
+
+namespace cubos::core::geom
+{
+    /// @brief Represents an axis-aligned bounding box.
+    /// @ingroup core-geom
+    struct AABB
+    {
+        /// @brief Diagonal of the AABB.
+        glm::vec3 diag[2] = {glm::vec3{-INFINITY}, glm::vec3{INFINITY}};
+
+        /// @brief Minimum point of the AABB.
+        /// @return Minimum point of the AABB.
+        glm::vec3 min() const
+        {
+            return diag[0];
+        }
+
+        /// @brief Maximum point of the AABB.
+        /// @return Maximum point of the AABB.
+        glm::vec3 max() const
+        {
+            return diag[1];
+        }
+
+        /// @brief Sets the minimum point of the AABB.
+        /// @param min Minimum point of the AABB.
+        void min(const glm::vec3& min)
+        {
+            diag[0] = min;
+        }
+
+        /// @brief Sets the maximum point of the AABB.
+        /// @param max Maximum point of the AABB.
+        void max(const glm::vec3& max)
+        {
+            diag[1] = max;
+        }
+
+        /// @brief Gets a @ref Box representation of the AABB.
+        /// @return @ref Box representation.
+        Box box() const
+        {
+            auto size = max() - min();
+            return Box{size / 2.0F};
+        }
+
+        /// @brief Gets the center of the AABB.
+        /// @return Center of the AABB.
+        glm::vec3 center() const
+        {
+            return (min() + max()) / 2.0F;
+        }
+
+        /// @brief Checks if the AABB overlaps with another AABB on the X axis.
+        /// @param other Other AABB.
+        /// @return Whether the AABBs overlap.
+        bool overlapsX(const AABB& other) const
+        {
+            return min().x <= other.max().x && max().x >= other.min().x;
+        }
+
+        /// @brief Checks if the AABB overlaps with another AABB on the Y axis.
+        /// @param other Other AABB.
+        /// @return Whether the AABBs overlap.
+        bool overlapsY(const AABB& other) const
+        {
+            return min().y <= other.max().y && max().y >= other.min().y;
+        }
+
+        /// @brief Checks if the AABB overlaps with another AABB on the Z axis.
+        /// @param other Other AABB.
+        /// @return Whether the AABBs overlap.
+        bool overlapsZ(const AABB& other) const
+        {
+            return min().z <= other.max().z && max().z >= other.min().z;
+        }
+
+        /// @brief Checks if the AABB overlaps with another AABB.
+        /// @param other Other AABB.
+        /// @return Whether the AABBs overlap.
+        bool overlaps(const AABB& other) const
+        {
+            return overlapsX(other) && overlapsY(other) && overlapsZ(other);
+        }
+    };
+} // namespace cubos::core::geom
+
+namespace cubos::core::data::old
+{
+    inline void serialize(Serializer& ser, const core::geom::AABB& aabb, const char* name)
+    {
+        ser.beginObject(name);
+        ser.write(aabb.min(), "min");
+        ser.write(aabb.max(), "max");
+        ser.endObject();
+    }
+
+    inline void deserialize(Deserializer& des, core::geom::AABB& aabb)
+    {
+        des.beginObject();
+        des.read(aabb.diag[0]);
+        des.read(aabb.diag[1]);
+        des.endObject();
+    }
+} // namespace cubos::core::data::old
--- engine/include/cubos/engine/collisions/shapes/box.hpp
+++ engine/include/cubos/engine/collisions/shapes/box.hpp
@@ -8,10 +8,10 @@
 
 namespace cubos::engine
 {
-    /// @brief Component which adds a box collision shape to an entity, used with a collider component.
+    /// @brief Component which adds a box collision shape to an entity, used with a @ref Collider component.
     /// @ingroup collisions-plugin
     struct [[cubos::component("cubos/box_collision_shape", VecStorage)]] BoxCollisionShape
     {
-        cubos::core::geom::Box shape; ///< Box shape.
+        cubos::core::geom::Box box; ///< Box shape.
     };
 } // namespace cubos::engine
