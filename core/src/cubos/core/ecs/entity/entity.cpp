8f7192f1a04324f51bb507ab42aa6d18edb55be3
Ricardo Antunes <me@riscadoa.com>
2023-10-07 15:55:04 +0100

    feat(ecs): implement reflection for Entity

--- core/src/cubos/core/ecs/entity/entity.cpp
+++ core/src/cubos/core/ecs/entity/entity.cpp
@@ -3,9 +3,27 @@
 #include <cubos/core/data/old/serializer.hpp>
 #include <cubos/core/ecs/entity/entity.hpp>
 #include <cubos/core/ecs/entity/hash.hpp>
+#include <cubos/core/reflection/external/primitives.hpp>
+#include <cubos/core/reflection/traits/constructible.hpp>
+#include <cubos/core/reflection/traits/fields.hpp>
+#include <cubos/core/reflection/type.hpp>
 
 using cubos::core::ecs::Entity;
 using cubos::core::ecs::EntityHash;
+using cubos::core::reflection::ConstructibleTrait;
+using cubos::core::reflection::FieldsTrait;
+using cubos::core::reflection::Type;
+
+CUBOS_REFLECT_IMPL(Entity)
+{
+    return Type::create("cubos::core::ecs::Entity")
+        .with(ConstructibleTrait::typed<Entity>()
+                  .withCopyConstructor()
+                  .withMoveConstructor()
+                  .withDefaultConstructor()
+                  .build())
+        .with(FieldsTrait().withField("index", &Entity::index).withField("generation", &Entity::generation));
+}
 
 template <>
 void cubos::core::data::old::serialize<Entity>(Serializer& ser, const Entity& obj, const char* name)
