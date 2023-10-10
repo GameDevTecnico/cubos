1a108dce7c68ac4bfeb36332bb9808233f28b265
Ricardo Antunes <me@riscadoa.com>
2023-10-10 22:26:29 +0100

    feat(memory): add AnyValue

---
+++ core/src/cubos/core/memory/any_value.cpp
@@ -0,0 +1,86 @@
+#include <new>
+
+#include <cubos/core/log.hpp>
+#include <cubos/core/memory/any_value.hpp>
+#include <cubos/core/memory/move.hpp>
+#include <cubos/core/reflection/traits/constructible.hpp>
+#include <cubos/core/reflection/type.hpp>
+
+using cubos::core::memory::AnyValue;
+using cubos::core::reflection::ConstructibleTrait;
+using cubos::core::reflection::Type;
+
+AnyValue::~AnyValue()
+{
+    // Might have been moved, we must check it here.
+    if (mValue != nullptr)
+    {
+        const auto& trait = mType.get<ConstructibleTrait>();
+        trait.destruct(mValue);
+        operator delete(mValue, static_cast<std::align_val_t>(trait.alignment()));
+    }
+}
+
+AnyValue::AnyValue(AnyValue&& other) noexcept
+    : mType(other.mType)
+    , mValue(other.mValue)
+{
+    other.mValue = nullptr;
+}
+
+AnyValue AnyValue::defaultConstruct(const Type& type) noexcept
+{
+    CUBOS_ASSERT(type.has<ConstructibleTrait>(), "Type must be constructible");
+    const auto& trait = type.get<ConstructibleTrait>();
+    CUBOS_ASSERT(trait.hasDefaultConstruct(), "Type must be default constructible");
+
+    AnyValue any{type};
+    trait.defaultConstruct(any.get());
+    return move(any);
+}
+
+AnyValue AnyValue::copyConstruct(const Type& type, const void* value) noexcept
+{
+    CUBOS_ASSERT(type.has<ConstructibleTrait>(), "Type must be constructible");
+    const auto& trait = type.get<ConstructibleTrait>();
+    CUBOS_ASSERT(trait.hasCopyConstruct(), "Type must be copy constructible");
+
+    AnyValue any{type};
+    trait.copyConstruct(any.get(), value);
+    return move(any);
+}
+
+AnyValue AnyValue::moveConstruct(const Type& type, void* value) noexcept
+{
+    CUBOS_ASSERT(type.has<ConstructibleTrait>(), "Type must be constructible");
+    const auto& trait = type.get<ConstructibleTrait>();
+    CUBOS_ASSERT(trait.hasMoveConstruct(), "Type must be move constructible");
+
+    AnyValue any{type};
+    trait.moveConstruct(any.get(), value);
+    return move(any);
+}
+
+const Type& AnyValue::type() const
+{
+    return mType;
+}
+
+void* AnyValue::get()
+{
+    return mValue;
+}
+
+const void* AnyValue::get() const
+{
+    return mValue;
+}
+
+AnyValue::AnyValue(const Type& type) noexcept
+    : mType(type)
+{
+    const auto& trait = type.get<ConstructibleTrait>();
+    mValue = operator new(trait.size(), static_cast<std::align_val_t>(trait.alignment()), std::nothrow);
+
+    CUBOS_ASSERT(mValue != nullptr, "Could not allocate memory for value of type '{}'", type.name());
+}
---
+++ core/tests/memory/any_value.cpp
@@ -0,0 +1,52 @@
+#include <doctest/doctest.h>
+
+#include <cubos/core/memory/any_value.hpp>
+#include <cubos/core/reflection/external/primitives.hpp>
+#include <cubos/core/reflection/type.hpp>
+
+#include "../utils.hpp"
+
+TEST_CASE("memory::AnyValue")
+{
+    using cubos::core::memory::AnyValue;
+    using cubos::core::reflection::reflect;
+
+    SUBCASE("with integers")
+    {
+        SUBCASE("defaultConstruct")
+        {
+            const auto any = AnyValue::defaultConstruct(reflect<int>());
+            CHECK(any.type().is<int>());
+            CHECK(*static_cast<const int*>(any.get()) == 0);
+        }
+
+        SUBCASE("copyConstruct")
+        {
+            int value = 1337;
+            auto any = AnyValue::copyConstruct(reflect<int>(), &value);
+            CHECK(any.type().is<int>());
+            CHECK(*static_cast<int*>(any.get()) == value);
+        }
+
+        SUBCASE("moveConstruct")
+        {
+            int value = 1337;
+            auto any = AnyValue::moveConstruct(reflect<int>(), &value);
+            CHECK(any.type().is<int>());
+            CHECK(*static_cast<int*>(any.get()) == value);
+        }
+    }
+
+    SUBCASE("detect destructor")
+    {
+        bool destructed = false;
+        DetectDestructor detector{&destructed};
+
+        {
+            auto any = AnyValue::moveConstruct(reflect<DetectDestructor>(), &detector);
+            CHECK_FALSE(destructed);
+        }
+
+        CHECK(destructed);
+    }
+}
