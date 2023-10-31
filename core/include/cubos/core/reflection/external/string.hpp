142dfc43981f7ad9aca6319bb9db2cc200abbf57
Ricardo Antunes <me@riscadoa.com>
2023-10-31 19:07:28 +0000

    feat(reflection): implement for std::string

---
+++ core/include/cubos/core/reflection/external/string.hpp
@@ -0,0 +1,11 @@
+/// @file
+/// @brief Reflection declaration for `std::string`.
+/// @ingroup core-reflection
+
+#pragma once
+
+#include <string>
+
+#include <cubos/core/reflection/reflect.hpp>
+
+CUBOS_REFLECT_EXTERNAL_DECL(std::string);
