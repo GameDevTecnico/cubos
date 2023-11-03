be1b8d99c2ce08c51b43246f164e9aba7f252453
Ricardo Antunes <me@riscadoa.com>
2023-11-03 16:50:40 +0000

    feat(serialization): add Deserializer

---
+++ core/src/cubos/core/data/des/deserializer.cpp
@@ -0,0 +1,34 @@
+#include <cubos/core/data/des/deserializer.hpp>
+#include <cubos/core/log.hpp>
+#include <cubos/core/reflection/type.hpp>
+
+using cubos::core::data::Deserializer;
+
+bool Deserializer::read(const reflection::Type& type, void* value)
+{
+    if (auto it = mHooks.find(&type); it != mHooks.end())
+    {
+        if (!it->second(value))
+        {
+            CUBOS_WARN("Deserialization hook for type '{}' failed", type.name());
+            return false;
+        }
+    }
+    else if (!this->decompose(type, value))
+    {
+        CUBOS_WARN("Deserialization decomposition for type '{}' failed", type.name());
+        return false;
+    }
+
+    return true;
+}
+
+void Deserializer::hook(const reflection::Type& type, Hook hook)
+{
+    if (auto it = mHooks.find(&type); it != mHooks.end())
+    {
+        CUBOS_WARN("Hook for type '{}' already exists, overwriting", type.name());
+    }
+
+    mHooks.emplace(&type, memory::move(hook));
+}
