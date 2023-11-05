5f51e476e5757e7b3d0859b9ee4c4eb172a26e3e
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-11-05 20:11:36 +0000

    fix(physics): remove damping and gravity from velocity component and add folder for components

---
+++ engine/include/cubos/engine/physics/resources/damping.hpp
@@ -0,0 +1,15 @@
+/// @file
+/// @brief Resource @ref cubos::engine::Damping.
+/// @ingroup physics-plugin
+
+#pragma once
+
+namespace cubos::engine
+{
+    /// @brief Resource which holds the damping value for integration.
+    /// @ingroup physics-plugin
+    struct Damping
+    {
+        float value = 0.99F;
+    };
+} // namespace cubos::engine
