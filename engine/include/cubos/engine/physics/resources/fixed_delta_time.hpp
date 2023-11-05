5159b6b496d232abc36aada747f901d74718bf80
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-11-05 20:15:05 +0000

    feat(physics): implement initial xpbd algorithm

---
+++ engine/include/cubos/engine/physics/resources/fixed_delta_time.hpp
@@ -0,0 +1,15 @@
+/// @file
+/// @brief Resource @ref cubos::engine::FixedDeltaTime.
+/// @ingroup physics-plugin
+
+#pragma once
+
+namespace cubos::engine
+{
+    /// @brief Resouce which holds the value of the fixed delta for the physics update.
+    /// @ingroup physics-plugin
+    struct FixedDeltaTime
+    {
+        float value = 1.0f/100.0F;
+    };
+} // namespace cubos::engine
