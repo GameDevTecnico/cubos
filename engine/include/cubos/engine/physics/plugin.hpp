d5b62824ace20e2f1811c4d2805d99cb0fb016e8
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-11-09 17:03:42 +0000

    feat(physics): add separate plugin to apply gravity

--- engine/include/cubos/engine/physics/plugin.hpp
+++ engine/include/cubos/engine/physics/plugin.hpp
@@ -10,10 +10,11 @@
 
 #include <cubos/engine/cubos.hpp>
 
+#include <cubos/engine/physics/plugins/gravity.hpp>
+
 #include <cubos/engine/physics/resources/accumulator.hpp>
 #include <cubos/engine/physics/resources/damping.hpp>
 #include <cubos/engine/physics/resources/fixed_delta_time.hpp>
-#include <cubos/engine/physics/resources/gravity.hpp>
 #include <cubos/engine/physics/resources/substeps.hpp>
 
 #include <cubos/engine/physics/components/physics_velocity.hpp>
@@ -36,7 +37,6 @@ namespace cubos::engine
     /// - @ref FixedDeltaTime - holds the value of the fixed delta for the physics update.
     /// - @ref Accumulator - holds the progress to the next integration.
     /// - @ref Damping - holds the damping value for integration.
-    /// - @ref Gravity - holds the global value of gravity.
     /// - @ref Substeps - holds the amount of substeps for the physics update.
     ///
     /// ## Components
