79fd46da285ff89862ec018804d2165fb680bf9a
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-11-09 17:03:42 +0000

    feat(physics): add separate plugin to apply gravity

---
+++ engine/include/cubos/engine/physics/plugins/gravity.hpp
@@ -0,0 +1,26 @@
+/// @file
+/// @brief Gravity plugin, resource @ref cubos::engine::Gravity.
+/// @ingroup physics-plugin
+
+#pragma once
+
+#include <cubos/engine/cubos.hpp>
+
+#include <cubos/engine/physics/resources/gravity.hpp>
+
+#include <cubos/engine/physics/components/physics_velocity.hpp>
+#include <cubos/engine/physics/components/physics_mass.hpp>
+
+namespace cubos::engine
+{
+    /// @defgroup physics-plugin Physics
+    /// @ingroup engine
+    /// @brief Adds gravity to entities.
+    /// ## Resources
+    /// - @ref Gravity - holds the global value of gravity.
+
+    /// @brief Plugin entry function.
+    /// @param cubos @b CUBOS. main class
+    /// @ingroup physics-plugin
+    void gravityPlugin(Cubos& cubos);
+} // namespace cubos::engine
---
+++ engine/src/cubos/engine/physics/gravity.cpp
@@ -0,0 +1,32 @@
+#include <cubos/core/ecs/system/query.hpp>
+
+#include <cubos/engine/physics/plugins/gravity.hpp>
+
+#include <glm/glm.hpp>
+
+using cubos::core::ecs::Query;
+using cubos::core::ecs::Read;
+using cubos::core::ecs::Write;
+
+using namespace cubos::engine;
+
+static void applyGravitySystem(Query<Write<PhysicsVelocity>, Read<PhysicsMass>> query, Read<Gravity> gravity)
+{
+    for (auto [entity, velocity, mass] : query)
+    {
+        if (mass->inverseMass <= 0.0f) return;
+
+        // Apply gravity force
+        glm::vec3 gravitationForce = mass->mass * gravity->value;
+
+        velocity->force = velocity->force + gravitationForce;
+    }
+}
+
+void cubos::engine::gravityPlugin(Cubos& cubos)
+{
+    cubos.addResource<Gravity>();
+
+    cubos.system(applyGravitySystem).tagged("cubos.physics.apply_forces");
+}
+//reset forces??
