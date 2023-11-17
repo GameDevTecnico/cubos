836e4f19740d2878350bbce9b993fe092ec200f3
roby2014 <robyawyx@gmail.com>
2023-11-17 11:08:01 +0000

    feat(reflection): impl nullable trait

---
+++ core/include/cubos/core/reflection/traits/nullable.hpp
@@ -0,0 +1,41 @@
+/// @file
+/// @brief Class @ref cubos::core::reflection::NullableTrait.
+/// @ingroup core-reflection
+
+#pragma once
+
+#include <cubos/core/reflection/reflect.hpp>
+
+namespace cubos::core::reflection
+{
+    /// @brief Used to manipulate values of null-representable types.
+    /// @see See @ref examples-core-reflection-traits-nullable for an example of using this trait.
+    /// @ingroup core-reflection
+    class NullableTrait
+    {
+    public:
+        /// @brief Function pointer to check if a value represents null.
+        using IsNull = bool (*)(const void* instance);
+
+        /// @brief Function pointer to set a value to its null representation.
+        using SetToNull = void (*)(void* instance);
+
+        /// @brief Constructs.
+        /// @param into IsNull.
+        /// @param from SetToNull.
+        NullableTrait(IsNull isNull, SetToNull setToNull);
+
+        /// @brief Checks if the given value represents null.
+        /// @param instance Instance.
+        /// @return Whether the value represents null.
+        bool isNull(const void* instance) const;
+
+        /// @brief Sets a value to its null representation.
+        /// @param instance Instance.
+        void setToNull(void* instance) const;
+
+    private:
+        IsNull mIsNull;
+        SetToNull mSetToNull;
+    };
+} // namespace cubos::core::reflection
