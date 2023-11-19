0c1d35ae73a72679c335af0847756db4ba8cdb99
Ricardo Antunes <me@riscadoa.com>
2023-11-19 15:35:29 +0000

    build(core): remove fmt

--- core/include/cubos/core/data/old/debug_serializer.hpp
+++ core/include/cubos/core/data/old/debug_serializer.hpp
@@ -2,35 +2,11 @@
 
 #include <stack>
 
-#include <fmt/format.h>
-
 #include <cubos/core/data/old/serializer.hpp>
 #include <cubos/core/memory/buffer_stream.hpp>
 
 namespace cubos::core::data::old
 {
-    /// Used to wrap a value to be printed using the debug serializer.
-    /// Can be pretty-printed using the `p` option. Types can be printed using the `t` option.
-    ///
-    /// @details Example usage:
-    ///
-    ///    CUBOS_ERROR("Cool debug serialized object: {}", Debug(mySerializableObj));
-    ///    CUBOS_INFO("Pretty printed: {:p}", Debug(mySerializableObj));
-    ///    CUBOS_INFO("With types too: {:pt}", Debug(mySerializableObj));
-    ///
-    /// @tparam T The type of the value to wrap.
-    template <typename T>
-    struct Debug
-    {
-        /// @param value The value to debug.
-        inline Debug(const T& value)
-            : value(value)
-        {
-        }
-
-        const T& value;
-    };
-
     /// Implementation of the abstract Serializer class for debugging purposes.
     /// This class is used internally by the logging functions.
     class DebugSerializer : public Serializer
@@ -90,53 +66,3 @@ namespace cubos::core::data::old
         bool mTypeNames;          ///< Whether to print the type names.
     };
 } // namespace cubos::core::data::old
-
-// Add a formatter for Debug<T>.
-
-/// @cond
-template <typename T>
-struct fmt::formatter<cubos::core::data::old::Debug<T>> : formatter<string_view>
-{
-    bool pretty = false; ///< Whether to pretty print the data.
-    bool types = false;  ///< Whether to print the type name.
-
-    inline constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin())
-    {
-        const auto* it = ctx.begin();
-        const auto* end = ctx.end();
-        while (it != end)
-        {
-            if (*it == 'p')
-            {
-                this->pretty = true;
-            }
-            else if (*it == 't')
-            {
-                this->types = true;
-            }
-            else if (*it != '}')
-            {
-                throw format_error("invalid format");
-            }
-            else
-            {
-                break;
-            }
-            ++it;
-        }
-        return it;
-    }
-
-    template <typename FormatContext>
-    inline auto format(const cubos::core::data::old::Debug<T>& dbg, FormatContext& ctx) -> decltype(ctx.out())
-    {
-        auto stream = cubos::core::memory::BufferStream(32);
-        cubos::core::data::old::DebugSerializer serializer(stream, this->pretty, this->types);
-        serializer.write(dbg.value, nullptr);
-        stream.put('\0');
-        // Skip the '?: ' prefix.
-        auto result = std::string(static_cast<const char*>(stream.getBuffer()) + 3);
-        return formatter<string_view>::format(string_view(result), ctx);
-    }
-};
-/// @endcond
