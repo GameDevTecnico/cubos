9bb4d0bc0240e4ba8cf584b174c78d4b79371d8a
Ricardo Antunes <me@riscadoa.com>
2023-11-19 15:24:25 +0000

    feat(reflection): add StringConversionTrait to std::string

--- core/src/cubos/core/reflection/external/string.cpp
+++ core/src/cubos/core/reflection/external/string.cpp
@@ -1,8 +1,15 @@
 #include <cubos/core/reflection/external/string.hpp>
 #include <cubos/core/reflection/traits/constructible.hpp>
+#include <cubos/core/reflection/traits/string_conversion.hpp>
 #include <cubos/core/reflection/type.hpp>
 
 CUBOS_REFLECT_EXTERNAL_IMPL(std::string)
 {
-    return Type::create("std::string").with(ConstructibleTrait::typed<std::string>().withBasicConstructors().build());
+    return Type::create("std::string")
+        .with(ConstructibleTrait::typed<std::string>().withBasicConstructors().build())
+        .with(StringConversionTrait{[](const void* instance) { return *static_cast<const std::string*>(instance); },
+                                    [](void* instance, const std::string& string) {
+                                        *static_cast<std::string*>(instance) = string;
+                                        return true;
+                                    }});
 }
--- core/tests/reflection/external/string.cpp
+++ core/tests/reflection/external/string.cpp
@@ -4,10 +4,12 @@
 #include <cubos/core/reflection/traits/constructible.hpp>
 
 #include "../traits/constructible.hpp"
+#include "../traits/string_conversion.hpp"
 
 TEST_CASE("reflection::reflect<std::string>()")
 {
     std::string value = "Hello, world!";
     CHECK(reflect<std::string>().name() == "std::string");
     testConstructible<std::string>(&value);
+    testStringConversion<std::string>({{"", ""}, {"Hello World!", "Hello World!"}}, {});
 }
