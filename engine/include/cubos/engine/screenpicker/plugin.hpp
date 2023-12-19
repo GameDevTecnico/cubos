0b11222d3bfe7c9d1c18139f16ee844b3bf859b3
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2023-12-19 21:47:22 +0000

    docs(rendering): add screenPickerPlugin docs

--- engine/include/cubos/engine/screenpicker/plugin.hpp
+++ engine/include/cubos/engine/screenpicker/plugin.hpp
@@ -1,8 +1,36 @@
+/// @dir
+/// @brief @ref screenpicker-plugin plugin directory.
+
+/// @file
+/// @brief Plugin entry point.
+/// @ingroup screenpicker-plugin
+
 #pragma once
 
 #include <cubos/engine/cubos.hpp>
 
 namespace cubos::engine
 {
+    /// @defgroup screenpicker-plugin ScreenPicker
+    /// @ingroup engine
+    /// @brief Used to select entities and gizmos by clicking them.
+    ///
+    /// ## Resources
+    /// - @ref ScreenPicker - provides a texture to store entity and gizmo ids.
+    ///
+    /// ## Startup tags
+    /// - `cubos.screenpicker.init` - the ScreenPicker resource is initialized, after `cubos.window.init`
+    ///
+    /// ## Tags
+    /// - `cubos.screenpicker.clear` - the picking texture is cleared, before `cubos.renderer.draw`
+    /// - `cubos.screenpicker.resize` - window resize events are handled and the ScreenPicker texture is resized, after
+    /// `cubos.window.poll` and before `cubos.screenpicker.clear`.
+    ///
+    /// ## Dependencies
+    /// - @ref window-plugin
+
+    /// @brief Plugin entry function.
+    /// @param cubos @b CUBOS. main class.
+    /// @ingroup screenpicker-plugin
     void screenPickerPlugin(Cubos& cubos);
 } // namespace cubos::engine
--- engine/include/cubos/engine/screenpicker/screenpicker.hpp
+++ engine/include/cubos/engine/screenpicker/screenpicker.hpp
@@ -1,3 +1,7 @@
+/// @file
+/// @brief Resource @ref cubos::engine::ScreenPicker.
+/// @ingroup screenpicker-plugin
+
 #pragma once
 
 #include <cubos/core/gl/render_device.hpp>
@@ -6,22 +10,46 @@
 
 namespace cubos::engine
 {
+    /// @brief Resource which provides a texture to store entity/gizmo ids, for selection with a mouse.
+    ///
+    /// @ingroup screenpicker-plugin
     class ScreenPicker final
     {
     public:
+        /// @brief Initializes ScreenPicker for a render device.
+        /// @param renderDevice The current Render device being used.
+        /// @param size The size of the window in pixels.
         void init(cubos::core::gl::RenderDevice* currentRenderDevice, glm::ivec2 size);
+
+        /// @brief Resizes the picking texture.
+        /// @param size New size of the texture.
         void resizeTexture(glm::ivec2 size);
+
+        /// @brief Clears the picking texture.
         void clearTexture();
+
+        /// @brief Returns the framebuffer used to draw to the picking texture.
+        /// @return Framebuffer used to draw to the picking texture.
         core::gl::Framebuffer framebuffer();
+
+        /// @brief Returns the identifier associated to the pixel in the coords (x, y).
+        /// @param x Coordinate of pixel in x axis
+        /// @param y Coordinate of pixel in y axis
+        /// @return Entity or gizmo identifier associated to the pixel in the coords (x, y).
         uint32_t at(int x, int y) const;
+
+        /// @brief Returns the size of the picking texture in pixels.
+        /// @return Size of the picking texture in pixels.
         glm::uvec2 size() const;
 
     private:
-        cubos::core::gl::RenderDevice* renderDevice;
-        core::gl::Framebuffer idFramebuffer;
-        core::gl::Texture2D idTexture;
-        glm::ivec2 textureSize;
+        cubos::core::gl::RenderDevice* renderDevice; ///< Active render device.
+        core::gl::Framebuffer idFramebuffer;         ///< Framebuffer used to draw to the picking texture.
+        core::gl::Texture2D idTexture; ///< Picking texture, stores entity/gizmo ids for each pixel on the screen.
+        glm::ivec2 textureSize;        ///< Size of the picking texture in pixels.
 
+        /// @brief Creates the picking texture with a specified size.
+        /// @param size Size of the picking texture in pixels.
         void initIdTexture(glm::ivec2 size);
     };
 } // namespace cubos::engine
