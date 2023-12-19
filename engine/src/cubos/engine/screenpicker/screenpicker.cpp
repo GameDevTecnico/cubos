240231b40f32366285a1be64b0f5584bff54b76f
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2023-12-19 11:22:00 +0000

    feat(rendering): add screenPickerPlugin

---
+++ engine/src/cubos/engine/screenpicker/screenpicker.cpp
@@ -0,0 +1,76 @@
+#include <cubos/core/gl/render_device.hpp>
+
+#include <cubos/engine/screenpicker/screenpicker.hpp>
+
+using cubos::engine::ScreenPicker;
+
+using namespace cubos::core::gl;
+
+void ScreenPicker::init(cubos::core::gl::RenderDevice* currentRenderDevice, glm::ivec2 size)
+{
+    renderDevice = currentRenderDevice;
+    initIdTexture(size);
+}
+
+void ScreenPicker::resizeTexture(glm::ivec2 size)
+{
+    initIdTexture(size);
+}
+
+void ScreenPicker::clearTexture()
+{
+    auto* texBuffer =
+        new uint16_t[(std::size_t)textureSize.x * (std::size_t)textureSize.y * 2U];
+
+    std::fill(texBuffer, texBuffer + (std::size_t)textureSize.x * (std::size_t)textureSize.y * 2U, UINT16_MAX);
+
+    idTexture->update(0, 0, textureSize.x, textureSize.y, texBuffer);
+
+    delete[] texBuffer;
+}
+
+Framebuffer ScreenPicker::framebuffer()
+{
+    return idFramebuffer;
+}
+
+uint32_t ScreenPicker::at(int x, int y) const
+{
+    auto* texBuffer =
+        new uint16_t[(std::size_t)textureSize.x * (std::size_t)textureSize.y * 2U];
+
+    idTexture->read(texBuffer);
+
+    uint16_t r = texBuffer[(ptrdiff_t)(y * textureSize.x + x) * 2U];
+    uint16_t g = texBuffer[(ptrdiff_t)(y * textureSize.x + x) * 2U + 1U];
+
+    uint32_t id = (static_cast<uint32_t>(r) << 16U) | g;
+
+    delete[] texBuffer;
+
+    return id;
+}
+
+glm::uvec2 ScreenPicker::size() const
+{
+    return static_cast<glm::uvec2>(textureSize);
+}
+
+void ScreenPicker::initIdTexture(glm::ivec2 size)
+{
+    Texture2DDesc texDesc;
+    texDesc.width = (std::size_t)size.x;
+    texDesc.height = (std::size_t)size.y;
+    texDesc.usage = Usage::Dynamic;
+    texDesc.format = TextureFormat::RG16UInt;
+
+    idTexture = renderDevice->createTexture2D(texDesc);
+
+    FramebufferDesc frameDesc;
+    frameDesc.targetCount = 1;
+    frameDesc.targets[0].setTexture2DTarget(idTexture);
+
+    idFramebuffer = renderDevice->createFramebuffer(frameDesc);
+
+    textureSize = size;
+}
