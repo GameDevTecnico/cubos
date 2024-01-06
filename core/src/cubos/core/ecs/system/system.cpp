0b2b2dc40f7c92479dcbf69ac3a9467bf1a4130b
Diogo Mendonça <diogo.cunhamen@gmail.com>
2024-01-06 14:04:52 +0000

    fix(tools): remove direct world access on transform gizmo

--- core/src/cubos/core/ecs/system/system.cpp
+++ core/src/cubos/core/ecs/system/system.cpp
@@ -4,6 +4,12 @@ using namespace cubos::core::ecs;
 
 bool SystemInfo::valid() const
 {
+    if (this->usesWorld)
+    {
+        return !this->usesCommands && this->resourcesRead.empty() && this->resourcesWritten.empty() &&
+               this->componentsRead.empty() && this->componentsWritten.empty();
+    }
+
     for (const auto& rsc : this->resourcesRead)
     {
         if (this->resourcesWritten.contains(rsc))
