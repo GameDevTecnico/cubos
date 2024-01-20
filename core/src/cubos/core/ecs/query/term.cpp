6cca63f8d4acc4402c5d5299958a2010ebf615b2
Ricardo Antunes <me@riscadoa.com>
2024-01-20 21:41:53 +0000

    feat(ecs): add QueryTerm::resolve

--- core/src/cubos/core/ecs/query/term.cpp
+++ core/src/cubos/core/ecs/query/term.cpp
@@ -1,4 +1,5 @@
 #include <cubos/core/ecs/query/term.hpp>
+#include <cubos/core/log.hpp>
 
 using cubos::core::ecs::QueryTerm;
 
@@ -42,4 +43,135 @@ bool QueryTerm::isEntity() const
 bool QueryTerm::isComponent(const Types& types) const
 {
     return type != DataTypeId::Invalid && types.isComponent(type);
+}
+
+std::vector<QueryTerm> QueryTerm::resolve(const Types& types, const std::vector<QueryTerm>& baseTerms,
+                                          std::vector<QueryTerm>& otherTerms)
+{
+    // This code assumes otherTerms comes from the query argument types, and thus, all of its targets
+    // should be unspecified (-1).
+
+    std::vector<QueryTerm> terms{};
+    int defaultTarget = 0;
+
+    // For each base term.
+    auto otherTermIt = otherTerms.begin();
+    for (auto baseTerm : baseTerms)
+    {
+        // Iterate over the remaining other terms until one with the same type is found.
+        for (; otherTermIt != otherTerms.end(); ++otherTermIt)
+        {
+            auto& otherTerm = *otherTermIt;
+
+            // The 'other' term has the same type as the 'base' term, we can treat them as one.
+            if (baseTerm.type == otherTerm.type)
+            {
+                // Merge data from the other term into the base term.
+                if (baseTerm.isEntity())
+                {
+                    CUBOS_ASSERT(otherTerm.entity.target == -1); // See comment at the beginning of the function.
+
+                    if (baseTerm.entity.target == -1)
+                    {
+                        baseTerm.entity.target = defaultTarget;
+                    }
+
+                    otherTerm.entity.target = baseTerm.entity.target;
+                    ++otherTermIt;
+                    break;
+                }
+                else if (baseTerm.isComponent(types) && baseTerm.component.without == otherTerm.component.without)
+                {
+                    CUBOS_ASSERT(otherTerm.component.target == -1); // See comment at the beginning of the function.
+
+                    if (baseTerm.entity.target == -1)
+                    {
+                        baseTerm.entity.target = defaultTarget;
+                    }
+
+                    baseTerm.component.optional = otherTerm.component.optional;
+                    otherTerm.entity.target = baseTerm.entity.target;
+                    ++otherTermIt;
+                    break;
+                }
+            }
+
+            // Change the other term's target to the default, as it was unspecified.
+            if (otherTerm.isEntity())
+            {
+                CUBOS_ASSERT(otherTerm.entity.target == -1); // See comment at the beginning of the function.
+                otherTerm.entity.target = defaultTarget;
+            }
+            else if (otherTerm.isComponent(types))
+            {
+                CUBOS_ASSERT(otherTerm.component.target == -1); // See comment at the beginning of the function.
+                otherTerm.component.target = defaultTarget;
+            }
+            else
+            {
+                CUBOS_UNREACHABLE();
+            }
+
+            // Add it to the result.
+            terms.emplace_back(otherTerm);
+        }
+
+        // Update either the defaultTarget or the term's target, depending on it being specified.
+        if (baseTerm.isEntity())
+        {
+            if (baseTerm.entity.target == -1)
+            {
+                baseTerm.entity.target = defaultTarget;
+            }
+            else
+            {
+                defaultTarget = baseTerm.entity.target;
+            }
+        }
+        else if (baseTerm.isComponent(types))
+        {
+            if (baseTerm.component.target == -1)
+            {
+                baseTerm.component.target = defaultTarget;
+            }
+            else
+            {
+                defaultTarget = baseTerm.component.target;
+            }
+        }
+        else
+        {
+            CUBOS_UNREACHABLE();
+        }
+
+        // Add the base term to the result.
+        terms.emplace_back(baseTerm);
+    }
+
+    // Add the remaining other terms.
+    for (; otherTermIt != otherTerms.end(); ++otherTermIt)
+    {
+        auto& otherTerm = *otherTermIt;
+
+        // Change the other term's target to the default, as it was unspecified.
+        if (otherTerm.isEntity())
+        {
+            CUBOS_ASSERT(otherTerm.entity.target == -1); // See comment at the beginning of the function.
+            otherTerm.entity.target = defaultTarget;
+        }
+        else if (otherTerm.isComponent(types))
+        {
+            CUBOS_ASSERT(otherTerm.component.target == -1); // See comment at the beginning of the function.
+            otherTerm.component.target = defaultTarget;
+        }
+        else
+        {
+            CUBOS_UNREACHABLE();
+        }
+
+        // Add it to the result.
+        terms.emplace_back(otherTerm);
+    }
+
+    return terms;
 }
