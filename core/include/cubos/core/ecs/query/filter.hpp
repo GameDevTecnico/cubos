35b4426d9915c008860616c7cf25439541076fd5
Ricardo Antunes <me@riscadoa.com>
2024-01-21 10:38:42 +0000

    feat(ecs): add QueryFilter

---
+++ core/include/cubos/core/ecs/query/filter.hpp
@@ -0,0 +1,149 @@
+/// @file
+/// @brief Class @ref cubos::core::ecs::QueryFilter.
+/// @ingroup core-ecs-query
+
+#pragma once
+
+#include <vector>
+
+#include <cubos/core/ecs/entity/archetype_id.hpp>
+#include <cubos/core/ecs/entity/entity.hpp>
+#include <cubos/core/ecs/query/term.hpp>
+
+namespace cubos::core::ecs
+{
+    class World;
+
+    /// @brief Used to find matches for the given query terms. Essentially contains the non-templated part of the query
+    /// logic.
+    /// @ingroup core-ecs-query
+    class QueryFilter
+    {
+    public:
+        /// @brief Maximum number of targets per match allowed in a single query.
+        static constexpr int MaxTargetCount = 1;
+
+        /// @brief Maximum number of cursors per query.
+        static constexpr int MaxCursorCount = MaxTargetCount;
+
+        /// @brief Can be iterated to view the query matches.
+        class View;
+
+        /// @brief Constructs.
+        /// @param world World being queried.
+        /// @param terms Query terms.
+        QueryFilter(World& world, const std::vector<QueryTerm>& terms);
+
+        /// @brief Gets the cursor index for the given term index.
+        /// @param termIndex Term index.
+        /// @return Cursor index.
+        std::size_t cursorIndex(std::size_t termIndex) const;
+
+        /// @brief Fetches any new matching archetypes that have been added since the last call to this function.
+        void update();
+
+        /// @brief Returns a view which can be used to iterate over the matches.
+        View view();
+
+    private:
+        /// @brief Holds the necessary data for each target.
+        struct Target
+        {
+            /// @brief Archetype which holds only the required components for this target.
+            ArchetypeId baseArchetype{ArchetypeId::Empty};
+
+            /// @brief Archetypes which match the target, found through calls to @ref update().
+            std::vector<ArchetypeId> archetypes;
+
+            /// @brief How many archetypes have already been seen through @ref ArchetypeGraph::collect.
+            std::size_t seenCount{0};
+        };
+
+        World& mWorld;
+
+        std::vector<QueryTerm> mTerms;
+        std::vector<std::size_t> mTermCursors;
+        int mCursorCount{1};
+
+        Target mTargets[MaxTargetCount];
+        int mTargetCount{1};
+    };
+
+    class QueryFilter::View
+    {
+    public:
+        /// @brief Used to iterate over the query matches.
+        class Iterator;
+
+        /// @brief Constructs.
+        /// @param filter Query filter.
+        View(QueryFilter& filter);
+
+        /// @brief Returns a new view equal to this one but with the given target pinned to the given entity.
+        ///
+        /// Effectively this filters out all matches where the given target isn't the given entity.
+        ///
+        /// @param target Target index.
+        /// @param entity Entity.
+        /// @return View.
+        View&& pin(int target, Entity entity) &&;
+
+        /// @brief Returns an iterator pointing to the first query match.
+        /// @return Iterator.
+        Iterator begin();
+
+        /// @brief Returns an out of bounds iterator representing the end of the query matches.
+        /// @return Iterator.
+        Iterator end();
+
+    private:
+        QueryFilter& mFilter;
+        Entity mPins[MaxTargetCount];
+    };
+
+    class QueryFilter::View::Iterator
+    {
+    public:
+        /// @brief Constructs.
+        /// @param view Query filter view.
+        /// @param end Whether the iterator should point to the end of the matches.
+        Iterator(View& view, bool end);
+
+        /// @brief Copy constructs.
+        /// @param other Other iterator.
+        Iterator(const Iterator& other) = default;
+
+        /// @brief Compares two iterators.
+        /// @param other Other iterator.
+        /// @return Whether the iterators point to the same match.
+        bool operator==(const Iterator& other) const;
+
+        /// @brief Advances the iterator.
+        /// @note Aborts if out of bounds.
+        /// @return Reference to this.
+        Iterator& operator++();
+
+        /// @brief Gets the archetype of each target.
+        /// @return Target archetypes array.
+        const ArchetypeId* targetArchetypes() const;
+
+        /// @brief Gets the row number for each cursor.
+        /// @return Cursor row number array.
+        const std::size_t* cursorRows() const;
+
+    private:
+        /// @brief Advances the iterator's archetype until a non-empty one is found.
+        void findArchetype();
+
+        View& mView;
+
+        /// @brief Archetype of each target.
+        ArchetypeId mTargetArchetypes[MaxTargetCount];
+
+        /// @brief Index of the currently selected archetype.
+        std::size_t mArchetypeIndex;
+
+        /// @brief Row number of each cursor. The first mTargetCount cursors represent the targets.
+        std::size_t mCursorRows[MaxCursorCount];
+    };
+} // namespace cubos::core::ecs
---
+++ core/tests/ecs/query/filter.cpp
@@ -0,0 +1,188 @@
+#include <doctest/doctest.h>
+
+#include <cubos/core/ecs/entity/hash.hpp>
+#include <cubos/core/ecs/query/filter.hpp>
+
+#include "../utils.hpp"
+
+using cubos::core::reflection::reflect;
+
+using namespace cubos::core::ecs;
+
+TEST_CASE("ecs::QueryFilter")
+{
+    World world{};
+    setupWorld(world);
+    auto integerComponent = world.types().id(reflect<IntegerComponent>());
+    auto parentComponent = world.types().id(reflect<ParentComponent>());
+
+    // Create some entities.
+    auto e1 = world.create();
+    auto e2I = world.create();
+    world.components(e2I).add(IntegerComponent{2});
+    auto e3I = world.create();
+    world.components(e3I).add(IntegerComponent{3});
+    auto e4P = world.create();
+    world.components(e4P).add(ParentComponent{e1});
+
+    auto a = ArchetypeId::Empty;
+    auto aI = world.archetypeGraph().with(a, DenseColumnId::make(integerComponent));
+    auto aP = world.archetypeGraph().with(a, DenseColumnId::make(parentComponent));
+
+    SUBCASE("with a single target")
+    {
+        std::vector<QueryTerm> terms{};
+        bool no_terms = false;
+
+        SUBCASE("no terms")
+        {
+            no_terms = true;
+        }
+
+        SUBCASE("optional integer")
+        {
+            terms = {QueryTerm::makeOptComponent(integerComponent, 0)};
+        }
+
+        if (no_terms || !terms.empty())
+        {
+            QueryFilter filter{world, terms};
+
+            SUBCASE("no pins")
+            {
+                auto view = filter.view();
+                auto it = view.begin();
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == a);
+                ++it;
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == aI);
+                ++it;
+                REQUIRE(it.cursorRows()[0] == 1);
+                REQUIRE(it.targetArchetypes()[0] == aI);
+                ++it;
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == aP);
+                ++it;
+                REQUIRE(it == view.end());
+            }
+
+            SUBCASE("pinned on e1")
+            {
+                auto view = filter.view().pin(0, e1);
+                auto it = view.begin();
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == a);
+                ++it;
+                REQUIRE(it == view.end());
+            }
+
+            SUBCASE("pinned on e3I")
+            {
+                auto view = filter.view().pin(0, e3I);
+                auto it = view.begin();
+                REQUIRE(it.cursorRows()[0] == 1);
+                REQUIRE(it.targetArchetypes()[0] == aI);
+                ++it;
+                REQUIRE(it == view.end());
+            }
+
+            SUBCASE("pinned on e4P")
+            {
+                auto view = filter.view().pin(0, e4P);
+                auto it = view.begin();
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == aP);
+                ++it;
+                REQUIRE(it == view.end());
+            }
+        }
+
+        SUBCASE("with integer")
+        {
+            QueryFilter filter{world, {QueryTerm::makeWithComponent(integerComponent, 0)}};
+
+            SUBCASE("no pins")
+            {
+                auto view = filter.view();
+                auto it = view.begin();
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == aI);
+                ++it;
+                REQUIRE(it.cursorRows()[0] == 1);
+                REQUIRE(it.targetArchetypes()[0] == aI);
+                ++it;
+                REQUIRE(it == view.end());
+            }
+
+            SUBCASE("pinned on e3I")
+            {
+                auto view = filter.view().pin(0, e3I);
+                auto it = view.begin();
+                REQUIRE(it.cursorRows()[0] == 1);
+                REQUIRE(it.targetArchetypes()[0] == aI);
+                ++it;
+                REQUIRE(it == view.end());
+            }
+
+            SUBCASE("pinned on e4P")
+            {
+                auto view = filter.view().pin(0, e4P);
+                REQUIRE(view.begin() == view.end());
+            }
+        }
+
+        SUBCASE("without integer")
+        {
+            QueryFilter filter{world, {QueryTerm::makeWithoutComponent(integerComponent, 0)}};
+
+            SUBCASE("no pins")
+            {
+                auto view = filter.view();
+                auto it = view.begin();
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == a);
+                ++it;
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == aP);
+                ++it;
+                REQUIRE(it == view.end());
+            }
+
+            SUBCASE("pinned on e2I")
+            {
+                auto view = filter.view().pin(0, e3I);
+                REQUIRE(view.begin() == view.end());
+            }
+
+            SUBCASE("pinned on e4P")
+            {
+                auto view = filter.view().pin(0, e4P);
+                auto it = view.begin();
+                REQUIRE(it.cursorRows()[0] == 0);
+                REQUIRE(it.targetArchetypes()[0] == aP);
+                ++it;
+                REQUIRE(it == view.end());
+            }
+        }
+
+        SUBCASE("with both integer and parent")
+        {
+            QueryFilter filter{
+                world,
+                {QueryTerm::makeWithComponent(integerComponent, 0), QueryTerm::makeWithComponent(parentComponent, 0)}};
+
+            SUBCASE("no pins")
+            {
+                auto view = filter.view();
+                REQUIRE(view.begin() == view.end());
+            }
+
+            SUBCASE("pinned on e1")
+            {
+                auto view = filter.view().pin(0, e1);
+                REQUIRE(view.begin() == view.end());
+            }
+        }
+    }
+}
