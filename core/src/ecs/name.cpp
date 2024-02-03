b110fe8760af3776188db23ac14156e0b031593a
Diogo Miranda <diogomsmiranda@gmail.com>
2024-02-03 18:17:40 +0000

    feat(ecs): add Name component

---
+++ core/src/ecs/name.cpp
@@ -0,0 +1,11 @@
+#include <cubos/core/ecs/name.hpp>
+
+#include <cubos/core/ecs/reflection.hpp>
+#include <cubos/core/reflection/external/string.hpp>
+
+CUBOS_REFLECT_IMPL(cubos::core::ecs::Name)
+{
+    return core::ecs::TypeBuilder<Name>("cubos::core::ecs::Name")
+        .withField("name", &Name::value)
+        .build();
+}
