c52f5594a6ad30ae6064ff304d200342bfcb252b
Diogo Miranda <diogomsmiranda@gmail.com>
2024-02-03 18:19:34 +0000

    feat(tesseratos): add display of name component as name of the entity in world inspector.

--- tools/tesseratos/src/tesseratos/world_inspector/plugin.cpp
+++ tools/tesseratos/src/tesseratos/world_inspector/plugin.cpp
@@ -1,5 +1,8 @@
 #include <imgui.h>
 
+#include <cubos/core/ecs/name.hpp>
+#include <cubos/core/ecs/query/opt.hpp>
+
 #include <cubos/engine/imgui/plugin.hpp>
 
 #include <tesseratos/entity_selector/plugin.hpp>
@@ -7,6 +10,8 @@
 #include <tesseratos/world_inspector/plugin.hpp>
 
 using cubos::core::ecs::Entity;
+using cubos::core::ecs::Name;
+using cubos::core::ecs::Opt;
 using cubos::core::ecs::Query;
 using cubos::core::ecs::World;
 
@@ -17,34 +22,43 @@ void tesseratos::worldInspectorPlugin(Cubos& cubos)
     cubos.addPlugin(cubos::engine::imguiPlugin);
     cubos.addPlugin(toolboxPlugin);
     cubos.addPlugin(entitySelectorPlugin);
-    cubos.system("show World Inspector UI").tagged("cubos.imgui").call([](World& world, Query<Entity> query) {
-        if (!(world.write<Toolbox>().get().isOpen("World Inspector")))
-        {
-            return;
-        }
-
-        auto selector = world.write<EntitySelector>();
-
-        ImGui::Begin("World Inspector");
-        if (!ImGui::IsWindowCollapsed())
-        {
-            int n = 0;
-            for (auto [entity] : query)
+    cubos.system("show World Inspector UI")
+        .tagged("cubos.imgui")
+        .call([](World& world, Query<Entity, Opt<const Name&>> query) {
+            if (!(world.write<Toolbox>().get().isOpen("World Inspector")))
             {
-                ImGui::PushID(n);
-
-                ImGui::BulletText("%s", std::to_string(entity.index).c_str());
-
-                ImGui::SameLine();
-                if (ImGui::Button("Select"))
-                {
-                    selector.get().selection = entity;
-                }
-
-                n++;
-                ImGui::PopID();
+                return;
             }
-        }
-        ImGui::End();
-    });
+
+            auto selector = world.write<EntitySelector>();
+
+            ImGui::Begin("World Inspector");
+            if (!ImGui::IsWindowCollapsed())
+            {
+                int n = 0;
+                for (auto [entity, name] : query)
+                {
+                    ImGui::PushID(n);
+
+                    if (name.contains())
+                    {
+                        ImGui::BulletText("%s", name->value.c_str());
+                    }
+                    else
+                    {
+                        ImGui::BulletText("%s", std::to_string(entity.index).c_str());
+                    }
+
+                    ImGui::SameLine();
+                    if (ImGui::Button("Select"))
+                    {
+                        selector.get().selection = entity;
+                    }
+
+                    n++;
+                    ImGui::PopID();
+                }
+            }
+            ImGui::End();
+        });
 }
