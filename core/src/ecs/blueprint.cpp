9abb58f82ab811130c70b970714385486ae0e013
Diogo Miranda <diogomsmiranda@gmail.com>
2024-02-04 13:31:08 +0000

    fix(ecs): fix error with blueprint.cpp

--- core/src/ecs/blueprint.cpp
+++ core/src/ecs/blueprint.cpp
@@ -159,9 +159,9 @@ void Blueprint::instantiate(void* userData, Create create, Add add, Relate relat
     std::unordered_map<Entity, Entity, EntityHash> thisToInstance{};
     for (const auto& [entity, name] : mBimap)
     {
-        Name nameComponent{name};
-        add(userData, entity, AnyValue::moveConstruct(reflect<Name>(), (void*)&nameComponent));
         thisToInstance.emplace(entity, create(userData, name));
+        Name nameComponent{name};
+        add(userData, thisToInstance.at(entity), AnyValue::moveConstruct(reflect<Name>(), (void*)&nameComponent));
     }
 
     // Add copies of our components to their instantiated entities. Since the components themselves
