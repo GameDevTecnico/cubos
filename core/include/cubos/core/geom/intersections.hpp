99a5538f2eee695a8df8726be115b815fb73ad83
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-02-11 17:07:21 +0000

    fix(geom): fix collinear collision between rotated boxes

--- core/include/cubos/core/geom/intersections.hpp
+++ core/include/cubos/core/geom/intersections.hpp
@@ -16,14 +16,13 @@ namespace cubos::core::geom
 {
     /// @brief Contains info regarding an intersection between shapes.
     /// @ingroup core-geom
-    struct Intersect
+    struct Intersection
     {
         CUBOS_REFLECT;
 
-        cubos::core::ecs::Entity entity; ///< Entity to which the normal belongs
-        float penetration;               ///< Penetration depth of the collision.
-        glm::vec3 position;              ///< Position of contact on the surface of the entity.
-        glm::vec3 normal;                ///< Normal of contact on the surface of the entity.
+        float penetration;  ///< Penetration depth of the collision.
+        glm::vec3 position; ///< Position of contact on the surface of the entity.
+        glm::vec3 normal;   ///< Normal of contact on the surface of the entity.
     };
 
     /// @brief Computes the intersection between two box shapes.
@@ -31,8 +30,7 @@ namespace cubos::core::geom
     /// @param localToWorld1 Local to world matrix of the first entity.
     /// @param box2 Box shape of the second entity.
     /// @param localToWorld2 Local to world matrix of the second entity.
-    bool intersects(const cubos::core::ecs::Entity& ent1, const Box& box1, const glm::mat4& localToWorld1,
-                    const cubos::core::ecs::Entity& ent2, const Box& box2, const glm::mat4& localToWorld2,
-                    Intersect& intersect);
+    bool intersects(const Box& box1, const glm::mat4& localToWorld1, const Box& box2, const glm::mat4& localToWorld2,
+                    Intersection& intersect);
 
 } // namespace cubos::core::geom
