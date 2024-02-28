02f11e5fc9d36722a440b1510025c7b1ddf5656d
roby2014 <robyawyx@gmail.com>
2024-02-28 17:25:19 +0000

    portaudio sample

--- core/samples/al/main.cpp
+++ core/samples/al/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <stdio.h>
 
 #include <cubos/core/al/audio_device.hpp>
@@ -5,11 +7,22 @@
 int main()
 {
     auto audio = cubos::core::al::AudioDevice::create();
-    std::vector<std::string> devices;
-    audio->enumerateDevices(devices);
-    printf("All my sound devices:");
-    for (const auto& d : devices)
-    {
-        printf("- %s\n", d.c_str());
-    }
+
+    std::vector<cubos::core::al::DeviceInfo> devices;
+    audio->enumerateDevices(devices, true);
+
+    audio->init([](void* outputBuffer, unsigned long framesPerBuffer, unsigned long, void*) -> int {
+        float* out = static_cast<float*>(outputBuffer);
+        for (unsigned int i = 0; i < framesPerBuffer; i++)
+        {
+            *out++ = 0.5f * static_cast<float>(std::sin(2.0 * 3.141592653589793 * 440.0 * i / 44100.0));
+        }
+        return 0;
+    });
+    audio->start();
+
+    std::cout << "press enter to stop the sound";
+    std::cin.get();
+
+    audio->stop();
 }
