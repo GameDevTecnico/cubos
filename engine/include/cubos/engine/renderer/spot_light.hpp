7df4b9f3d08ee0c3bd88d781bbad2fcfd31355f7
Ricardo Antunes <me@riscadoa.com>
2024-03-20 10:29:01 +0000

    docs(rendering): clear up SpotLight's direction

--- engine/include/cubos/engine/renderer/spot_light.hpp
+++ engine/include/cubos/engine/renderer/spot_light.hpp
@@ -10,9 +10,7 @@
 
 namespace cubos::engine
 {
-    /// @brief Component which makes an entity emit a spot light.
-    /// @note Should be used with @ref LocalToWorld.
-    /// @todo In what direction does the spot light point for an identity transform?
+    /// @brief Component which makes an entity emit a spot light in the direction of its forward vector.
     /// @ingroup renderer-plugin
     struct SpotLight
     {
