4b25ef9c5d82b8ad3f39a9b40f2f4e1329d6998d
Ricardo Antunes <me@riscadoa.com>
2024-03-20 11:06:15 +0000

    feat(settings): stop using old serialization to load Settings

--- engine/src/settings/plugin.cpp
+++ engine/src/settings/plugin.cpp
@@ -1,6 +1,7 @@
+#include <nlohmann/json.hpp>
+
 #include <cubos/core/data/fs/file_system.hpp>
 #include <cubos/core/data/fs/standard_archive.hpp>
-#include <cubos/core/data/old/json_deserializer.hpp>
 #include <cubos/core/reflection/external/string.hpp>
 
 #include <cubos/engine/settings/plugin.hpp>
@@ -8,7 +9,6 @@
 using cubos::core::data::File;
 using cubos::core::data::FileSystem;
 using cubos::core::data::StandardArchive;
-using cubos::core::data::old::JSONDeserializer;
 
 CUBOS_DEFINE_TAG(cubos::engine::settingsTag);
 
@@ -41,6 +41,40 @@ static Settings loadFromArguments(const Arguments& args)
     return settings;
 }
 
+static void loadFromJSON(const std::string& prefix, const nlohmann::json& json, Settings& settings)
+{
+    for (auto it = json.begin(); it != json.end(); ++it)
+    {
+        const auto& key = it.key();
+        const auto& value = it.value();
+
+        if (value.is_object())
+        {
+            loadFromJSON(prefix + key + ".", value, settings);
+        }
+        else if (value.is_boolean())
+        {
+            settings.setBool(prefix + key, value);
+        }
+        else if (value.is_string())
+        {
+            settings.setString(prefix + key, value);
+        }
+        else if (value.is_number_integer())
+        {
+            settings.setInteger(prefix + key, value);
+        }
+        else if (value.is_number_float())
+        {
+            settings.setDouble(prefix + key, value);
+        }
+        else
+        {
+            CUBOS_ERROR("Unsupported type {} for setting {}", value.type_name(), prefix + key);
+        }
+    }
+}
+
 static Settings loadFromFile(std::string_view path)
 {
     // Mount the real settings file as `/settings.json`.
@@ -52,17 +86,33 @@ static Settings loadFromFile(std::string_view path)
     auto stream = FileSystem::open("/settings.json", File::OpenMode::Read);
     stream->readUntil(contents, nullptr);
 
-    // Parse it as JSON.
-    Settings settings{};
-    if (!contents.empty())
+    // If the file is empty, ignore it.
+    if (contents.empty())
     {
-        JSONDeserializer deserializer{contents};
-        deserializer.read(settings);
-        if (deserializer.failed())
-        {
-        }
+        return {};
     }
 
+    // Parse it as JSON.
+    nlohmann::json json{};
+    try
+    {
+        json = nlohmann::json::parse(contents);
+    }
+    catch (nlohmann::json::parse_error& e)
+    {
+        CUBOS_ERROR("Couldn't parse the settings file: {}", e.what());
+        return {};
+    }
+
+    if (!json.is_object())
+    {
+        CUBOS_ERROR("Expected root element in settings file to be an object, but got {}", json.type_name());
+        return {};
+    }
+
+    // Read the settings from the JSON.
+    Settings settings{};
+    loadFromJSON("", json, settings);
     return settings;
 }
 
