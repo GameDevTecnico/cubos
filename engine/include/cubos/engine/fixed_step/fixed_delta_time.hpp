3ba278ad349679fdffccd67e2a396381a1981249
Ricardo Antunes <me@riscadoa.com>
2024-03-20 09:09:22 +0000

    feat(physics): implement reflection for FixedDeltaTime

--- engine/include/cubos/engine/fixed_step/fixed_delta_time.hpp
+++ engine/include/cubos/engine/fixed_step/fixed_delta_time.hpp
@@ -4,12 +4,16 @@
 
 #pragma once
 
+#include <cubos/core/reflection/reflect.hpp>
+
 namespace cubos::engine
 {
     /// @brief Resource which holds the value of the fixed delta for the fixedStep plugin.
     /// @ingroup fixed-step-plugin
     struct FixedDeltaTime
     {
+        CUBOS_REFLECT;
+
         float value = 1.0F / 100.0F;
     };
 } // namespace cubos::engine
