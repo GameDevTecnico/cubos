510684bf29011ecbbdf5fa576118773d9bbfd482
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2024-03-21 19:38:47 +0000

    feat(rendering): add RenderTarget plugin

---
+++ engine/src/render/target/plugin.cpp
@@ -0,0 +1,33 @@
+#include <cubos/core/io/window.hpp>
+#include <cubos/core/reflection/external/primitives.hpp>
+
+#include <cubos/engine/render/target/plugin.hpp>
+#include <cubos/engine/render/target/target.hpp>
+#include <cubos/engine/window/plugin.hpp>
+
+using cubos::core::io::Window;
+
+CUBOS_DEFINE_TAG(cubos::engine::resizeRenderTargetTag);
+
+void cubos::engine::renderTargetPlugin(Cubos& cubos)
+{
+    cubos.depends(windowPlugin);
+
+    cubos.component<RenderTarget>();
+
+    cubos.tag(resizeRenderTargetTag);
+
+    cubos.system("resize render targets")
+        .tagged(resizeRenderTargetTag)
+        .call([](const Window& window, Query<RenderTarget&> query) {
+            for (auto [target] : query)
+            {
+                if (target.framebuffer == nullptr && window->framebufferSize() != target.size)
+                {
+                    target.size = window->framebufferSize();
+
+                    CUBOS_INFO("Resized RenderTarget to {}x{}", target.size.x, target.size.y);
+                }
+            }
+        });
+}
