c423494c4560bb733aa2b0e0e0172c6633910464
Ricardo Antunes <me@riscadoa.com>
2024-03-21 19:27:24 +0000

    feat(ecs): add SystemRegistry

---
+++ core/include/cubos/core/ecs/system/registry.hpp
@@ -0,0 +1,99 @@
+/// @file
+/// @brief Class @ref cubos::core::ecs::SystemRegistry.
+/// @ingroup core-ecs-system
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <cubos/core/ecs/system/system.hpp>
+
+namespace cubos::core::ecs
+{
+    /// @brief Identifies a system.
+    /// @ingroup core-ecs-system
+    struct SystemId
+    {
+        std::size_t inner; ///< System identifier.
+
+        /// @brief Compares two identifiers for equality.
+        /// @param other Other identifier.
+        /// @return Whether the two identifiers are equal.
+        bool operator==(const SystemId& other) const = default;
+    };
+
+    /// @brief Identifies a condition.
+    /// @ingroup core-ecs-system
+    struct ConditionId
+    {
+        std::size_t inner; ///< Condition identifier.
+
+        /// @brief Compares two identifiers for equality.
+        /// @param other Other identifier.
+        /// @return Whether the two identifiers are equal.
+        bool operator==(const ConditionId& other) const = default;
+    };
+
+    /// @brief Stores known systems and conditions.
+    /// @ingroup core-ecs-system
+    class SystemRegistry
+    {
+    public:
+        ~SystemRegistry();
+
+        /// @brief Constructs.
+        SystemRegistry() = default;
+
+        /// @brief Forbid copying.
+        SystemRegistry(const SystemRegistry&) = delete;
+
+        /// @brief Forbid copy assignment.
+        SystemRegistry& operator=(const SystemRegistry&) = delete;
+
+        /// @brief Registers the given system.
+        /// @param name Debug name.
+        /// @param system System.
+        /// @return System identifier.
+        SystemId add(std::string name, System<void> system);
+
+        /// @brief Registers the given condition.
+        /// @param name Debug name.
+        /// @param system System.
+        /// @return Condition identifier.
+        ConditionId add(std::string name, System<bool> condition);
+
+        /// @brief Unregisters the given system.
+        /// @param id System identifier.
+        void remove(SystemId id);
+
+        /// @brief Unregisters the given condition.
+        /// @param id Condition identifier.
+        void remove(ConditionId id);
+
+        /// @brief Gets the debug name of the given system.
+        /// @param id System identifier.
+        /// @return System name.
+        const std::string& name(SystemId id) const;
+
+        /// @brief Gets the debug name of the given condition.
+        /// @param id Condition identifier.
+        /// @return Condition name.
+        const std::string& name(ConditionId id) const;
+
+        /// @brief Gets the system with the given identifier.
+        /// @param id System identifier.
+        System<void>& system(SystemId id);
+
+        /// @brief Gets the condition with the given identifier.
+        /// @param id Condition identifier.
+        System<bool>& condition(ConditionId id);
+
+    private:
+        std::vector<std::string> mSystemNames;
+        std::vector<std::string> mConditionNames;
+
+        std::vector<System<void>*> mSystems;
+        std::vector<System<bool>*> mConditions;
+    };
+} // namespace cubos::core::ecs
---
+++ core/src/ecs/system/registry.cpp
@@ -0,0 +1,68 @@
+#include <cubos/core/ecs/system/registry.hpp>
+#include <cubos/core/log.hpp>
+
+using cubos::core::ecs::ConditionId;
+using cubos::core::ecs::System;
+using cubos::core::ecs::SystemId;
+using cubos::core::ecs::SystemRegistry;
+
+SystemRegistry::~SystemRegistry()
+{
+    for (auto* system : mSystems)
+    {
+        delete system;
+    }
+
+    for (auto* condition : mConditions)
+    {
+        delete condition;
+    }
+}
+
+SystemId SystemRegistry::add(std::string name, System<void> system)
+{
+    mSystems.push_back(new System<void>(std::move(system)));
+    mSystemNames.emplace_back(std::move(name));
+    return {mSystems.size() - 1};
+}
+
+ConditionId SystemRegistry::add(std::string name, System<bool> condition)
+{
+    mConditions.push_back(new System<bool>(std::move(condition)));
+    mConditionNames.emplace_back(std::move(name));
+    return {mConditions.size() - 1};
+}
+
+void SystemRegistry::remove(SystemId id)
+{
+    delete mSystems[id.inner];
+    mSystems[id.inner] = nullptr;
+}
+
+void SystemRegistry::remove(ConditionId id)
+{
+    delete mConditions[id.inner];
+    mConditions[id.inner] = nullptr;
+}
+
+const std::string& SystemRegistry::name(SystemId id) const
+{
+    return mSystemNames[id.inner];
+}
+
+const std::string& SystemRegistry::name(ConditionId id) const
+{
+    return mConditionNames[id.inner];
+}
+
+System<void>& SystemRegistry::system(SystemId id)
+{
+    CUBOS_ASSERT(mSystems[id.inner] != nullptr, "System was already removed");
+    return *mSystems[id.inner];
+}
+
+System<bool>& SystemRegistry::condition(ConditionId id)
+{
+    CUBOS_ASSERT(mConditions[id.inner] != nullptr, "Condition was already removed");
+    return *mConditions[id.inner];
+}
