77cd252ae5164ced215d23bbf75b333854dc726d
Ricardo Antunes <me@riscadoa.com>
2024-03-21 19:27:24 +0000

    feat(ecs): add SystemRegistry

---
+++ core/tests/ecs/system/registry.cpp
@@ -0,0 +1,27 @@
+#include <doctest/doctest.h>
+
+#include <cubos/core/ecs/system/registry.hpp>
+
+using namespace cubos::core::ecs;
+
+TEST_CASE("ecs::SystemRegistry")
+{
+    World world{};
+    SystemRegistry registry{};
+
+    auto systemId1 = registry.add("my system 1", System<void>::make(world, []() {}, {}));
+    REQUIRE(registry.name(systemId1) == "my system 1");
+    REQUIRE(registry.system(systemId1).access().dataTypes.empty());
+
+    auto conditionId1 = registry.add("my condition 1", System<bool>::make(world, []() { return false; }, {}));
+    REQUIRE(registry.name(conditionId1) == "my condition 1");
+    REQUIRE(registry.condition(conditionId1).access().dataTypes.empty());
+
+    auto systemId2 = registry.add("my system 2", System<void>::make(world, []() {}, {}));
+    REQUIRE(registry.name(systemId2) == "my system 2");
+    REQUIRE(systemId1 != systemId2);
+
+    auto conditionId2 = registry.add("my condition 2", System<bool>::make(world, []() { return false; }, {}));
+    REQUIRE(registry.name(conditionId2) == "my condition 2");
+    REQUIRE(conditionId1 != conditionId2);
+}
