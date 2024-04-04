d4296b21c410aba91b3d83a2a4a402a6a7b58931
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2024-04-04 21:46:28 +0100

    feat(rendering): add RenderPicker plugin

---
+++ engine/src/render/picker/picker.cpp
@@ -0,0 +1,12 @@
+#include <cubos/core/ecs/reflection.hpp>
+#include <cubos/core/reflection/external/glm.hpp>
+#include <cubos/core/reflection/external/primitives.hpp>
+
+#include <cubos/engine/render/picker/picker.hpp>
+
+CUBOS_REFLECT_IMPL(cubos::engine::RenderPicker)
+{
+    return core::ecs::TypeBuilder<RenderPicker>("cubos::engine::RenderPicker")
+        .withField("size", &RenderPicker::size)
+        .build();
+}
