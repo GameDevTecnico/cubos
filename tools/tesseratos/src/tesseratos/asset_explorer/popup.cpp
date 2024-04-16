17e34d419f445c68851abe528b3ae9d807124502
Teresa Matos <teresa.vargas.matos@tecnico.ulisboa.com>
2024-04-16 10:36:21 +0100

    fix(tesseratos):  Improve assetSelectionPopup functionality

--- tools/tesseratos/src/tesseratos/asset_explorer/popup.cpp
+++ tools/tesseratos/src/tesseratos/asset_explorer/popup.cpp
@@ -10,38 +10,32 @@ using cubos::engine::AnyAsset;
 using cubos::engine::Asset;
 using cubos::engine::Assets;
 
-using namespace tesseratos;
-
-bool assetSelectionPopup(const std::string& title, AnyAsset& selectedAsset, const Type& type, const Assets& assets)
+bool tesseratos::assetSelectionPopup(const std::string& title, AnyAsset& selectedAsset, const Type& type,
+                                     const Assets& assets)
 {
     bool returnValue = false;
 
-    ImGui::PushID(title.c_str());
-    ImGui::OpenPopup(title.c_str());
-    if (ImGui::BeginPopupModal(title.c_str(), nullptr))
+    if (ImGui::BeginPopup(title.c_str()))
     {
         for (auto const& asset : assets.listAll())
         {
             if (&assets.type(asset) == &type)
             {
                 auto path = assets.readMeta(asset)->get("path");
+                ImGui::PushID(path->c_str());
                 ImGui::Text("- %s", path->c_str());
                 ImGui::SameLine();
                 if (ImGui::Button("Select"))
                 {
                     selectedAsset = asset;
                     returnValue = true;
+                    ImGui::CloseCurrentPopup();
                 }
+                ImGui::PopID();
             }
         }
         ImGui::EndPopup();
     }
 
-    if (returnValue)
-    {
-        ImGui::CloseCurrentPopup();
-    }
-
-    ImGui::PopID();
     return returnValue;
 }
