1df22c2ceae53038aabec1dd555fa4b7358a6b01
Ricardo Antunes <me@riscadoa.com>
2024-05-01 15:04:17 +0100

    fix(tools): take Collider's transform into account on colliderGizmosPlugin

--- tools/tesseratos/src/tesseratos/collider_gizmos/plugin.cpp
+++ tools/tesseratos/src/tesseratos/collider_gizmos/plugin.cpp
@@ -1,3 +1,4 @@
+#include <cubos/engine/collisions/collider.hpp>
 #include <cubos/engine/collisions/plugin.hpp>
 #include <cubos/engine/collisions/shapes/box.hpp>
 #include <cubos/engine/gizmos/plugin.hpp>
@@ -7,14 +8,7 @@
 #include <tesseratos/entity_selector/plugin.hpp>
 #include <tesseratos/toolbox/plugin.hpp>
 
-using cubos::engine::BoxCollisionShape;
-using cubos::engine::Commands;
-using cubos::engine::Cubos;
-using cubos::engine::Entity;
-using cubos::engine::Gizmos;
-using cubos::engine::LocalToWorld;
-using cubos::engine::Position;
-using cubos::engine::Query;
+using namespace cubos::engine;
 
 void tesseratos::colliderGizmosPlugin(Cubos& cubos)
 {
@@ -29,17 +23,17 @@ void tesseratos::colliderGizmosPlugin(Cubos& cubos)
         .after(cubos::engine::transformUpdateTag)
         .before(cubos::engine::gizmosDrawTag)
         .call([](Toolbox& toolbox, Gizmos& gizmos, const EntitySelector& selector,
-                 Query<Entity, const LocalToWorld&, const BoxCollisionShape&> boxes) {
+                 Query<Entity, const LocalToWorld&, const Collider&, const BoxCollisionShape&> boxes) {
             bool showAll = toolbox.isOpen("All Collider Gizmos");
 
             gizmos.color({1.0F, 0.0F, 0.0F});
 
-            for (auto [ent, localToWorld, shape] : boxes)
+            for (auto [ent, localToWorld, collider, shape] : boxes)
             {
                 if (showAll || ent == selector.selection)
                 {
                     auto size = shape.box.halfSize * 2.0F;
-                    auto transform = glm::scale(localToWorld.mat, size);
+                    auto transform = glm::scale(localToWorld.mat * collider.transform, size);
                     gizmos.drawWireBox("collider-gizmos", transform);
                 }
             }
