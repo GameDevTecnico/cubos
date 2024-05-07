7aa793ccf778c95b4599db9603d6e5dc49291a2e
Tiago Antunes <78920440+fallenatlas@users.noreply.github.com>
2024-05-07 18:48:54 +0100

    fix(fixed_substep): add missing include
    Co-authored-by: Ricardo Antunes <me@riscadoa.com>

--- engine/src/physics/fixed_substep/substeps.cpp
+++ engine/src/physics/fixed_substep/substeps.cpp
@@ -1,8 +1,9 @@
 #include "substeps.hpp"
 
 #include <cubos/core/ecs/reflection.hpp>
+#include <cubos/core/reflection/external/primitives.hpp>
 
 CUBOS_REFLECT_IMPL(cubos::engine::Substeps)
 {
-    return core::ecs::TypeBuilder<Substeps>("cubos::engine::Substeps").build();
+    return core::ecs::TypeBuilder<Substeps>("cubos::engine::Substeps").withField("value", &Substeps::value).build();
 }
