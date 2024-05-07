57df6525da6d8ee9b56aa5c276532db6ebac2893
Ricardo Antunes <me@riscadoa.com>
2024-05-07 15:46:02 +0100

    feat(rendering): add RenderDefaultsTarget

--- engine/src/render/defaults/plugin.cpp
+++ engine/src/render/defaults/plugin.cpp
@@ -1,6 +1,7 @@
 #include <cubos/engine/render/bloom/plugin.hpp>
 #include <cubos/engine/render/camera/plugin.hpp>
 #include <cubos/engine/render/defaults/plugin.hpp>
+#include <cubos/engine/render/defaults/target.hpp>
 #include <cubos/engine/render/deferred_shading/plugin.hpp>
 #include <cubos/engine/render/depth/plugin.hpp>
 #include <cubos/engine/render/g_buffer/plugin.hpp>
@@ -14,6 +15,7 @@
 #include <cubos/engine/render/split_screen/plugin.hpp>
 #include <cubos/engine/render/ssao/plugin.hpp>
 #include <cubos/engine/render/target/plugin.hpp>
+#include <cubos/engine/render/target/target.hpp>
 #include <cubos/engine/render/tone_mapping/plugin.hpp>
 #include <cubos/engine/render/voxels/palette.hpp>
 #include <cubos/engine/render/voxels/plugin.hpp>
@@ -46,5 +48,35 @@ void cubos::engine::renderDefaultsPlugin(Cubos& cubos)
 
     cubos.plugin(deferredShadingPlugin);
 
+    cubos.component<RenderTargetDefaults>();
+
     cubos.tag(bloomTag).after(deferredShadingTag);
+
+    cubos.observer("unpack RenderTargetDefaults components")
+        .onAdd<RenderTargetDefaults>()
+        .call([](Commands cmds, Query<Entity, const RenderTargetDefaults&> query) {
+            for (auto [entity, defaults] : query)
+            {
+                cmds.remove<RenderTargetDefaults>(entity)
+                    .add(entity, defaults.target)
+                    .add(entity, defaults.hdr)
+                    .add(entity, defaults.gBuffer)
+                    .add(entity, defaults.picker)
+                    .add(entity, defaults.depth)
+                    .add(entity, defaults.gBufferRasterizer)
+                    .add(entity, defaults.ssao)
+                    .add(entity, defaults.toneMapping)
+                    .add(entity, defaults.deferredShading);
+
+                if (defaults.splitScreen)
+                {
+                    cmds.add(entity, defaults.splitScreen.value());
+                }
+
+                if (defaults.bloom)
+                {
+                    cmds.add(entity, defaults.bloom.value());
+                }
+            }
+        });
 }
