0d50205a7a9c87e356e6c8f5b8bb035f6d22d450
roby2014 <roby2x14@gmail.com>
2024-05-18 18:29:55 +0100

    feat(core): impl metrics

---
+++ core/include/cubos/core/metrics.hpp
@@ -0,0 +1,105 @@
+/// @file
+/// @brief Metrics and profiling utilities and macros.
+/// @ingroup core
+
+#pragma once
+
+#include <chrono>
+#include <string>
+
+#include <cubos/core/api.hpp>
+#include <cubos/core/reflection/reflect.hpp>
+
+namespace cubos::core
+{
+    /// @brief Singleton class that manages a collection of metrics.
+    /// @ingroup core
+    class CUBOS_CORE_API Metrics
+    {
+    public:
+        /// @brief Deleted constructor.
+        Metrics() = delete;
+
+        /// @brief Add a metric in the pool.
+        /// @param metric The metric to add.
+        static void metric(const std::string& name, double metric);
+
+        /// @brief Size of the metric pool.
+        /// @return Metrics count.
+        static std::size_t size();
+
+        /// @brief Size of metrics associated with a given name.
+        /// @param name Metric name.
+        /// @return Metrics count.
+        static std::size_t sizeByName(const std::string& name);
+
+        /// @brief Clears the metric pool
+        static void clear();
+
+        /// @brief Sets maximum metric entries.
+        /// @param n The new maximum value.
+        static void setMaxEntries(std::size_t n);
+
+        /// @brief Read a metric value by its name.
+        /// @param name Metric name.
+        /// @param[out] value Buffer to store metric value.
+        /// @param offset Read count per metric.
+        /// @return Whether the metric was found.
+        static bool readValue(const std::string& name, double& value, size_t& offset);
+
+        /// @brief Search for a new unique metric name.
+        /// @param[out] name Buffer to store metric name.
+        /// @param seenCount Seen metrics count.
+        /// @return Whether a new metric was found.
+        static bool readName(std::string& name, size_t& seenCount);
+    };
+
+    /// @brief Utility to measure the execution time of a scope.
+    ///
+    /// This class measures the time between its construction and destruction,
+    /// providing a way to profile code execution time.
+    class CUBOS_CORE_API ScopeProfiler
+    {
+    public:
+        /// @brief Construct a ScopeProfiler with a given name.
+        ScopeProfiler(std::string name, std::string file, std::size_t line);
+
+        /// @brief Destruct the ScopeProfiler and calculate the elapsed time.
+        ~ScopeProfiler();
+
+    private:
+        std::string mName;                                                  ///< The name of the scope.
+        std::string mFile;                                                  ///< The file from where it was invoked.
+        std::size_t mLine;                                                  ///< The line from where it was invoked.
+        std::chrono::time_point<std::chrono::high_resolution_clock> mStart; ///< Start time when constructed.
+    };
+} // namespace cubos::core
+
+/// @def CUBOS_METRIC
+/// @ingroup core
+/// @brief Macro to add a metric.
+/// @param name The name of the metric.
+/// @param val The value to set for the metric.
+
+#ifdef CUBOS_PROFILING
+#define CUBOS_METRIC(name, val) ::cubos::core::Metrics::metric(name, val)
+#else
+#define CUBOS_METRIC(...)                                                                                              \
+    do                                                                                                                 \
+    {                                                                                                                  \
+    } while (false)
+#endif
+
+/// @def CUBOS_PROFILE
+/// @ingroup core
+/// @brief Macro to measure the execution time of a function/scope, registering it as a metric.
+/// @note The macro uses `__FUNCTION__` to register the metric name.
+
+#ifdef CUBOS_PROFILING
+#define CUBOS_PROFILE() ::cubos::core::ScopeProfiler _profilingTimeTracker(__FUNCTION__, __FILE__, __LINE__)
+#else
+#define CUBOS_PROFILE(...)                                                                                             \
+    do                                                                                                                 \
+    {                                                                                                                  \
+    } while (false)
+#endif
---
+++ core/src/metrics.cpp
@@ -0,0 +1,132 @@
+#include <algorithm>
+#include <deque>
+#include <map>
+#include <mutex>
+#include <utility>
+#include <vector>
+
+#include <cubos/core/log.hpp>
+#include <cubos/core/metrics.hpp>
+
+using cubos::core::Metrics;
+using cubos::core::ScopeProfiler;
+
+namespace
+{
+    /// @brief Private type which stores the state of the metrics pool.
+    struct State
+    {
+        std::map<std::string, std::deque<double>> mMetrics; ///< Collection of metrics.
+        std::vector<std::string> mNames;                    ///< Collection of unique metric names.
+        std::size_t mMaxEntries{CUBOS_METRIC_MAX_ENTRIES};  ///< Maximum number of entries each metric can store.
+        mutable std::mutex mMutex;                          ///< Mutex for thread-safe access to the metrics.
+    };
+} // namespace
+
+/// @brief Metrics pool state singleton. Guarantees it is initialized exactly once, when needed.
+/// @return Metrics pool.
+static State& state()
+{
+    static State instance{};
+    return instance;
+}
+
+void Metrics::metric(const std::string& name, const double metric)
+{
+    std::lock_guard<std::mutex> lock(state().mMutex);
+
+    if (state().mMetrics[name].size() >= state().mMaxEntries)
+    {
+        state().mMetrics[name].pop_back();
+    }
+
+    state().mMetrics[name].push_back(metric);
+
+    // add metric name if new
+    if (std::find(state().mNames.cbegin(), state().mNames.cend(), name) == state().mNames.end())
+    {
+        state().mNames.emplace_back(name);
+    }
+}
+
+std::size_t Metrics::size()
+{
+    std::lock_guard<std::mutex> lock(state().mMutex);
+
+    return state().mNames.size();
+}
+
+std::size_t Metrics::sizeByName(const std::string& name)
+{
+    std::lock_guard<std::mutex> lock(state().mMutex);
+
+    if (!state().mMetrics.contains(name))
+    {
+        return 0;
+    }
+
+    return state().mMetrics.at(name).size();
+}
+
+void Metrics::clear()
+{
+    std::lock_guard<std::mutex> lock(state().mMutex);
+
+    state().mMetrics.clear();
+    state().mNames.clear();
+}
+
+void Metrics::setMaxEntries(std::size_t n)
+{
+    state().mMaxEntries = n;
+}
+
+bool Metrics::readValue(const std::string& name, double& value, std::size_t& offset)
+{
+    std::lock_guard<std::mutex> lock(state().mMutex);
+
+    if (!state().mMetrics.contains(name))
+    {
+        return false;
+    }
+
+    if (offset >= state().mMetrics.at(name).size())
+    {
+        return false;
+    }
+
+    auto test = state().mMetrics;
+    value = state().mMetrics.at(name).at(offset);
+    offset++;
+    test.clear();
+    return true;
+}
+
+bool Metrics::readName(std::string& name, size_t& seenCount)
+{
+    std::lock_guard<std::mutex> lock(state().mMutex);
+
+    if (seenCount >= state().mNames.size())
+    {
+        return false;
+    }
+
+    name = state().mNames.at(seenCount);
+    seenCount++;
+    return true;
+}
+
+ScopeProfiler::ScopeProfiler(std::string name, std::string file, std::size_t line)
+    : mName(std::move(name))
+    , mFile(std::move(file))
+    , mLine(line)
+    , mStart(std::chrono::time_point<std::chrono::high_resolution_clock>::clock::now())
+{
+}
+
+ScopeProfiler::~ScopeProfiler()
+{
+    auto now = std::chrono::time_point<std::chrono::high_resolution_clock>::clock::now();
+    std::chrono::duration<double, std::milli> elapsed = now - mStart;
+    Metrics::metric("profiling:" + mFile + ":" + mName + ":" + std::to_string(mLine), elapsed.count());
+};
