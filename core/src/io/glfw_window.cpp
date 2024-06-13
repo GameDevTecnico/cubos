f7897352a86ff3148f381d69da41ce524042aadd
Luís Fonseca <luis@lhf.pt>
2024-06-13 15:34:13 +0100

    build: compile to web

--- core/src/io/glfw_window.cpp
+++ core/src/io/glfw_window.cpp
@@ -360,7 +360,7 @@ void GLFWWindow::modifiers(Modifiers modifiers)
 
 bool GLFWWindow::gamepadState(int gamepad, GamepadState& state) const
 {
-#ifdef WITH_GLFW
+#if defined(WITH_GLFW) && !defined(__EMSCRIPTEN__)
     GLFWgamepadstate glfwState;
     if (glfwGetGamepadState(gamepad, &glfwState) == GLFW_FALSE)
     {
@@ -397,6 +397,8 @@ bool GLFWWindow::gamepadState(int gamepad, GamepadState& state) const
 
     return true;
 #else
+    (void)gamepad;
+    (void)state;
     UNSUPPORTED();
 #endif
 }
