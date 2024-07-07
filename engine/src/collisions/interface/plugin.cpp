8970cb384cb68afc53405409afce2990e675a1e5
joaomanita <joaomanoita3@gmail.com>
2024-07-07 17:53:44 +0100

    feat(engine): add voxel collision shape

--- engine/src/collisions/interface/plugin.cpp
+++ engine/src/collisions/interface/plugin.cpp
@@ -5,12 +5,14 @@
 #include <cubos/engine/collisions/contact_manifold.hpp>
 #include <cubos/engine/collisions/shapes/box.hpp>
 #include <cubos/engine/collisions/shapes/capsule.hpp>
+#include <cubos/engine/collisions/shapes/voxel.hpp>
 
 void cubos::engine::interfaceCollisionsPlugin(Cubos& cubos)
 {
     cubos.component<Collider>();
     cubos.component<BoxCollisionShape>();
     cubos.component<CapsuleCollisionShape>();
+    cubos.component<VoxelCollisionShape>();
 
     cubos.relation<CollidingWith>();
     cubos.relation<ContactManifold>();
