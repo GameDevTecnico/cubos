a191eb481b948fbdfeccad71c88211e53a66336a
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-07-29 15:44:44 +0100

    feat(physics): implement adding force on point to force component

--- engine/include/cubos/engine/physics/components/force.hpp
+++ engine/include/cubos/engine/physics/components/force.hpp
@@ -24,18 +24,32 @@ namespace cubos::engine
             return mForce;
         }
 
+        // Torque resulting from addForceOnPoint.
+        glm::vec3 torque() const
+        {
+            return mTorque;
+        }
+
         void add(glm::vec3 force)
         {
             mForce += force;
         }
 
+        void addForceOnPoint(glm::vec3 force, glm::vec3 localPoint, glm::vec3 centerOfMass)
+        {
+            mForce += force;
+            mTorque = glm::cross(localPoint - centerOfMass, force);
+        }
+
         void clear()
         {
             mForce = {0.0F, 0.0F, 0.0F};
+            mTorque = {0.0F, 0.0F, 0.0F};
         }
 
     private:
         glm::vec3 mForce = {0.0F, 0.0F, 0.0F};
+        glm::vec3 mTorque = {0.0F, 0.0F, 0.0F};
     };
 
 } // namespace cubos::engine
