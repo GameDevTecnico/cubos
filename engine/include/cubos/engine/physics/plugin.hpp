59b7df33b2a9a3fb34d0bb6d617a28590038e82e
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-07-29 16:16:04 +0100

    docs(physics): update components in physics plugin documentation

--- engine/include/cubos/engine/physics/plugin.hpp
+++ engine/include/cubos/engine/physics/plugin.hpp
@@ -12,10 +12,15 @@
 #include <cubos/core/reflection/external/primitives.hpp>
 
 #include <cubos/engine/physics/components/accumulated_correction.hpp>
+#include <cubos/engine/physics/components/angular_impulse.hpp>
+#include <cubos/engine/physics/components/angular_velocity.hpp>
+#include <cubos/engine/physics/components/center_of_mass.hpp>
 #include <cubos/engine/physics/components/force.hpp>
 #include <cubos/engine/physics/components/impulse.hpp>
+#include <cubos/engine/physics/components/inertia.hpp>
 #include <cubos/engine/physics/components/mass.hpp>
 #include <cubos/engine/physics/components/physics_material.hpp>
+#include <cubos/engine/physics/components/torque.hpp>
 #include <cubos/engine/physics/components/velocity.hpp>
 #include <cubos/engine/physics/physics_bundle.hpp>
 #include <cubos/engine/physics/resources/damping.hpp>
@@ -36,9 +41,14 @@ namespace cubos::engine
     /// ## Components
     /// - @ref PhysicsBundle - bundle that holds the physics information to give to a new entity.
     /// - @ref Velocity - holds the information for moving an object straight.
+    /// - @ref AngularVelocity - holds the angular velocity of an object.
     /// - @ref Force - holds forces applied on a particle.
+    /// - @ref Torque - holds torque applied on an object.
     /// - @ref Impulse - holds impulses applied on a particle.
+    /// - @ref AngularImpulse - holds angular impulses applied on a particle.
     /// - @ref Mass - holds the mass of an object.
+    /// - @ref Inertia - holds the inertia tensor of an object.
+    /// - @ref CenterOfMass - holds the center of mass of an object.
     /// - @ref AccumulatedCorrection - holds the corrections accumulated from the constraints solving.
     /// - @ref PhysicsMaterial - holds the friction and bounciness properties of the particle.
     ///
