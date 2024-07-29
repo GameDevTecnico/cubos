098829cb8f98a412c308e3e1059a9aae2eab7004
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-07-29 15:49:31 +0100

    feat(physics): add center of mass component

---
+++ engine/include/cubos/engine/physics/components/center_of_mass.hpp
@@ -0,0 +1,23 @@
+/// @file
+/// @brief Component @ref cubos::engine::CenterOfMass.
+/// @ingroup physics-plugin
+
+#pragma once
+
+#include <glm/vec3.hpp>
+
+#include <cubos/core/reflection/reflect.hpp>
+
+#include <cubos/engine/api.hpp>
+
+namespace cubos::engine
+{
+    /// @brief Component which defines the center of mass of a body.
+    /// @ingroup physics-plugin
+    struct CUBOS_ENGINE_API CenterOfMass
+    {
+        CUBOS_REFLECT;
+
+        glm::vec3 vec = {0.0F, 0.0F, 0.0F};
+    };
+} // namespace cubos::engine
