329d55c6fa43a3beed52eabedc5973fa0a547c3a
João Santos <joao.miguel.santos1809@gmail.com>
2024-08-01 17:31:55 +0100

    feat(engine): add VoxelModel

--- engine/src/voxels/plugin.cpp
+++ engine/src/voxels/plugin.cpp
@@ -2,16 +2,18 @@
 #include <cubos/engine/voxels/grid_bridge.hpp>
 #include <cubos/engine/voxels/palette_bridge.hpp>
 #include <cubos/engine/voxels/plugin.hpp>
+#include <cubos/engine/voxels/voxel_model_bridge.hpp>
 
 void cubos::engine::voxelsPlugin(Cubos& cubos)
 {
     cubos.depends(assetsPlugin);
 
-    cubos.startupSystem("setup VoxelGrid and VoxelPalette asset bridges")
+    cubos.startupSystem("setup VoxelGrid, VoxelPalette & VoxelModel asset bridges")
         .tagged(assetsBridgeTag)
         .call([](Assets& assets) {
-            // Add the bridges to load .grd and .pal files.
+            // Add the bridges to load .grd, .pal and .qb files.
             assets.registerBridge(".grd", std::make_unique<VoxelGridBridge>());
             assets.registerBridge(".pal", std::make_unique<VoxelPaletteBridge>());
+            assets.registerBridge(".qb", std::make_unique<VoxelModelBridge>());
         });
 }
