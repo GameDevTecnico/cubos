04e5811418cbbd8423b1c53c9700dc19f22aa5b2
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-08-21 20:27:36 +0100

    feat(geom): implement sutherland hodgman clipping algorithm

--- core/include/cubos/core/geom/intersections.hpp
+++ core/include/cubos/core/geom/intersections.hpp
@@ -4,12 +4,14 @@
 
 #pragma once
 
+#include <list>
 #include <vector>
 
 #include <glm/glm.hpp>
 
 #include <cubos/core/ecs/entity/entity.hpp>
 #include <cubos/core/geom/box.hpp>
+#include <cubos/core/geom/plane.hpp>
 #include <cubos/core/reflection/reflect.hpp>
 
 namespace cubos::core::geom
@@ -20,9 +22,8 @@ namespace cubos::core::geom
     {
         CUBOS_REFLECT;
 
-        float penetration;  ///< Penetration depth of the collision.
-        glm::vec3 position; ///< Position of contact on the surface of the entity.
-        glm::vec3 normal;   ///< Normal of contact on the surface of the entity.
+        float penetration; ///< Penetration depth of the collision. Always positive in the direction of the normal.
+        glm::vec3 normal;  ///< Normal of contact on the surface of the entity.
     };
 
     /// @brief Computes the intersection between two box shapes.
@@ -32,5 +33,25 @@ namespace cubos::core::geom
     /// @param localToWorld2 Local to world matrix of the second entity.
     CUBOS_CORE_API bool intersects(const Box& box1, const glm::mat4& localToWorld1, const Box& box2,
                                    const glm::mat4& localToWorld2, Intersection& intersect);
+
+    /// @brief Performs the Sutherland-Hodgman Clipping algorithm.
+    /// @param inputPolygon The polygon to perform the clipping on.
+    /// @param numClipPlanes Number of cliping planes.
+    /// @param clipPlanes Clipping planes
+    /// @param removeNotClipToPlane Whether to remove the points if they're outside the plane.
+    /// @return The polygon resulting from the clipping.
+    CUBOS_CORE_API std::list<glm::vec3> sutherlandHodgmanClipping(const std::list<glm::vec3>& inputPolygon,
+                                                                  int numClipPlanes,
+                                                                  const cubos::core::geom::Plane* clipPlanes,
+                                                                  bool removeNotClipToPlane);
+
+    /// @brief Compute the intersection between a plane and an edge.
+    /// @param plane The plane.
+    /// @param start The start point of the edge.
+    /// @param end The end point of the edge.
+    /// @param outPoint The position of the intersection.
+    /// @return True if there's intersection, false otherwise.
+    CUBOS_CORE_API bool planeEdgeIntersection(const cubos::core::geom::Plane& plane, const glm::vec3& start,
+                                              const glm::vec3& end, glm::vec3& outPoint);
 
 } // namespace cubos::core::geom
