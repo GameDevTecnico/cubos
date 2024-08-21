d96f9f7c5417c22038aa36dc553481b4f39c8ca0
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-08-21 20:40:56 +0100

    feat(penetration_constraint): update constraint to follow manifold structure

--- engine/src/physics/constraints/penetration_constraint.hpp
+++ engine/src/physics/constraints/penetration_constraint.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <vector>
+
 #include <glm/vec3.hpp>
 
 #include <cubos/core/ecs/entity/entity.hpp>
@@ -11,6 +13,14 @@
 
 namespace cubos::engine
 {
+    struct PenetrationConstraintPointData
+    {
+        /// TODO: should contain what contact point has but with also information to resolve the constraint
+        /// TODO: should accumulated things be here?
+        float initialPenetration;
+        glm::vec3 initialRelativeVelocity; // I believe this depends on rotation speed at the point so should be here
+    };
+
     /// @brief Relation which holds the information for resolving a penetration between particles.
     /// @ingroup physics-solver-plugin
     struct PenetrationConstraint
@@ -18,27 +28,27 @@ namespace cubos::engine
         CUBOS_REFLECT;
 
         // colision info
-        cubos::core::ecs::Entity entity;   ///< Entity to which the normal is relative to.
-        glm::vec3 entity1OriginalPosition; ///< Position of the entity when the collision occured. (could be replaced by
-                                           ///< contact point in the future)
-        glm::vec3 entity2OriginalPosition; ///< Position of the other entity when the collision occured. (could be
-                                           ///< replaced by contact point in the future)
-        float penetration;                 ///< Penetration depth of the collision.
-        glm::vec3 normal;                  ///< Normal of contact on the surface of the entity.
+        cubos::core::ecs::Entity entity; ///< Entity to which the normal is relative to.
+        float penetration;               ///< Penetration depth of the collision. This is to be removed
+        glm::vec3 normal;                ///< Normal of contact on the surface of the entity. (world space)
 
         // separation
-        float normalMass;    ///< Mass to use for normal impulse calculation.
+        float normalMass; ///< Mass to use for normal impulse calculation. (can these be here or should be by point (in
+                          ///< which case it always depends probably))
         float normalImpulse; ///< Accumulated impulse for separation.
 
         // friction
-        float friction;         ///< Friction of the constraint.
-        float frictionMass;     ///< Mass to use for friction impulse calculation.
+        float friction;     ///< Friction of the constraint. yes.
+        float frictionMass; ///< Mass to use for friction impulse calculation.
+        /// TODO: store tangents?
         float frictionImpulse1; ///< Accumulated impulse for friction along the first tangent.
         float frictionImpulse2; ///< Accumulated impulse for friction along the second tangent.
 
         // restitution
-        float restitution;      ///< Restitution coefficient of the constraint.
-        float relativeVelocity; ///< Relative velocity for computing restitution.
+        float restitution;      ///< Restitution coefficient of the constraint. yes.
+        float relativeVelocity; ///< Relative velocity for computing restitution. This is to be removed (probably)
+
+        std::vector<PenetrationConstraintPointData> points;
 
         // soft constraint
         float biasCoefficient;
