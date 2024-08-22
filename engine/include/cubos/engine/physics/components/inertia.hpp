66e756103d4e9c3b673ecbdb22a43e8e9c843a8e
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-08-22 11:28:47 +0100

    feat(integration): add rotation to integration

--- engine/include/cubos/engine/physics/components/inertia.hpp
+++ engine/include/cubos/engine/physics/components/inertia.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <glm/gtx/quaternion.hpp>
 #include <glm/mat3x3.hpp>
 
 #include <cubos/core/reflection/reflect.hpp>
@@ -21,5 +22,19 @@ namespace cubos::engine
         glm::mat3 inertia;
         glm::mat3 inverseInertia;
         bool autoUpdate = false;
+
+        glm::mat3 rotatedInertia(const glm::quat& rotationQuat) const
+        {
+            auto rotationMat = glm::mat3(rotationQuat);
+            return rotationMat * inertia * glm::transpose(rotationMat);
+        }
+
+        // TODO: check if we use this many times. If we do, it might be worth it to calculate and store them
+        // somewhere instead.
+        glm::mat3 rotatedInverseInertia(const glm::quat& rotationQuat) const
+        {
+            auto rotationMat = glm::mat3(rotationQuat);
+            return rotationMat * inverseInertia * glm::transpose(rotationMat);
+        }
     };
 } // namespace cubos::engine
