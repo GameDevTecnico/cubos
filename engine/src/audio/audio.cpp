02d6ab0269481addff112b16280a2f96539ed7f5
Diogo Miranda <diogomsmiranda@gmail.com>
2024-09-29 22:26:52 +0100

    feat(audio): Add Audio asset (#230)
    Co-Authored-By: João Miguel Nogueira <101069446+Dageus@users.noreply.github.com>

---
+++ engine/src/audio/audio.cpp
@@ -0,0 +1,44 @@
+#include <cubos/core/log.hpp>
+#include <cubos/core/reflection/external/cstring.hpp>
+#include <cubos/core/reflection/type.hpp>
+
+#include <cubos/engine/audio/audio.hpp>
+
+CUBOS_REFLECT_IMPL(cubos::engine::Audio)
+{
+    return core::reflection::Type::create("cubos::engine::Audio");
+}
+
+cubos::engine::Audio::Audio(core::memory::Stream& stream)
+{
+    stream.seek(0, cubos::core::memory::SeekOrigin::End);
+    size_t streamSize = stream.tell();
+    void* contents = operator new(sizeof(char) * streamSize);
+    stream.seek(0, cubos::core::memory::SeekOrigin::Begin);
+    if (stream.read((char*)contents, streamSize) < streamSize)
+    {
+        CUBOS_ERROR("Couldn't load audio: Read less than stream lenght");
+        // TODO: add audio buffer here
+        mData = nullptr;
+        operator delete(contents);
+        return;
+    };
+
+    mData = cubos::core::al::MiniaudioDevice::createBuffer(contents, (size_t)streamSize);
+    if (mData == nullptr)
+    {
+        CUBOS_ERROR("Couldn't create audio buffer");
+    }
+    operator delete(contents);
+}
+
+cubos::engine::Audio::~Audio()
+{
+    // no need to delete buffer thanks to shared_ptr
+}
+
+cubos::engine::Audio::Audio(Audio&& other) noexcept
+    : mData(other.mData)
+{
+    other.mData = nullptr;
+}
