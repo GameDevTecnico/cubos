ee760ae16bfab0c2b781fb11017107350c77cac3
Diogo Miranda <diogomsmiranda@gmail.com>
2024-10-12 18:21:31 +0100

    feat(audio): Add Audio asset (#230)
    Co-Authored-By: João Miguel Nogueira <101069446+Dageus@users.noreply.github.com>

---
+++ engine/include/cubos/engine/audio/bridge.hpp
@@ -0,0 +1,35 @@
+/// @file
+/// @brief Class @ref cubos::engine::AudioBridge.
+/// @ingroup audio-plugin
+
+#pragma once
+
+#include <utility>
+
+#include <cubos/engine/assets/bridges/file.hpp>
+#include <cubos/engine/audio/audio.hpp>
+
+namespace cubos::engine
+{
+    /// @brief Bridge which loads and saves @ref Sound assets.
+    ///
+    /// Uses the format specified in @ref Audio::loadFrom and @ref Audio::writeTo
+    ///
+    /// @ingroup audio-plugin
+    class AudioBridge : public FileBridge
+    {
+    public:
+        std::shared_ptr<cubos::core::al::AudioContext> mContext;
+
+        /// @brief Constructs a bridge.
+        AudioBridge(std::shared_ptr<cubos::core::al::AudioContext> context)
+            : FileBridge(core::reflection::reflect<Audio>())
+            , mContext(std::move(context))
+        {
+        }
+
+    protected:
+        bool loadFromFile(Assets& assets, const AnyAsset& handle, core::memory::Stream& stream) override;
+        bool saveToFile(const Assets& assets, const AnyAsset& handle, core::memory::Stream& stream) override;
+    };
+} // namespace cubos::engine
