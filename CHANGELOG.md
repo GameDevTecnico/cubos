5fbe6e33510771c4271127dcd6f42d0c8a475dc2
Ricardo Antunes <me@riscadoa.com>
2024-12-06 10:32:45 +0100

    feat(collisions): add layers and masks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Collision layers and masks, for filtering collisions (**@RiscadoA**).
+
 ### Fixed
 
 - Normalize normals while drawing to the GBuffer to avoid shiny small objects (**@RiscadoA**).
--- engine/src/collisions/interface/collider.cpp
+++ engine/src/collisions/interface/collider.cpp
@@ -1,5 +1,6 @@
 #include <cubos/core/ecs/reflection.hpp>
 #include <cubos/core/reflection/external/glm.hpp>
+#include <cubos/core/reflection/external/primitives.hpp>
 
 #include <cubos/engine/collisions/collider.hpp>
 
@@ -7,5 +8,7 @@ CUBOS_REFLECT_IMPL(cubos::engine::Collider)
 {
     return core::ecs::TypeBuilder<Collider>("cubos::engine::Collider")
         .withField("transform", &Collider::transform)
+        .withField("layer", &Collider::layer)
+        .withField("mask", &Collider::mask)
         .build();
 }
