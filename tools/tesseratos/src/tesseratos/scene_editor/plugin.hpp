3957e8f8b7a83956d052037201abf786be795635
jdbaracho <joao.d.baracho@gmail.com>
2025-01-31 00:02:08 +0000

    feat(tesseratos): move tesserato's tools status inside their respective plugins plugin

--- tools/tesseratos/src/tesseratos/scene_editor/plugin.hpp
+++ tools/tesseratos/src/tesseratos/scene_editor/plugin.hpp
@@ -18,6 +18,14 @@ namespace tesseratos
     /// @note Selected entities are registered in the @ref Selection resource.
     /// @note The opened scene is identified by the @ref AssetSelectedEvent event.
 
+    /// @brief Tool state.
+    struct SceneEditorTool
+    {
+        CUBOS_REFLECT;
+
+        bool isOpen{false};
+    };
+
     /// @brief Plugin entry function.
     /// @param cubos @b Cubos main class
     /// @ingroup tesseratos-scene-editor-plugin
