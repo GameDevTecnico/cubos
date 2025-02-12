8ac4878d1064f24e6e811668384073c82bd1c8aa
fallenatlas <tiago.a.p.antunes@gmail.com>
2025-02-12 12:39:36 +0000

    feat(physics): add inertia tensor for voxel and capsule shape

--- engine/src/physics/plugin.cpp
+++ engine/src/physics/plugin.cpp
@@ -5,8 +5,10 @@
 #include <cubos/core/reflection/traits/enum.hpp>
 #include <cubos/core/reflection/type.hpp>
 
+#include <cubos/engine/assets/plugin.hpp>
 #include <cubos/engine/collisions/plugin.hpp>
 #include <cubos/engine/collisions/shapes/box.hpp>
+#include <cubos/engine/collisions/shapes/voxel.hpp>
 #include <cubos/engine/fixed_step/plugin.hpp>
 #include <cubos/engine/physics/plugin.hpp>
 #include <cubos/engine/physics/solver/plugin.hpp>
@@ -35,15 +37,6 @@ CUBOS_REFLECT_IMPL(CenterOfMass)
 {
     return cubos::core::ecs::TypeBuilder<CenterOfMass>("cubos::engine::CenterOfMass")
         .withField("vec", &CenterOfMass::vec)
-        .build();
-}
-
-CUBOS_REFLECT_IMPL(Inertia)
-{
-    return cubos::core::ecs::TypeBuilder<Inertia>("cubos::engine::Inertia")
-        .withField("inertia", &Inertia::inertia)
-        .withField("inverseInertia", &Inertia::inverseInertia)
-        .withField("autoUpdate", &Inertia::autoUpdate)
         .build();
 }
 
@@ -140,22 +133,11 @@ CUBOS_REFLECT_IMPL(SolverConstants)
 
 CUBOS_DEFINE_TAG(cubos::engine::physicsPrepareTag);
 
-// Compute Inertia Tensor for box shape
-static glm::mat3 boxInertiaTensor(float mass, glm::vec3 dimensions)
-{
-    float constant = mass / 12.0F;
-    auto x2 = (float)glm::pow(dimensions.x, 2);
-    auto y2 = (float)glm::pow(dimensions.y, 2);
-    auto z2 = (float)glm::pow(dimensions.z, 2);
-
-    return glm::mat3{constant * (z2 + y2), 0.0F, 0.0F, 0.0F, constant * (x2 + z2), 0.0F, 0.0F, 0.0F,
-                     constant * (x2 + y2)};
-}
-
 void cubos::engine::physicsPlugin(Cubos& cubos)
 {
     cubos.depends(fixedStepPlugin);
     cubos.depends(collisionsPlugin);
+    cubos.depends(assetsPlugin);
 
     cubos.plugin(physicsFixedSubstepPlugin);
 
@@ -222,10 +204,12 @@ void cubos::engine::physicsPlugin(Cubos& cubos)
         });
 
     // Should this be here?
-    cubos.system("update inertia - box collider")
+    // Updates center of mass and inertia for a body with a box shape collider based on changes in the shape and mass
+    // TODO: Update CenterOfMass when we change the offset of the collider
+    cubos.system("update mass properties - box collider")
         .tagged(physicsPrepareTag)
-        .call([](Query<Mass&, BoxCollisionShape&, Inertia&> query) {
-            for (auto [mass, shape, inertia] : query)
+        .call([](Query<Mass&, BoxCollisionShape&, Inertia&, CenterOfMass&> query) {
+            for (auto [mass, shape, inertia, centerOfMass] : query)
             {
                 if (!inertia.autoUpdate)
                 {
@@ -248,10 +232,50 @@ void cubos::engine::physicsPlugin(Cubos& cubos)
                     continue;
                 }
 
+                // TODO: change this to adapt to the offset with the collider transform
+                centerOfMass.vec = glm::vec3(0.0F, 0.0F, 0.0F);
+
                 // Recalculate inertia tensor
-                glm::mat3 inertiaTensor = boxInertiaTensor(mass.mass, shape.box.halfSize * 2.0F);
-                inertia.inertia = inertiaTensor;
-                inertia.inverseInertia = glm::inverse(inertiaTensor);
+                glm::mat3 inertiaTensor = boxShapeInertiaTensor(mass.mass, shape.box.halfSize * 2.0F);
+                inertia.setFromTensor(inertiaTensor);
+            }
+        });
+
+    // Should this be here?
+    // Updates center of mass and inertia for a body with a voxel shape collider based on changes in the shape and mass
+    // TODO: Update CenterOfMass when we change the offset of the collider
+    cubos.system("update mass properties - voxel collider")
+        .tagged(physicsPrepareTag)
+        .call([](Query<Mass&, VoxelCollisionShape&, Inertia&, CenterOfMass&> query) {
+            for (auto [mass, shape, inertia, centerOfMass] : query)
+            {
+                if (!inertia.autoUpdate)
+                {
+                    continue;
+                }
+
+                if (!mass.changed && !shape.changed)
+                {
+                    continue;
+                }
+
+                mass.changed = false;
+                shape.changed = false;
+
+                // Object has infinite mass
+                if (mass.inverseMass == 0.0F)
+                {
+                    inertia.inertia = glm::mat3(0.0F);
+                    inertia.inverseInertia = glm::mat3(0.0F);
+                    continue;
+                }
+
+                // TODO: change this to adapt to the offset with the collider transform
+                centerOfMass.vec = glm::vec3(0.0F, 0.0F, 0.0F);
+
+                // Recalculate inertia tensor
+                glm::mat3 inertiaTensor = voxelShapeInertiaTensor(mass.mass, shape);
+                inertia.setFromTensor(inertiaTensor);
             }
         });
 
