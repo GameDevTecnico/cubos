8d6b9562aef4092ae3247844461f0aa00257ddbc
Ricardo Antunes <me@riscadoa.com>
2025-05-07 19:54:33 +0100

    feat(rendering): add reflection to RenderEnvironment

--- engine/src/render/lights/environment.cpp
+++ engine/src/render/lights/environment.cpp
@@ -1,8 +1,13 @@
 #include <cubos/core/ecs/reflection.hpp>
+#include <cubos/core/reflection/external/carray.hpp>
+#include <cubos/core/reflection/external/glm.hpp>
 
 #include <cubos/engine/render/lights/environment.hpp>
 
 CUBOS_REFLECT_IMPL(cubos::engine::RenderEnvironment)
 {
-    return core::ecs::TypeBuilder<RenderEnvironment>("cubos::engine::RenderEnvironment").build();
+    return core::ecs::TypeBuilder<RenderEnvironment>("cubos::engine::RenderEnvironment")
+        .withField("ambient", &RenderEnvironment::ambient)
+        .withField("skyGradient", &RenderEnvironment::skyGradient)
+        .build();
 }
