4266f55692e5ef15def3a3e63c4fb5e575e2cb90
fkatar <fmacatarino@sapo.pt>
2025-06-11 17:03:53 +0100

    feat: Allow generating metas with random UUIDs through Quadrados

--- tools/quadrados/src/tools.hpp
+++ tools/quadrados/src/tools.hpp
@@ -2,7 +2,8 @@
 
 #include <string>
 
-struct GlobalArgs {
+struct GlobalArgs
+{
     std::string installDir = QUADRADOS_INSTALL_PATH;
 };
 /// Stores info about a tool.
@@ -12,17 +13,14 @@ struct Tool
     int (*run)(int, char**, GlobalArgs&);
 };
 
-
 int runHelp(int argc, char** argv, GlobalArgs& ga);
 int runEmbed(int argc, char** argv, GlobalArgs& ga);
 int runConvert(int argc, char** argv, GlobalArgs& ga);
 int runInit(int argc, char** argv, GlobalArgs& ga);
-int runCreate(int argc, char **argv, GlobalArgs& ga);
+int runImport(int argc, char** argv, GlobalArgs& ga);
+int runCreate(int argc, char** argv, GlobalArgs& ga);
 
 static const Tool Tools[] = {
-    {"help", runHelp},
-    {"embed", runEmbed},
-    {"convert", runConvert},
-    {"init", runInit},
-    {"create", runCreate},
+    {"help", runHelp}, {"embed", runEmbed},   {"convert", runConvert},
+    {"init", runInit}, {"import", runImport}, {"create", runCreate},
 };
