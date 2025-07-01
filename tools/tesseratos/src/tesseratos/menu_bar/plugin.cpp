1919ddf335ed2e0cad0efd6e87e9f951c690aab9
jdbaracho <joao.d.baracho@gmail.com>
2025-07-01 18:36:02 +0100

    refactor(tesseratos): refactor menu bar plugin

--- tools/tesseratos/src/tesseratos/menu_bar/plugin.cpp
+++ tools/tesseratos/src/tesseratos/menu_bar/plugin.cpp
@@ -17,12 +17,6 @@ using namespace tesseratos;
 void tesseratos::menuBarPlugin(Cubos& cubos)
 {
     cubos.depends(imguiPlugin);
-    
-    // For readability purposes, plugins are divided into blocks based on their dependencies.
-    // The first block contains plugins without dependencies.
-    // The second block contains plugins which depend on plugins from the first block.
-    // The third block contains plugins which depend on plugins from the first and second blocks.
-    // And so on.
 
     cubos.plugin(selectionPlugin);
 
@@ -37,69 +31,69 @@ void tesseratos::menuBarPlugin(Cubos& cubos)
 
     cubos.system("setup Menu Bar")
         .tagged(imguiTag)
-        .call([](AssetExplorer& assetExplorer, Debugger& debugger,Project& project,
-            Importer& importer, SceneEditor& sceneEditor, VoxelPalleteEditor& voxelPalleteEditor) {
+        .call([](AssetExplorer& assetExplorer, Debugger& debugger, Project& project, Importer& importer,
+                 SceneEditor& sceneEditor, VoxelPalleteEditor& voxelPalleteEditor) {
+            ImGui::Begin("Tesseratos");
 
-    ImGui::Begin("Tesseratos");
-
-        if (ImGui::BeginMenuBar())
-        {
-            if (ImGui::BeginMenu("Project"))
+            if (ImGui::BeginMenuBar())
             {
-                ImGui::MenuItem("New...", "Ctrl+N");
-                ImGui::MenuItem("Open...", "Ctrl+O");
-                ImGui::MenuItem("Browse in Bridge...", "Alt+Ctrl+O");
-                ImGui::MenuItem("Open As...", "Alt+Shift+Ctrl+O");
-                if(ImGui::BeginMenu("Open Recent")) {
-                    ImGui::Text("No recent files...");
+                if (ImGui::BeginMenu("Project"))
+                {
+                    ImGui::MenuItem("New...", "Ctrl+N");
+                    ImGui::MenuItem("Open...", "Ctrl+O");
+                    ImGui::MenuItem("Browse in Bridge...", "Alt+Ctrl+O");
+                    ImGui::MenuItem("Open As...", "Alt+Shift+Ctrl+O");
+                    if (ImGui::BeginMenu("Open Recent"))
+                    {
+                        ImGui::Text("No recent files...");
+                        ImGui::EndMenu();
+                    }
+                    ImGui::Separator();
+
+                    ImGui::MenuItem("Close", "Ctrl+W");
+                    ImGui::MenuItem("Close All", "Alt+Ctrl+W");
+                    ImGui::Separator();
+
+                    ImGui::MenuItem("Save", "Ctrl+S");
+                    ImGui::MenuItem("Save As", "Shift+Ctrl+S");
+                    ImGui::MenuItem("Revert", "F12");
+                    ImGui::Separator();
+
+                    ImGui::MenuItem("Export");
+                    ImGui::MenuItem("Generate");
+                    ImGui::MenuItem("Share");
+                    ImGui::Separator();
+
+                    ImGui::MenuItem("Print...", "Ctrl+P");
+                    ImGui::MenuItem("Print One Copy", "Ctrl+P");
+                    ImGui::Separator();
+
+                    ImGui::MenuItem("File Info...", "Alt+Shift+Ctrl+1");
+                    ImGui::MenuItem("Exit", "Ctrl+Q");
+
                     ImGui::EndMenu();
                 }
-                ImGui::Separator();
-                
-                ImGui::MenuItem("Close", "Ctrl+W");
-                ImGui::MenuItem("Close All", "Alt+Ctrl+W");
-                ImGui::Separator();
+                if (ImGui::BeginMenu("Edit"))
+                {
+                    ImGui::MenuItem("Fullscreen");
+                    ImGui::MenuItem("Padding");
+                    ImGui::EndMenu();
+                }
+                if (ImGui::BeginMenu("View"))
+                {
+                    ImGui::MenuItem("Asset Explorer", "", &assetExplorer.isOpen);
+                    ImGui::MenuItem("Debugger", "", &debugger.isOpen);
+                    ImGui::MenuItem("Importer", "", &importer.isOpen);
+                    ImGui::MenuItem("Project", "", &project.isOpen);
+                    ImGui::MenuItem("Scene Editor", "", &sceneEditor.isOpen);
+                    ImGui::MenuItem("Voxel Pallete Editor", "", &voxelPalleteEditor.isOpen);
 
-                ImGui::MenuItem("Save", "Ctrl+S");
-                ImGui::MenuItem("Save As", "Shift+Ctrl+S");
-                ImGui::MenuItem("Revert", "F12");
-                ImGui::Separator();
+                    ImGui::EndMenu();
+                }
 
-                ImGui::MenuItem("Export");
-                ImGui::MenuItem("Generate");
-                ImGui::MenuItem("Share");
-                ImGui::Separator();
-
-                ImGui::MenuItem("Print...", "Ctrl+P");
-                ImGui::MenuItem("Print One Copy", "Ctrl+P");
-                ImGui::Separator();
-
-                ImGui::MenuItem("File Info...", "Alt+Shift+Ctrl+1");
-                ImGui::MenuItem("Exit", "Ctrl+Q");
-
-                ImGui::EndMenu();
-            }
-            if (ImGui::BeginMenu("Edit"))
-            {
-                ImGui::MenuItem("Fullscreen");
-                ImGui::MenuItem("Padding");
-                ImGui::EndMenu();
-            }
-            if (ImGui::BeginMenu("View"))
-            {
-                ImGui::MenuItem("Asset Explorer", "", &assetExplorer.isOpen);
-                ImGui::MenuItem("Debugger", "", &debugger.isOpen);
-                ImGui::MenuItem("Importer", "", &importer.isOpen);
-                ImGui::MenuItem("Project", "", &project.isOpen);
-                ImGui::MenuItem("Scene Editor", "", &sceneEditor.isOpen);
-                ImGui::MenuItem("Voxel Pallete Editor", "", &voxelPalleteEditor.isOpen);
-
-                ImGui::EndMenu();
+                ImGui::EndMenuBar();
             }
 
-            ImGui::EndMenuBar();
-        }
-
-    ImGui::End();
-    });
+            ImGui::End();
+        });
 }
