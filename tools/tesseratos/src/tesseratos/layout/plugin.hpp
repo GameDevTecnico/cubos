32474c4b692089a9119764e12630cba123225967
jdbaracho <joao.d.baracho@gmail.com>
2025-07-01 18:37:16 +0100

    feat(tesseratos): implement programatic layout setup

---
+++ tools/tesseratos/src/tesseratos/layout/plugin.hpp
@@ -0,0 +1,31 @@
+/// @dir
+/// @brief @ref tesseratos-layout-plugin plugin directory.
+
+/// @file
+/// @brief Plugin entry point.
+/// @ingroup tesseratos-layout-plugin
+
+#pragma once
+
+#include <cubos/engine/prelude.hpp>
+
+namespace tesseratos
+{
+    /// @defgroup tesseratos-layout-plugin Menu bar
+    /// @ingroup tesseratos
+    /// @brief Manages the docking layouts for tesseratos.
+
+    /// @brief Layout state.
+    struct LayoutState
+    {
+        CUBOS_REFLECT;
+
+        bool isReady{false};
+        std::string layout;
+    };
+
+    /// @brief Plugin entry function.
+    /// @param cubos @b Cubos main class
+    /// @ingroup tesseratos-layout-plugin
+    void layoutPlugin(cubos::engine::Cubos& cubos);
+} // namespace tesseratos
