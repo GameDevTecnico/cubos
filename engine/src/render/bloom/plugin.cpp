0b08d969cbdbc7c875880644b59926e3c55e0bb0
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2025-07-12 14:36:28 +0100

    feat(rendering): rename profiling metrics

--- engine/src/render/bloom/plugin.cpp
+++ engine/src/render/bloom/plugin.cpp
@@ -287,7 +287,7 @@ void cubos::engine::bloomPlugin(Cubos& cubos)
 
             if (profiler.profilingEnabled)
             {
-                CUBOS_METRIC("Bloom", state.timer->end().result);
+                CUBOS_METRIC("Graphics::Bloom", state.timer->end().result);
             }
         });
 }
