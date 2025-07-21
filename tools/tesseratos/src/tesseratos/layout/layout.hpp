b6f308aefd9f8241c7f00ee532a3e2936df46696
jdbaracho <joao.d.baracho@gmail.com>
2025-07-21 13:10:50 +0100

    chore(tesseratos): cleanup wrong comments and unnused functions

--- tools/tesseratos/src/tesseratos/layout/layout.hpp
+++ tools/tesseratos/src/tesseratos/layout/layout.hpp
@@ -41,30 +41,15 @@ namespace tesseratos
     };
 
     /// @brief Describes a layout for tesseratos.
-    ///
-    /// A layout defines how the dockspace is split and wich windows are docked where.
     class Layout
     {
     public:
-        CUBOS_REFLECT;
-
-        /// @brief Empty constructor.
-        Layout() = default;
-
-        /// @brief Loads the scene node from a JSON object.
-        /// @param json JSON object.
-        /// @param components Component type registry.
-        /// @param relations Relation type registry.
-        /// @return Whether the scene node was successfully loaded.
-        void load(const ImGuiID root, const nlohmann::json& json);
-
+        static LayoutNode createFromImGui();
         static Layout loadFromJson(nlohmann::json json);
 
         void applyToImGui(const ImGuiID root);
 
     private:
         std::unique_ptr<LayoutNode> content;
-
-        ImGuiDir direction(std::string dir);
     };
 } // namespace tesseratos
